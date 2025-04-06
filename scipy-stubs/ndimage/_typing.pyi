13665ac882577d93f6cc15762d3697b2a13b1beb
jorenham <jhammudoglu@gmail.com>
2025-04-06 10:41:42 +0200

    🔥 `ndimage`: remove unused type aliases

--- scipy-stubs/ndimage/_typing.pyi
+++ scipy-stubs/ndimage/_typing.pyi
@@ -3,20 +3,8 @@ from typing import Any, TypeAlias
 import numpy as np
 import optype.numpy as onp
 
-__all__ = (
-    "_ComplexArrayOut",
-    "_ComplexValueOut",
-    "_FloatArrayOut",
-    "_FloatValueOut",
-    "_ScalarArrayOut",
-    "_ScalarValueOut",
-)
+__all__ = "_ComplexArrayOut", "_FloatArrayOut", "_ScalarArrayOut"
 
-_FloatValueOut: TypeAlias = np.float64 | np.float32
-_FloatArrayOut: TypeAlias = onp.ArrayND[_FloatValueOut]
-
-_ComplexValueOut: TypeAlias = _FloatValueOut | np.complex128 | np.complex64
-_ComplexArrayOut: TypeAlias = onp.ArrayND[_ComplexValueOut]
-
-_ScalarValueOut: TypeAlias = np.number[Any] | np.bool_
-_ScalarArrayOut: TypeAlias = onp.ArrayND[_ScalarValueOut]
+_FloatArrayOut: TypeAlias = onp.ArrayND[np.float64 | np.float32]
+_ComplexArrayOut: TypeAlias = onp.ArrayND[np.complex128 | np.float64 | np.complex64 | np.float32]
+_ScalarArrayOut: TypeAlias = onp.ArrayND[np.number[Any] | np.bool_]
