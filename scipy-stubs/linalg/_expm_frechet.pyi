083555c36c7aec9b1489ec7a482e369dd14d1f87
jorenham <jhammudoglu@gmail.com>
2025-05-22 19:06:52 +0200

    👽️ scipy 1.16 batch support for `linalg._expm_frechet`

--- scipy-stubs/linalg/_expm_frechet.pyi
+++ scipy-stubs/linalg/_expm_frechet.pyi
@@ -7,36 +7,41 @@ from scipy._typing import Falsy, Truthy
 __all__ = ["expm_cond", "expm_frechet"]
 
 _Method: TypeAlias = Literal["SPS", "blockEnlarge"]
-_Float2D: TypeAlias = onp.Array2D[np.float64]
-_Complex2D: TypeAlias = onp.Array2D[np.complex128]
+_ArrayF64: TypeAlias = onp.ArrayND[np.float64]
+_ArrayC128: TypeAlias = onp.ArrayND[np.complex128]
 
 ###
 
 @overload
 def expm_frechet(
-    A: onp.ToComplex2D,
-    E: onp.ToComplex2D,
+    A: onp.ToComplexND,
+    E: onp.ToComplexND,
     method: _Method | None = None,
     compute_expm: Truthy = True,
     check_finite: bool = True,
-) -> tuple[_Float2D, _Float2D] | tuple[_Float2D | _Complex2D, _Complex2D]: ...
+) -> tuple[_ArrayF64, _ArrayF64] | tuple[_ArrayF64 | _ArrayC128, _ArrayC128]: ...
 @overload
 def expm_frechet(
-    A: onp.ToComplex2D,
-    E: onp.ToComplex2D,
+    A: onp.ToComplexND,
+    E: onp.ToComplexND,
     method: _Method | None,
     compute_expm: Falsy,
     check_finite: bool = True,
-) -> tuple[_Float2D, _Float2D] | tuple[_Float2D | _Complex2D, _Complex2D]: ...
+) -> tuple[_ArrayF64, _ArrayF64] | tuple[_ArrayF64 | _ArrayC128, _ArrayC128]: ...
 @overload
 def expm_frechet(
-    A: onp.ToComplex2D,
-    E: onp.ToComplex2D,
+    A: onp.ToComplexND,
+    E: onp.ToComplexND,
     method: _Method | None = None,
     *,
     compute_expm: Falsy,
     check_finite: bool = True,
-) -> tuple[_Float2D, _Float2D] | tuple[_Float2D | _Complex2D, _Complex2D]: ...
+) -> tuple[_ArrayF64, _ArrayF64] | tuple[_ArrayF64 | _ArrayC128, _ArrayC128]: ...
 
 #
-def expm_cond(A: onp.ToComplex2D, check_finite: bool = True) -> np.float64: ...
+@overload
+def expm_cond(A: onp.ToComplexStrict2D, check_finite: bool = True) -> np.float64: ...
+@overload
+def expm_cond(A: onp.ToComplexStrict3D, check_finite: bool = True) -> onp.Array1D[np.float64]: ...
+@overload
+def expm_cond(A: onp.ToComplexND, check_finite: bool = True) -> np.float64 | onp.ArrayND[np.float64]: ...
