a40cd1d992504b073298d53283b71ce3e4e5beff
jorenham <jhammudoglu@gmail.com>
2025-05-22 18:44:49 +0200

    👽️ scipy 1.16 batch support for `linalg.[ord]qz`

--- scipy-stubs/linalg/_decomp_qz.pyi
+++ scipy-stubs/linalg/_decomp_qz.pyi
@@ -11,12 +11,9 @@ _T2 = TypeVar("_T2")
 _Tuple4: TypeAlias = tuple[_T2, _T2, _T2, _T2]
 _Tuple222: TypeAlias = tuple[_T2, _T2, _T1, _T1, _T2, _T2]
 
-_Float1D: TypeAlias = onp.Array1D[np.floating[Any]]
-_Float2D: TypeAlias = onp.Array2D[np.floating[Any]]
-_Complex1D: TypeAlias = onp.Array1D[np.complexfloating[Any, Any]]
-_Complex2D: TypeAlias = onp.Array2D[np.complexfloating[Any, Any]]
-_Inexact1D: TypeAlias = onp.Array1D[np.inexact[Any]]
-_Inexact2D: TypeAlias = onp.Array2D[np.inexact[Any]]
+_FloatND: TypeAlias = onp.ArrayND[np.floating[Any]]
+_ComplexND: TypeAlias = onp.ArrayND[np.complexfloating[Any, Any]]
+_InexactND: TypeAlias = onp.ArrayND[np.inexact[Any]]
 
 _OutputReal: TypeAlias = Literal["real", "r"]
 _OutputComplex: TypeAlias = Literal["complex", "c"]
@@ -28,77 +25,77 @@ _Sort: TypeAlias = Literal["lhp", "rhp", "iuc", "ouc"] | Callable[[float, float]
 # NOTE: `sort` will raise `ValueError` if not `None`.
 @overload  # float, {"real"}
 def qz(
-    A: onp.ToFloat2D,
-    B: onp.ToFloat2D,
+    A: onp.ToFloatND,
+    B: onp.ToFloatND,
     output: _OutputReal = "real",
     lwork: onp.ToJustInt | None = None,
     sort: None = None,
     overwrite_a: onp.ToBool = False,
     overwrite_b: onp.ToBool = False,
     check_finite: onp.ToBool = True,
-) -> _Tuple4[_Float2D]: ...
+) -> _Tuple4[_FloatND]: ...
 @overload  # complex, {"real"}
 def qz(
-    A: onp.ToComplex2D,
-    B: onp.ToComplex2D,
+    A: onp.ToComplexND,
+    B: onp.ToComplexND,
     output: _OutputReal = "real",
     lwork: onp.ToJustInt | None = None,
     sort: None = None,
     overwrite_a: onp.ToBool = False,
     overwrite_b: onp.ToBool = False,
     check_finite: onp.ToBool = True,
-) -> _Tuple4[_Inexact2D]: ...
+) -> _Tuple4[_InexactND]: ...
 @overload  # complex, {"complex"}
 def qz(
-    A: onp.ToComplex2D,
-    B: onp.ToComplex2D,
+    A: onp.ToComplexND,
+    B: onp.ToComplexND,
     output: _OutputComplex,
     lwork: onp.ToJustInt | None = None,
     sort: None = None,
     overwrite_a: onp.ToBool = False,
     overwrite_b: onp.ToBool = False,
     check_finite: onp.ToBool = True,
-) -> _Tuple4[_Complex2D]: ...
+) -> _Tuple4[_ComplexND]: ...
 
 #
 @overload  # float, {"real"}
 def ordqz(
-    A: onp.ToFloat2D,
-    B: onp.ToFloat2D,
+    A: onp.ToFloatND,
+    B: onp.ToFloatND,
     sort: _Sort = "lhp",
     output: _OutputReal = "real",
     overwrite_a: onp.ToBool = False,
     overwrite_b: onp.ToBool = False,
     check_finite: onp.ToBool = True,
-) -> _Tuple222[_Float2D, _Float1D]: ...
+) -> _Tuple222[_FloatND, _FloatND]: ...
 @overload  # complex, {"real"}
 def ordqz(
-    A: onp.ToComplex2D,
-    B: onp.ToComplex2D,
+    A: onp.ToComplexND,
+    B: onp.ToComplexND,
     sort: _Sort = "lhp",
     output: _OutputReal = "real",
     overwrite_a: onp.ToBool = False,
     overwrite_b: onp.ToBool = False,
     check_finite: onp.ToBool = True,
-) -> _Tuple222[_Inexact2D, _Inexact1D]: ...
+) -> _Tuple222[_InexactND, _InexactND]: ...
 @overload  # complex, {"complex"} (positional)
 def ordqz(
-    A: onp.ToComplex2D,
-    B: onp.ToComplex2D,
+    A: onp.ToComplexND,
+    B: onp.ToComplexND,
     sort: _Sort,
     output: _OutputComplex,
     overwrite_a: onp.ToBool = False,
     overwrite_b: onp.ToBool = False,
     check_finite: onp.ToBool = True,
-) -> _Tuple222[_Complex2D, _Complex1D]: ...
+) -> _Tuple222[_ComplexND, _ComplexND]: ...
 @overload  # complex, {"complex"} (keyword)
 def ordqz(
-    A: onp.ToComplex2D,
-    B: onp.ToComplex2D,
+    A: onp.ToComplexND,
+    B: onp.ToComplexND,
     sort: _Sort = "lhp",
     *,
     output: _OutputComplex,
     overwrite_a: onp.ToBool = False,
     overwrite_b: onp.ToBool = False,
     check_finite: onp.ToBool = True,
-) -> _Tuple222[_Complex2D, _Complex1D]: ...
+) -> _Tuple222[_ComplexND, _ComplexND]: ...
