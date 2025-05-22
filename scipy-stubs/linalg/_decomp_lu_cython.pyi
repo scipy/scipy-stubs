59fefa05578ce73c11d41bf20de3cd45e9ca1b63
jorenham <jhammudoglu@gmail.com>
2025-05-22 16:45:05 +0200

    👽️ scipy 1.16 addition for `linalg._decomp_lu_cython`

--- scipy-stubs/linalg/_decomp_lu_cython.pyi
+++ scipy-stubs/linalg/_decomp_lu_cython.pyi
@@ -3,9 +3,10 @@ from typing import TypeAlias, TypeVar
 import numpy as np
 import optype.numpy as onp
 
-_ST = TypeVar("_ST", bound=np.float32 | np.float64 | np.complex64 | np.complex128)
-_Int1D: TypeAlias = onp.Array1D[np.int32 | np.int64]
+_AnyInexactT = TypeVar("_AnyInexactT", np.float32, np.float64, np.complex64, np.complex128)
+_Int1D: TypeAlias = onp.ArrayND[np.int32 | np.int64]
 
 ###
 
-def lu_dispatcher(a: onp.Array2D[_ST], u: onp.Array2D[_ST], piv: _Int1D, permute_l: onp.ToBool) -> None: ...
+def lu_decompose(a: onp.Array2D[_AnyInexactT], u: onp.Array2D[_AnyInexactT], piv: _Int1D, permute_l: onp.ToBool) -> None: ...
+def lu_dispatcher(a: onp.Array2D[_AnyInexactT], u: onp.Array2D[_AnyInexactT], piv: _Int1D, permute_l: onp.ToBool) -> None: ...
