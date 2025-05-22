505ce946b3910762eb559fc21a40c57b3206ecee
jorenham <jhammudoglu@gmail.com>
2025-05-22 03:07:33 +0200

    🚨 `fftpack.helper`: fix python-version-dependent  stubtest error

--- scipy-stubs/fftpack/helper.pyi
+++ scipy-stubs/fftpack/helper.pyi
@@ -1,5 +1,4 @@
 # This module is not meant for public use and will be removed in SciPy v2.0.0.
-import sys
 from typing import Final
 from typing_extensions import deprecated
 
@@ -7,14 +6,8 @@ __all__ = ["fftfreq", "fftshift", "ifftshift", "next_fast_len", "rfftfreq"]
 
 __MESSAGE: Final = "will be removed in SciPy v2.0.0"
 
-if sys.version_info >= (3, 13):
-    # `device` was added in numpy 2
-    @deprecated(__MESSAGE)
-    def fftfreq(n: object, d: object = ..., device: object = ...) -> object: ...
-else:
-    @deprecated(__MESSAGE)
-    def fftfreq(n: object, d: object = ...) -> object: ...
-
+@deprecated(__MESSAGE)
+def fftfreq(n: object, d: object = ..., device: object = ...) -> object: ...
 @deprecated(__MESSAGE)
 def fftshift(x: object, axes: object = ...) -> object: ...
 @deprecated(__MESSAGE)
