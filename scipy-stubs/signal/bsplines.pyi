14b6999cb9ae0d00c825112c3c52b908983286ec
jorenham <jhammudoglu@gmail.com>
2025-05-26 02:31:55 +0200

    🎨 `signal`: dedupe the `@deprecated` public submodule function definitions

--- scipy-stubs/signal/bsplines.pyi
+++ scipy-stubs/signal/bsplines.pyi
@@ -1,6 +1,8 @@
 # This module is not meant for public use and will be removed in SciPy v2.0.0.
 from typing_extensions import deprecated
 
+from .spline import sepfir2d
+
 __all__ = ["cspline1d", "cspline1d_eval", "cspline2d", "gauss_spline", "qspline1d", "qspline1d_eval", "sepfir2d", "spline_filter"]
 
 @deprecated("will be removed in SciPy v2.0.0")
@@ -17,7 +19,3 @@ def qspline1d(signal: object, lamb: object = ...) -> object: ...
 def qspline1d_eval(cj: object, newx: object, dx: object = ..., x0: object = ...) -> object: ...
 @deprecated("will be removed in SciPy v2.0.0")
 def spline_filter(Iin: object, lmbda: object = ...) -> object: ...
-
-# _spline
-@deprecated("will be removed in SciPy v2.0.0")
-def sepfir2d(input: object, hrow: object, hcol: object) -> object: ...
