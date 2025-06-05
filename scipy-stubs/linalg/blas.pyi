01e72b449b829c8462972663da412241d94118c0
jorenham <jhammudoglu@gmail.com>
2025-06-05 04:48:56 +0200

    ✨ `linalg.blas`: overload `get_blas_funcs` for better return type in most cases

--- scipy-stubs/linalg/blas.pyi
+++ scipy-stubs/linalg/blas.pyi
@@ -1,11 +1,11 @@
 from collections.abc import Iterable, Sequence
-from typing import Literal
+from typing import Literal as L, overload
 
 import numpy as np
 import numpy.typing as npt
 import optype.numpy as onp
 
-import scipy._typing as spt
+from scipy._typing import SequenceNotStr, _FortranFunction
 
 __all__ = ["find_best_blas_type", "get_blas_funcs"]
 
@@ -13,14 +13,28 @@ __all__ = ["find_best_blas_type", "get_blas_funcs"]
 def find_best_blas_type(
     arrays: Sequence[onp.ArrayND] = (), dtype: npt.DTypeLike | None = None
 ) -> (
-    tuple[Literal["s"], np.dtype[np.float32], bool]
-    | tuple[Literal["f"], np.dtype[np.float64], bool]
-    | tuple[Literal["c"], np.dtype[np.complex64], bool]
-    | tuple[Literal["z"], np.dtype[np.complex128], bool]
+    tuple[L["s"], np.dtype[np.float32], bool]
+    | tuple[L["f"], np.dtype[np.float64], bool]
+    | tuple[L["c"], np.dtype[np.complex64], bool]
+    | tuple[L["z"], np.dtype[np.complex128], bool]
 ): ...
+
+#
+@overload
 def get_blas_funcs(
-    names: Iterable[str] | str,
+    names: str, arrays: Sequence[onp.ArrayND] = (), dtype: npt.DTypeLike | None = None, ilp64: L["preferred"] | bool = False
+) -> _FortranFunction: ...
+@overload
+def get_blas_funcs(
+    names: SequenceNotStr[str],
     arrays: Sequence[onp.ArrayND] = (),
     dtype: npt.DTypeLike | None = None,
-    ilp64: Literal["preferred"] | bool = False,
-) -> list[spt._FortranFunction] | spt._FortranFunction: ...
+    ilp64: L["preferred"] | bool = False,
+) -> list[_FortranFunction]: ...
+@overload
+def get_blas_funcs(
+    names: Iterable[str],
+    arrays: Sequence[onp.ArrayND] = (),
+    dtype: npt.DTypeLike | None = None,
+    ilp64: L["preferred"] | bool = False,
+) -> list[_FortranFunction] | _FortranFunction: ...
