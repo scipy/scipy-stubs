2da6343b7cb9be081fbb67ea64026582d7494bf2
jorenham <jhammudoglu@gmail.com>
2025-06-15 00:31:47 +0200

    🏷️ `sparse`: prefer `typing.SupportsIndex`  over `op.CanIndex` for performance reasons

--- scipy-stubs/sparse/_typing.pyi
+++ scipy-stubs/sparse/_typing.pyi
@@ -1,10 +1,9 @@
 # NOTE(scipy-stubs): This ia a module only exists `if typing.TYPE_CHECKING: ...`, and has no stable API.
 
-from typing import Literal, Protocol, TypeAlias, TypeVar, final, type_check_only
+from typing import Literal, Protocol, SupportsIndex, TypeAlias, TypeVar, final, type_check_only
 from typing_extensions import TypeAliasType
 
 import numpy as np
-import optype as op
 import optype.numpy as onp
 import optype.numpy.compat as npc
 
@@ -28,10 +27,10 @@ Numeric = TypeAliasType("Numeric", np.bool_ | npc.number)
 
 Index1D: TypeAlias = onp.Array1D[np.int32]
 
-ToShape1D: TypeAlias = tuple[op.CanIndex]  # ndim == 1
-ToShape2D: TypeAlias = tuple[op.CanIndex, op.CanIndex]  # ndim == 2
-ToShapeMin1D: TypeAlias = tuple[op.CanIndex, *tuple[op.CanIndex, ...]]  # ndim >= 1
-ToShapeMin3D: TypeAlias = tuple[op.CanIndex, op.CanIndex, op.CanIndex, *tuple[op.CanIndex, ...]]  # ndim >= 2
+ToShape1D: TypeAlias = tuple[SupportsIndex]  # ndim == 1
+ToShape2D: TypeAlias = tuple[SupportsIndex, SupportsIndex]  # ndim == 2
+ToShapeMin1D: TypeAlias = tuple[SupportsIndex, *tuple[SupportsIndex, ...]]  # ndim >= 1
+ToShapeMin3D: TypeAlias = tuple[SupportsIndex, SupportsIndex, SupportsIndex, *tuple[SupportsIndex, ...]]  # ndim >= 2
 
 SPFormat: TypeAlias = Literal["bsr", "coo", "csc", "csr", "dia", "dok", "lil"]
 
