5b0e14fc9d9589323b64840b21d903eaba31a59b
jorenham <jhammudoglu@gmail.com>
2025-06-16 06:17:54 +0200

    🩹 `datasets`: remove parameter default in `_utils._clear_cache`

--- scipy-stubs/datasets/_utils.pyi
+++ scipy-stubs/datasets/_utils.pyi
@@ -10,6 +10,6 @@ _Datasets: TypeAlias = Fetcher | list[Callable[[], onp.ArrayND]] | tuple[Fetcher
 # NOTE: The implementation explicitly checks that `datasets` sequence is either a `list` or `tuple`, and will raise an
 # `AssertionError` for any other (sequence) types.
 def _clear_cache(
-    datasets: _Datasets | None = None, cache_dir: str | None = None, method_map: Mapping[str, Iterable[str]] | None = None
+    datasets: _Datasets | None, cache_dir: str | None = None, method_map: Mapping[str, Iterable[str]] | None = None
 ) -> None: ...  # undocumented
 def clear_cache(datasets: _Datasets | None = None) -> None: ...
