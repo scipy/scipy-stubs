75b4c0efe04bbf4b20ce3c8cea62c2b3816b4301
jorenham <jhammudoglu@gmail.com>
2025-06-23 17:53:37 +0200

    🐛 `optimize`: Add missing `seed` kwarg to `dual_annealing`

--- scipy-stubs/optimize/_dual_annealing.pyi
+++ scipy-stubs/optimize/_dual_annealing.pyi
@@ -6,7 +6,6 @@ import optype.numpy as onp
 
 from ._constraints import Bounds
 from ._typing import MinimizerKwargs
-from scipy._typing import ToRNG
 from scipy.optimize import OptimizeResult as _OptimizeResult
 
 __all__ = ["dual_annealing"]
@@ -37,8 +36,10 @@ def dual_annealing(
     visit: onp.ToFloat = 2.62,
     accept: onp.ToFloat = -5.0,
     maxfun: onp.ToFloat = 10_000_000.0,
-    rng: ToRNG = None,
+    rng: onp.random.ToRNG | None = None,
     no_local_search: onp.ToBool = False,
     callback: Callable[[_Float1D, float, Literal[0, 1, 2]], bool | None] | None = None,
     x0: onp.ToFloat1D | None = None,
+    *,
+    seed: onp.random.ToRNG | None = None,
 ) -> _OptimizeResult: ...
