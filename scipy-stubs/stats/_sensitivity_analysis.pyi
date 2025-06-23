0b1e4b856ec23aaafa1030398f06618d54592686
jorenham <jhammudoglu@gmail.com>
2025-06-23 18:57:33 +0200

    🐛 `stats`: Add missing `random_state` kwarg to `sobol_indices`

--- scipy-stubs/stats/_sensitivity_analysis.pyi
+++ scipy-stubs/stats/_sensitivity_analysis.pyi
@@ -1,18 +1,18 @@
 from collections.abc import Callable, Mapping, Sequence
 from dataclasses import dataclass
-from typing import Any, Literal, Protocol, TypeAlias, type_check_only
+from typing import Literal, Protocol, TypeAlias, type_check_only
 
 import numpy as np
 import optype.numpy as onp
+import optype.numpy.compat as npc
 
 from ._resampling import BootstrapResult
-from scipy._typing import ToRNG
 
 __all__ = ["sobol_indices"]
 
 _SobolKey: TypeAlias = Literal["f_A", "f_B", "f_AB"]
 _SobolMethod: TypeAlias = Callable[
-    [onp.ArrayND[np.float64], onp.ArrayND[np.float64], onp.ArrayND[np.float64]],
+    [onp.Array2D[np.float64], onp.Array2D[np.float64], onp.Array3D[np.float64]],
     tuple[onp.ToFloat | onp.ToFloatND, onp.ToFloat | onp.ToFloatND],
 ]
 
@@ -30,37 +30,39 @@ class BootstrapSobolResult:
 
 @dataclass
 class SobolResult:
-    first_order: onp.ArrayND[np.float64]
-    total_order: onp.ArrayND[np.float64]
-    _indices_method: Callable[[onp.ArrayND[np.float64]], onp.ArrayND[np.float64]]
-    _f_A: onp.ArrayND[np.float64]
-    _f_B: onp.ArrayND[np.float64]
-    _f_AB: onp.ArrayND[np.float64]
-    _A: onp.ArrayND[np.float64] | None = None
-    _B: onp.ArrayND[np.float64] | None = None
-    _AB: onp.ArrayND[np.float64] | None = None
+    first_order: onp.Array2D[np.float64]
+    total_order: onp.Array2D[np.float64]
+
+    _indices_method: _SobolMethod
+    _f_A: onp.Array2D[np.float64]
+    _f_B: onp.Array2D[np.float64]
+    _f_AB: onp.Array3D[np.float64]
+    _A: onp.Array2D[np.float64] | None = None
+    _B: onp.Array2D[np.float64] | None = None
+    _AB: onp.Array3D[np.float64] | None = None
     _bootstrap_result: BootstrapResult | None = None
 
     def bootstrap(self, /, confidence_level: onp.ToFloat = 0.95, n_resamples: onp.ToInt = 999) -> BootstrapSobolResult: ...
 
 #
-def f_ishigami(x: onp.ToFloat2D) -> onp.Array1D[np.floating[Any]]: ...
+def f_ishigami(x: onp.ToFloat2D) -> onp.Array1D[npc.floating]: ...
 
 #
-def sample_A_B(n: onp.ToInt, dists: Sequence[_HasPPF], rng: ToRNG = None) -> onp.ArrayND[np.float64]: ...
-def sample_AB(A: onp.ArrayND[np.float64], B: onp.ArrayND[np.float64]) -> onp.ArrayND[np.float64]: ...
+def sample_A_B(n: onp.ToInt, dists: Sequence[_HasPPF], rng: onp.random.ToRNG | None = None) -> onp.Array2D[np.float64]: ...
+def sample_AB(A: onp.Array2D[np.float64], B: onp.Array2D[np.float64]) -> onp.Array3D[np.float64]: ...
 
 #
 def saltelli_2010(
-    f_A: onp.ArrayND[np.float64], f_B: onp.ArrayND[np.float64], f_AB: onp.ArrayND[np.float64]
-) -> tuple[onp.ArrayND[np.float64], onp.ArrayND[np.float64]]: ...
+    f_A: onp.Array2D[np.float64], f_B: onp.Array2D[np.float64], f_AB: onp.Array3D[np.float64]
+) -> tuple[onp.Array2D[np.float64], onp.Array2D[np.float64]]: ...
 
 #
 def sobol_indices(
     *,
-    func: Callable[[onp.Array2D[np.float64]], onp.ToComplex2D] | Mapping[_SobolKey, onp.ArrayND[np.number[Any]]],
+    func: Callable[[onp.Array2D[np.float64]], onp.ToComplex2D] | Mapping[_SobolKey, onp.ArrayND[npc.number]],
     n: onp.ToJustInt,
     dists: Sequence[_HasPPF] | None = None,
     method: _SobolMethod | Literal["saltelli_2010"] = "saltelli_2010",
-    rng: ToRNG = None,
+    rng: onp.random.ToRNG | None = None,
+    random_state: onp.random.ToRNG | None = None,
 ) -> SobolResult: ...
