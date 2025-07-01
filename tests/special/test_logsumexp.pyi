b7217802657232c9d9fe10300cb8b4dfc6fa1292
jorenham <jhammudoglu@gmail.com>
2025-07-02 00:01:51 +0200

    ✨ `special`: improved `[log_]softmax` annotations

--- tests/special/test_logsumexp.pyi
+++ tests/special/test_logsumexp.pyi
@@ -3,7 +3,7 @@ from typing import Any, assert_type
 import numpy as np
 import numpy.typing as npt
 
-from scipy.special import logsumexp
+from scipy.special import logsumexp, softmax
 
 py_f_0d: float
 py_c_0d: complex
@@ -75,3 +75,16 @@ assert_type(
 assert_type(
     logsumexp(c64_1d, axis=0, return_sign=True), tuple[npt.NDArray[np.floating[Any]] | Any, npt.NDArray[np.complex64] | Any]
 )
+
+###
+# softmax (equiv log_softmax)
+
+assert_type(softmax(py_f_0d), np.float64)
+assert_type(softmax(py_c_0d), np.complex128)
+assert_type(softmax(f16_0d), np.float16)
+assert_type(softmax(c64_0d), np.complex64)
+
+assert_type(softmax(py_f_1d), npt.NDArray[np.float64])
+assert_type(softmax(py_c_1d), npt.NDArray[np.complex128])
+assert_type(softmax(f16_1d), np.ndarray[tuple[int], np.dtype[np.float16]])
+assert_type(softmax(c64_1d), np.ndarray[tuple[int], np.dtype[np.complex64]])
