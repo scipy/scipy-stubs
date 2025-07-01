2ba937e7c007bd7c5e897e27fad4e4b2749d7b32
jorenham <jhammudoglu@gmail.com>
2025-07-02 00:42:42 +0200

    ✨ `linalg`: improved `svd` annotations

--- scipy-stubs/linalg/_decomp_svd.pyi
+++ scipy-stubs/linalg/_decomp_svd.pyi
@@ -4,69 +4,91 @@ from typing_extensions import TypeVar
 import numpy as np
 import optype as op
 import optype.numpy as onp
+import optype.numpy.compat as npc
 
 from scipy._typing import Falsy, Truthy
 
 __all__ = ["diagsvd", "null_space", "orth", "subspace_angles", "svd", "svdvals"]
 
+_T = TypeVar("_T")
+_Tuple3: TypeAlias = tuple[_T, _T, _T]
+
 _Float: TypeAlias = np.float32 | np.float64
 _FloatND: TypeAlias = onp.ArrayND[_Float]
 
 _Complex: TypeAlias = np.complex64 | np.complex128
-_ComplexND: TypeAlias = onp.ArrayND[_Complex]
 
 _LapackDriver: TypeAlias = Literal["gesdd", "gesvd"]
-
-_FloatSVD: TypeAlias = tuple[_FloatND, _FloatND, _FloatND]
-_ComplexSVD: TypeAlias = tuple[_ComplexND, _FloatND, _ComplexND]
 
 _RealT = TypeVar("_RealT", bound=np.bool_ | np.integer[Any] | np.floating[Any])
 _InexactT = TypeVar("_InexactT", bound=_Float | _Complex)
 
+_as_f32: TypeAlias = np.float32 | np.float16  # noqa: PYI042
+_as_f64: TypeAlias = np.longdouble | np.float64 | npc.integer | np.bool_  # noqa: PYI042
+
 ###
 
-@overload
+@overload  # nd float64
 def svd(
-    a: onp.ToFloatND,
+    a: onp.ToArrayND[float, _as_f64],
     full_matrices: onp.ToBool = True,
     compute_uv: Truthy = True,
     overwrite_a: onp.ToBool = False,
     check_finite: onp.ToBool = True,
     lapack_driver: _LapackDriver = "gesdd",
-) -> _FloatSVD: ...
-@overload
+) -> _Tuple3[onp.ArrayND[np.float64]]: ...
+@overload  # nd float32
 def svd(
-    a: onp.ToComplexND,
+    a: onp.ToArrayND[_as_f32, _as_f32],
     full_matrices: onp.ToBool = True,
     compute_uv: Truthy = True,
     overwrite_a: onp.ToBool = False,
     check_finite: onp.ToBool = True,
     lapack_driver: _LapackDriver = "gesdd",
-) -> _FloatSVD | _ComplexSVD: ...
-@overload  # complex, compute_uv: {False}
+) -> _Tuple3[onp.ArrayND[np.float32]]: ...
+@overload  # nd complex128
 def svd(
-    a: onp.ToComplexND,
-    full_matrices: onp.ToBool,
-    compute_uv: Falsy,
+    a: onp.ToArrayND[op.JustComplex, np.complex128 | np.clongdouble],
+    full_matrices: onp.ToBool = True,
+    compute_uv: Truthy = True,
     overwrite_a: onp.ToBool = False,
     check_finite: onp.ToBool = True,
     lapack_driver: _LapackDriver = "gesdd",
-) -> _FloatND: ...
-@overload  # complex, *, compute_uv: {False}
+) -> tuple[onp.ArrayND[np.complex128], onp.ArrayND[np.float64], onp.ArrayND[np.complex128]]: ...
+@overload  # nd complex64
 def svd(
-    a: onp.ToComplexND,
+    a: onp.ToArrayND[np.complex64, np.complex64],
+    full_matrices: onp.ToBool = True,
+    compute_uv: Truthy = True,
+    overwrite_a: onp.ToBool = False,
+    check_finite: onp.ToBool = True,
+    lapack_driver: _LapackDriver = "gesdd",
+) -> tuple[onp.ArrayND[np.complex64], onp.ArrayND[np.float32], onp.ArrayND[np.complex64]]: ...
+@overload  # nd float64 | complex128, compute_uv=False (keyword)
+def svd(
+    a: onp.ToArrayND[complex, _as_f64 | np.complex128 | np.clongdouble],
     full_matrices: onp.ToBool = True,
     *,
     compute_uv: Falsy,
     overwrite_a: onp.ToBool = False,
     check_finite: onp.ToBool = True,
     lapack_driver: _LapackDriver = "gesdd",
-) -> _FloatND: ...
+) -> onp.ArrayND[np.float64]: ...
+@overload  # nd float32 | complex64, compute_uv=False (keyword)
+def svd(
+    a: onp.ToArrayND[_as_f32, _as_f32 | np.complex64],
+    full_matrices: onp.ToBool = True,
+    *,
+    compute_uv: Falsy,
+    overwrite_a: onp.ToBool = False,
+    check_finite: onp.ToBool = True,
+    lapack_driver: _LapackDriver = "gesdd",
+) -> onp.ArrayND[np.float32]: ...
 
 #
 def svdvals(a: onp.ToComplexND, overwrite_a: onp.ToBool = False, check_finite: onp.ToBool = True) -> _FloatND: ...
 
-# beware the overlapping overloads for bool <: int (<: float)
+#
 @overload
 def diagsvd(s: onp.SequenceND[_RealT] | onp.CanArrayND[_RealT], M: op.CanIndex, N: op.CanIndex) -> onp.ArrayND[_RealT]: ...
 @overload
