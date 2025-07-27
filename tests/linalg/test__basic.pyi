752170938503c539fee9a4fb6fcbdb22025180a2
jorenham <jhammudoglu@gmail.com>
2025-07-27 15:19:25 +0200

    ✅ `linalg`: fix some of the failing tests on `numpy<2.1`

--- tests/linalg/test__basic.pyi
+++ tests/linalg/test__basic.pyi
@@ -1,3 +1,5 @@
+# ruff: noqa: ERA001
+
 # type-tests for `linalg/_basic.pyi`
 
 from typing import assert_type
@@ -45,9 +47,6 @@ c128_2d: onp.Array2D[np.complex128]
 c128_3d: onp.Array3D[np.complex128]
 c128_nd: onp.ArrayND[np.complex128]
 
-# NOTE: These extended precision types may not exist at runtime, but are used
-# here to work around `[c]longdouble` issues on `numpy<2.2`
-
 f80_1d: onp.Array1D[np.float128]
 f80_2d: onp.Array2D[np.float128]
 f80_3d: onp.Array3D[np.float128]
@@ -71,6 +70,8 @@ py_f_3d: list[list[list[float]]]
 py_c_1d: list[complex]
 py_c_2d: list[list[complex]]
 py_c_3d: list[list[list[complex]]]
+
+# NOTE: the commented out assertions only work on numpy 2.1+, so we instead check for assignability
 
 ###
 # solve
@@ -340,36 +341,54 @@ assert_type(solve_circulant(py_c_2d, py_c_1d), onp.ArrayND[np.complex128])
 ###
 # inv
 
-assert_type(inv(f32_2d), onp.Array2D[np.float32])
-assert_type(inv(f64_2d), onp.Array2D[np.float64])
-assert_type(inv(c64_2d), onp.Array2D[np.complex64])
-assert_type(inv(c128_2d), onp.Array2D[np.complex128])
+# assert_type(inv(f32_2d), onp.Array2D[np.float32])
+# assert_type(inv(f64_2d), onp.Array2D[np.float64])
+# assert_type(inv(c64_2d), onp.Array2D[np.complex64])
+# assert_type(inv(c128_2d), onp.Array2D[np.complex128])
+_00: onp.Array2D[np.float32] = inv(f32_2d)
+_01: onp.Array2D[np.float64] = inv(f64_2d)
+_02: onp.Array2D[np.complex64] = inv(c64_2d)
+_03: onp.Array2D[np.complex128] = inv(c128_2d)
 
 assert_type(inv(py_b_2d), onp.Array2D[np.float32])
 assert_type(inv(py_i_2d), onp.Array2D[np.float64])
 assert_type(inv(py_f_2d), onp.Array2D[np.float64])
 assert_type(inv(py_c_2d), onp.Array2D[np.complex128])
 
-assert_type(inv(f32_3d), onp.Array3D[np.float32])
-assert_type(inv(f64_3d), onp.Array3D[np.float64])
-assert_type(inv(c64_3d), onp.Array3D[np.complex64])
-assert_type(inv(c128_3d), onp.Array3D[np.complex128])
+# assert_type(inv(f32_3d), onp.Array3D[np.float32])
+# assert_type(inv(f64_3d), onp.Array3D[np.float64])
+# assert_type(inv(c64_3d), onp.Array3D[np.complex64])
+# assert_type(inv(c128_3d), onp.Array3D[np.complex128])
+_10: onp.Array3D[np.float32] = inv(f32_3d)
+_11: onp.Array3D[np.float64] = inv(f64_3d)
+_12: onp.Array3D[np.complex64] = inv(c64_3d)
+_13: onp.Array3D[np.complex128] = inv(c128_3d)
 
 assert_type(inv(py_b_3d), onp.ArrayND[np.float32])
 assert_type(inv(py_i_3d), onp.ArrayND[np.float64])
 assert_type(inv(py_f_3d), onp.ArrayND[np.float64])
 assert_type(inv(py_c_3d), onp.ArrayND[np.complex128])
 
-assert_type(inv(b1_nd), onp.ArrayND[np.float32])
-assert_type(inv(i8_nd), onp.ArrayND[np.float32])
-assert_type(inv(f16_nd), onp.ArrayND[np.float32])
-assert_type(inv(f32_nd), onp.ArrayND[np.float32])
-assert_type(inv(i32_nd), onp.ArrayND[np.float64])
-assert_type(inv(f64_nd), onp.ArrayND[np.float64])
-assert_type(inv(f80_nd), onp.ArrayND[np.float64])
-assert_type(inv(c64_nd), onp.ArrayND[np.complex64])
-assert_type(inv(c128_nd), onp.ArrayND[np.complex128])
-assert_type(inv(c160_nd), onp.ArrayND[np.complex128])
+# assert_type(inv(b1_nd), onp.ArrayND[np.float32])
+# assert_type(inv(i8_nd), onp.ArrayND[np.float32])
+# assert_type(inv(f16_nd), onp.ArrayND[np.float32])
+# assert_type(inv(f32_nd), onp.ArrayND[np.float32])
+# assert_type(inv(i32_nd), onp.ArrayND[np.float64])
+# assert_type(inv(f64_nd), onp.ArrayND[np.float64])
+# assert_type(inv(f80_nd), onp.ArrayND[np.float64])
+# assert_type(inv(c64_nd), onp.ArrayND[np.complex64])
+# assert_type(inv(c128_nd), onp.ArrayND[np.complex128])
+# assert_type(inv(c160_nd), onp.ArrayND[np.complex128])
+_20: onp.ArrayND[np.float32] = inv(b1_nd)
+_21: onp.ArrayND[np.float32] = inv(i8_nd)
+_22: onp.ArrayND[np.float32] = inv(f16_nd)
+_23: onp.ArrayND[np.float32] = inv(f32_nd)
+_24: onp.ArrayND[np.float64] = inv(i32_nd)
+_25: onp.ArrayND[np.float64] = inv(f64_nd)
+_26: onp.ArrayND[np.float64] = inv(f80_nd)
+_27: onp.ArrayND[np.complex64] = inv(c64_nd)
+_28: onp.ArrayND[np.complex128] = inv(c128_nd)
+_29: onp.ArrayND[np.complex128] = inv(c160_nd)
 
 ###
 # det
