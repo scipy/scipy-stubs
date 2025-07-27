52d42cabc29c29f9a8eb73c161aac3ec19ec4257
jorenham <jhammudoglu@gmail.com>
2025-07-27 15:11:34 +0200

    ✅ `fft`: fix failing tests on `numpy<2.1`

--- tests/fft/test_basic.pyi
+++ tests/fft/test_basic.pyi
@@ -1,3 +1,5 @@
+# ruff: noqa: ERA001
+
 from typing import assert_type
 
 import numpy as np
@@ -51,30 +53,42 @@ c160_3d: onp.Array3D[np.complex256]
 ###
 # fft
 
+# NOTE: the commented out assertions only work on numpy 2.1+, so we instead check for assignability
+
 assert_type(fft(int_1d), onp.Array1D[np.complex128])
-assert_type(fft(int_2d), onp.ArrayND[np.complex128])
 assert_type(fft(float_1d), onp.Array1D[np.complex128])
-assert_type(fft(float_2d), onp.ArrayND[np.complex128])
 assert_type(fft(complex_1d), onp.Array1D[np.complex128])
+
+assert_type(fft(int_2d), onp.ArrayND[np.complex128])
+assert_type(fft(float_2d), onp.ArrayND[np.complex128])
 assert_type(fft(complex_2d), onp.ArrayND[np.complex128])
 
-assert_type(fft(i16_1d), onp.Array1D[np.complex128])
-assert_type(fft(i16_2d), onp.Array2D[np.complex128])
+# assert_type(fft(i16_1d), onp.Array1D[np.complex128])
+# assert_type(fft(f32_1d), onp.Array1D[np.complex64])
+# assert_type(fft(f64_1d), onp.Array1D[np.complex128])
+# assert_type(fft(f80_1d), onp.Array1D[np.clongdouble])
+# assert_type(fft(c64_1d), onp.Array1D[np.complex64])
+# assert_type(fft(c128_1d), onp.Array1D[np.complex128])
+# assert_type(fft(c160_1d), onp.Array1D[np.clongdouble])
+_10: onp.Array1D[np.complex128] = fft(i16_1d)
+_11: onp.Array1D[np.complex64] = fft(f32_1d)
+_12: onp.Array1D[np.complex128] = fft(f64_1d)
+_13: onp.Array1D[np.clongdouble] = fft(f80_1d)
+_14: onp.Array1D[np.complex64] = fft(c64_1d)
+_15: onp.Array1D[np.complex128] = fft(c128_1d)
+_16: onp.Array1D[np.clongdouble] = fft(c160_1d)
 
-assert_type(fft(f32_1d), onp.Array1D[np.complex64])
-assert_type(fft(f32_2d), onp.Array2D[np.complex64])
-
-assert_type(fft(f64_1d), onp.Array1D[np.complex128])
-assert_type(fft(f64_2d), onp.Array2D[np.complex128])
-
-assert_type(fft(f80_1d), onp.Array1D[np.clongdouble])
-assert_type(fft(f80_2d), onp.Array2D[np.clongdouble])
-
-assert_type(fft(c64_1d), onp.Array1D[np.complex64])
-assert_type(fft(c64_2d), onp.Array2D[np.complex64])
-
-assert_type(fft(c128_1d), onp.Array1D[np.complex128])
-assert_type(fft(c128_2d), onp.Array2D[np.complex128])
-
-assert_type(fft(c160_1d), onp.Array1D[np.clongdouble])
-assert_type(fft(c160_2d), onp.Array2D[np.clongdouble])
+# assert_type(fft(i16_2d), onp.Array2D[np.complex128])
+# assert_type(fft(f32_2d), onp.Array2D[np.complex64])
+# assert_type(fft(f64_2d), onp.Array2D[np.complex128])
+# assert_type(fft(f80_2d), onp.Array2D[np.clongdouble])
+# assert_type(fft(c64_2d), onp.Array2D[np.complex64])
+# assert_type(fft(c128_2d), onp.Array2D[np.complex128])
+# assert_type(fft(c160_2d), onp.Array2D[np.clongdouble])
+_20: onp.Array2D[np.complex128] = fft(i16_2d)
+_21: onp.Array2D[np.complex64] = fft(f32_2d)
+_22: onp.Array2D[np.complex128] = fft(f64_2d)
+_23: onp.Array2D[np.clongdouble] = fft(f80_2d)
+_24: onp.Array2D[np.complex64] = fft(c64_2d)
+_25: onp.Array2D[np.complex128] = fft(c128_2d)
+_26: onp.Array2D[np.clongdouble] = fft(c160_2d)
