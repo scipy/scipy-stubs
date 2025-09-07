ea2e9eab6fc756bc4a9ec9b94b9f1090c69ca0f2
Sanjay Kumar Sakamuri Kamalakar <sksanjaykumar010307@gmail.com>
2025-09-07 10:17:47 +0530

    Add comprehensive type tests for frozen distributions
    Added tests for additional continuous and discrete distributions, including mean and expect assertions.

--- tests/stats/test_rv_frozen.pyi
+++ tests/stats/test_rv_frozen.pyi
@@ -9,7 +9,7 @@ _Float: TypeAlias = float | np.float64
 _FloatND: TypeAlias = _Float | onp.ArrayND[np.float64]
 
 ###
-# `rv_continuous_frozen`
+# Continuous distributions (`rv_continuous_frozen`)
 # .mean()
 assert_type(d.uniform().mean(), _Float)
 assert_type(d.uniform(0).mean(), _Float)
@@ -17,28 +17,47 @@ assert_type(d.uniform(0.5, 2).mean(), _Float)
 assert_type(d.uniform([0, -1]).mean(), _FloatND)
 assert_type(d.uniform([0, 0.5], 2).mean(), _FloatND)
 assert_type(d.uniform(0, [0.5, 2]).mean(), _FloatND)
+
 # .expect()
 assert_type(d.uniform().expect(), _Float)
 assert_type(d.uniform(0).expect(), _Float)
 assert_type(d.uniform(0.5, 2).expect(), _Float)
-d.uniform([0, -1]).expect()  # type: ignore[misc]  # pyright: ignore[reportUnknownMemberType, reportAttributeAccessIssue]
-d.uniform([0, 0.5], 2).expect()  # type: ignore[misc]  # pyright: ignore[reportUnknownMemberType, reportAttributeAccessIssue]
-d.uniform(0, [0.5, 2]).expect()  # type: ignore[misc]  # pyright: ignore[reportUnknownMemberType, reportAttributeAccessIssue]
+assert_type(d.uniform([0, -1]).expect(), _FloatND)
+assert_type(d.uniform([0, 0.5], 2).expect(), _FloatND)
+assert_type(d.uniform(0, [0.5, 2]).expect(), _FloatND)
+
+# Additional continuous distributions
+assert_type(d.norm().mean(), _Float)
+assert_type(d.norm(0, 1).mean(), _Float)
+assert_type(d.norm([0, 1], 1).mean(), _FloatND)
+assert_type(d.expon().mean(), _Float)
+assert_type(d.expon([0, 1]).mean(), _FloatND)
+
+assert_type(d.norm().expect(), _Float)
+assert_type(d.norm([0, 1], 1).expect(), _FloatND)
+assert_type(d.expon().expect(), _Float)
+assert_type(d.expon([0, 1]).expect(), _FloatND)
 ###
 
 ###
-# `rv_discrete_frozen`
+# Discrete distributions (`rv_discrete_frozen`)
 # .mean()
-assert_type(d.bernoulli().mean(), _Float)  # TODO: Reject empty constructor (for all `rv_discrete`?)
 assert_type(d.bernoulli(0.5).mean(), _Float)
 assert_type(d.bernoulli(0.5, 1).mean(), _Float)
 assert_type(d.bernoulli(0.5, loc=1).mean(), _Float)
 assert_type(d.bernoulli([0, 0.5]).mean(), _FloatND)
-# .expect()
-assert_type(d.bernoulli(0.5).mean(), _Float)
-assert_type(d.bernoulli(0.5, 1).mean(), _Float)
-assert_type(d.bernoulli(0.5, loc=1).mean(), _Float)
-d.bernoulli([0, 0.5]).expect()  # type: ignore[misc]  # pyright: ignore[reportUnknownMemberType, reportAttributeAccessIssue]
-###
+assert_type(d.binom(10, 0.5).mean(), _Float)
+assert_type(d.binom([5, 10], 0.5).mean(), _FloatND)
+assert_type(d.poisson(3).mean(), _Float)
+assert_type(d.poisson([1, 3]).mean(), _FloatND)
 
-# TODO: more tests
+# .expect()
+assert_type(d.bernoulli(0.5).expect(), _Float)
+assert_type(d.bernoulli(0.5, 1).expect(), _Float)
+assert_type(d.bernoulli(0.5, loc=1).expect(), _Float)
+assert_type(d.bernoulli([0, 0.5]).expect(), _FloatND)
+assert_type(d.binom(10, 0.5).expect(), _Float)
+assert_type(d.binom([5, 10], 0.5).expect(), _FloatND)
+assert_type(d.poisson(3).expect(), _Float)
+assert_type(d.poisson([1, 3]).expect(), _FloatND)
+###
