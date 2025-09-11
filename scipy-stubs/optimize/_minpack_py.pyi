79728e911d3454eb44215304517cc6fc4b31b505
jorenham <jhammudoglu@gmail.com>
2025-09-11 18:22:37 +0200

    🐛 `optimize`: also accept `None` for certain `curve_fit` kwargs

--- scipy-stubs/optimize/_minpack_py.pyi
+++ scipy-stubs/optimize/_minpack_py.pyi
@@ -5,9 +5,10 @@ from typing_extensions import Unpack
 import numpy as np
 import optype as op
 import optype.numpy as onp
+import optype.numpy.compat as npc
 
 from ._constraints import Bounds
-from scipy.sparse import sparray, spmatrix
+from scipy.sparse._typing import _Sparse2D
 
 __all__ = ["curve_fit", "fixed_point", "fsolve", "leastsq"]
 
@@ -25,6 +26,8 @@ _Fun2D: TypeAlias = _Fun[_Float2D, onp.ToFloat1D]
 _Jac1D: TypeAlias = _Fun[_Float1D, onp.ToFloat2D]
 _Jac2D: TypeAlias = _Fun[_Float2D, onp.ToFloat2D]
 
+_FloatBounds: TypeAlias = tuple[float | onp.ToFloat1D, float | onp.ToFloat1D]
+
 _JacMethod: TypeAlias = Literal["2-point", "3-point", "cs"]
 _CurveFitMethod: TypeAlias = Literal["lm", "trf", "dogbox"]
 _NanPolicy: TypeAlias = Literal["raise", "omit"]  # no "propagate"
@@ -33,26 +36,26 @@ _IERFlag: TypeAlias = Literal[1, 2, 3, 4, 5, 6, 7, 8]
 @final
 @type_check_only
 class _KwargsCurveFit(TypedDict, total=False):
-    ftol: onp.ToFloat
-    xtol: onp.ToFloat
-    gtol: onp.ToFloat
+    ftol: float | None  # = 1.49012e-8
+    xtol: float | None  # = 1.49012e-8
+    gtol: float | None  # = 0.0
 
     # leastsq
-    col_deriv: onp.ToBool
-    maxfev: onp.ToJustInt
-    epsfcn: onp.ToFloat | None
-    factor: onp.ToJustInt
-    diag: onp.ToFloat1D | None
+    col_deriv: op.CanBool  # = False
+    maxfev: int  # = 0
+    epsfcn: float | None  # = finfo(dtype).eps
+    factor: float | None  # = 100.0
+    diag: onp.ToFloat1D | None  # = None
 
     # least_squares
-    x_scale: onp.ToFloat | onp.ToFloatND | Literal["jac"]
-    f_scale: onp.ToFloat
+    x_scale: float | onp.ToFloatND | Literal["jac"]
+    f_scale: float
     loss: _Fun[_Float1D, onp.ToFloat1D] | Literal["linear", "soft_l1", "huber", "cauchy", "arctan"]
     diff_step: onp.ToFloat1D | None
     tr_solver: Literal["exact", "lsmr"]
     tr_options: Mapping[str, object]
-    jac_sparsity: onp.ToFloat2D | spmatrix | sparray
-    max_nfev: onp.ToJustInt
+    jac_sparsity: onp.ToFloat2D | _Sparse2D[npc.floating | npc.integer]
+    max_nfev: int | None  # = None
     verbose: Literal[0, 1, 2]
     kwargs: Mapping[str, object]
 
@@ -86,12 +89,12 @@ def fsolve(
     args: tuple[object, ...] = (),
     fprime: _Jac1D | None = None,
     full_output: onp.ToFalse = 0,
-    col_deriv: onp.ToBool = 0,
-    xtol: onp.ToFloat = 1.49012e-8,
-    maxfev: onp.ToJustInt = 0,
-    band: tuple[onp.ToJustInt, onp.ToJustInt] | None = None,
-    epsfcn: onp.ToFloat | None = None,
-    factor: onp.ToJustInt = 100,
+    col_deriv: op.CanBool = 0,
+    xtol: float | None = 1.49012e-8,
+    maxfev: int = 0,
+    band: tuple[int, int] | None = None,
+    epsfcn: float | None = None,
+    factor: float | None = 100,
     diag: onp.ToFloat1D | None = None,
 ) -> _Float1D: ...
 @overload  # full_output=True (positional)
@@ -101,12 +104,12 @@ def fsolve(
     args: tuple[object, ...],
     fprime: _Jac1D | None,
     full_output: onp.ToTrue,
-    col_deriv: onp.ToBool = 0,
-    xtol: onp.ToFloat = 1.49012e-8,
-    maxfev: onp.ToJustInt = 0,
-    band: tuple[onp.ToJustInt, onp.ToJustInt] | None = None,
-    epsfcn: onp.ToFloat | None = None,
-    factor: onp.ToJustInt = 100,
+    col_deriv: op.CanBool = 0,
+    xtol: float | None = 1.49012e-8,
+    maxfev: int = 0,
+    band: tuple[int, int] | None = None,
+    epsfcn: float | None = None,
+    factor: float | None = 100,
     diag: onp.ToFloat1D | None = None,
 ) -> tuple[_Float1D, _InfoDictSolve, _IERFlag, str]: ...
 @overload  # full_output=True (keyword)
@@ -117,12 +120,12 @@ def fsolve(
     fprime: _Jac1D | None = None,
     *,
     full_output: onp.ToTrue,
-    col_deriv: onp.ToBool = 0,
-    xtol: onp.ToFloat = 1.49012e-8,
-    maxfev: onp.ToJustInt = 0,
-    band: tuple[onp.ToJustInt, onp.ToJustInt] | None = None,
-    epsfcn: onp.ToFloat | None = None,
-    factor: onp.ToJustInt = 100,
+    col_deriv: op.CanBool = 0,
+    xtol: float | None = 1.49012e-8,
+    maxfev: int = 0,
+    band: tuple[int, int] | None = None,
+    epsfcn: float | None = None,
+    factor: float | None = 100,
     diag: onp.ToFloat1D | None = None,
 ) -> tuple[_Float1D, _InfoDictSolve, _IERFlag, str]: ...
 
@@ -134,13 +137,13 @@ def leastsq(
     args: tuple[object, ...] = (),
     Dfun: _Jac1D | None = None,
     full_output: onp.ToFalse = False,
-    col_deriv: onp.ToBool = False,
-    ftol: onp.ToFloat = 1.49012e-8,
-    xtol: onp.ToFloat = 1.49012e-8,
-    gtol: onp.ToFloat = 0.0,
-    maxfev: onp.ToJustInt = 0,
-    epsfcn: onp.ToFloat | None = None,
-    factor: onp.ToJustInt = 100,
+    col_deriv: op.CanBool = False,
+    ftol: float | None = 1.49012e-8,
+    xtol: float | None = 1.49012e-8,
+    gtol: float | None = 0.0,
+    maxfev: int = 0,
+    epsfcn: float | None = None,
+    factor: float | None = 100,
     diag: onp.ToFloat1D | None = None,
 ) -> tuple[_Float1D, _IERFlag]: ...
 @overload  # full_output=True (positional)
@@ -150,13 +153,13 @@ def leastsq(
     args: tuple[object, ...],
     Dfun: _Jac1D | None,
     full_output: onp.ToTrue,
-    col_deriv: onp.ToBool = False,
-    ftol: onp.ToFloat = 1.49012e-8,
-    xtol: onp.ToFloat = 1.49012e-8,
-    gtol: onp.ToFloat = 0.0,
-    maxfev: onp.ToJustInt = 0,
-    epsfcn: onp.ToFloat | None = None,
-    factor: onp.ToJustInt = 100,
+    col_deriv: op.CanBool = False,
+    ftol: float | None = 1.49012e-8,
+    xtol: float | None = 1.49012e-8,
+    gtol: float | None = 0.0,
+    maxfev: int = 0,
+    epsfcn: float | None = None,
+    factor: float | None = 100,
     diag: onp.ToFloat1D | None = None,
 ) -> tuple[_Float1D, _Float2D, _InfoDictLSQ, str, _IERFlag]: ...
 @overload  # full_output=True (keyword)
@@ -167,13 +170,13 @@ def leastsq(
     Dfun: _Jac1D | None = None,
     *,
     full_output: onp.ToTrue,
-    col_deriv: onp.ToBool = False,
-    ftol: onp.ToFloat = 1.49012e-8,
-    xtol: onp.ToFloat = 1.49012e-8,
-    gtol: onp.ToFloat = 0.0,
-    maxfev: onp.ToJustInt = 0,
-    epsfcn: onp.ToFloat | None = None,
-    factor: onp.ToJustInt = 100,
+    col_deriv: op.CanBool = False,
+    ftol: float | None = 1.49012e-8,
+    xtol: float | None = 1.49012e-8,
+    gtol: float | None = 0.0,
+    maxfev: int = 0,
+    epsfcn: float | None = None,
+    factor: float | None = 100,
     diag: onp.ToFloat1D | None = None,
 ) -> tuple[_Float1D, _Float2D, _InfoDictLSQ, str, _IERFlag]: ...
 
@@ -184,10 +187,10 @@ def curve_fit(
     xdata: onp.ToFloatStrict1D,
     ydata: onp.ToFloat1D,
     p0: onp.ToFloat1D | None = None,
-    sigma: onp.ToFloat | onp.ToFloat1D | onp.ToFloat2D | None = None,
+    sigma: float | onp.ToFloat1D | onp.ToFloat2D | None = None,
     absolute_sigma: op.CanBool = False,
     check_finite: op.CanBool | None = None,
-    bounds: tuple[onp.ToFloat | onp.ToFloat1D, onp.ToFloat | onp.ToFloat1D] | Bounds = ...,
+    bounds: _FloatBounds | Bounds = ...,  # = (-np.inf, np.inf)
     method: _CurveFitMethod | None = None,
     jac: _Jac1D | _JacMethod | None = None,
     *,
@@ -201,10 +204,10 @@ def curve_fit(
     xdata: onp.ToFloatStrict1D,
     ydata: onp.ToFloat1D,
     p0: onp.ToFloat1D | None = None,
-    sigma: onp.ToFloat | onp.ToFloat1D | onp.ToFloat2D | None = None,
+    sigma: float | onp.ToFloat1D | onp.ToFloat2D | None = None,
     absolute_sigma: op.CanBool = False,
     check_finite: op.CanBool | None = None,
-    bounds: tuple[onp.ToFloat | onp.ToFloat1D, onp.ToFloat | onp.ToFloat1D] | Bounds = ...,
+    bounds: _FloatBounds | Bounds = ...,  # = (-np.inf, np.inf)
     method: _CurveFitMethod | None = None,
     jac: _Jac1D | _JacMethod | None = None,
     *,
@@ -218,10 +221,10 @@ def curve_fit(
     xdata: onp.ToFloatStrict2D,
     ydata: onp.ToFloat1D,
     p0: onp.ToFloat1D | None = None,
-    sigma: onp.ToFloat | onp.ToFloat1D | onp.ToFloat2D | None = None,
+    sigma: float | onp.ToFloat1D | onp.ToFloat2D | None = None,
     absolute_sigma: op.CanBool = False,
     check_finite: op.CanBool | None = None,
-    bounds: tuple[onp.ToFloat | onp.ToFloat1D, onp.ToFloat | onp.ToFloat1D] | Bounds = ...,
+    bounds: _FloatBounds | Bounds = ...,  # = (-np.inf, np.inf)
     method: _CurveFitMethod | None = None,
     jac: _Jac2D | _JacMethod | None = None,
     *,
@@ -235,10 +238,10 @@ def curve_fit(
     xdata: onp.ToFloatStrict2D,
     ydata: onp.ToFloat1D,
     p0: onp.ToFloat1D | None = None,
-    sigma: onp.ToFloat | onp.ToFloat1D | onp.ToFloat2D | None = None,
+    sigma: float | onp.ToFloat1D | onp.ToFloat2D | None = None,
     absolute_sigma: op.CanBool = False,
     check_finite: op.CanBool | None = None,
-    bounds: tuple[onp.ToFloat | onp.ToFloat1D, onp.ToFloat | onp.ToFloat1D] | Bounds = ...,
+    bounds: _FloatBounds | Bounds = ...,  # = (-np.inf, np.inf)
     method: _CurveFitMethod | None = None,
     jac: _Jac2D | _JacMethod | None = None,
     *,
@@ -252,10 +255,10 @@ def curve_fit(
     xdata: onp.ToFloat1D | onp.ToFloat2D,
     ydata: onp.ToFloat1D,
     p0: onp.ToFloat1D | None = None,
-    sigma: onp.ToFloat | onp.ToFloat1D | onp.ToFloat2D | None = None,
+    sigma: float | onp.ToFloat1D | onp.ToFloat2D | None = None,
     absolute_sigma: op.CanBool = False,
     check_finite: op.CanBool | None = None,
-    bounds: tuple[onp.ToFloat | onp.ToFloat1D, onp.ToFloat | onp.ToFloat1D] | Bounds = ...,
+    bounds: _FloatBounds | Bounds = ...,  # = (-np.inf, np.inf)
     method: _CurveFitMethod | None = None,
     jac: _Jac1D | _Jac2D | _JacMethod | None = None,
     *,
@@ -269,10 +272,10 @@ def curve_fit(
     xdata: onp.ToFloat1D | onp.ToFloat2D,
     ydata: onp.ToFloat1D,
     p0: onp.ToFloat1D | None = None,
-    sigma: onp.ToFloat | onp.ToFloat1D | onp.ToFloat2D | None = None,
+    sigma: float | onp.ToFloat1D | onp.ToFloat2D | None = None,
     absolute_sigma: op.CanBool = False,
     check_finite: op.CanBool | None = None,
-    bounds: tuple[onp.ToFloat | onp.ToFloat1D, onp.ToFloat | onp.ToFloat1D] | Bounds = ...,
+    bounds: _FloatBounds | Bounds = ...,  # = (-np.inf, np.inf)
     method: _CurveFitMethod | None = None,
     jac: _Jac1D | _Jac2D | _JacMethod | None = None,
     *,
@@ -287,88 +290,88 @@ def fixed_point(
     func: _Fun[np.float64, onp.ToFloat] | _Fun[float, onp.ToFloat],
     x0: onp.ToFloat,
     args: tuple[object, ...] = (),
-    xtol: onp.ToFloat = 1e-08,
-    maxiter: onp.ToJustInt = 500,
+    xtol: float = 1e-8,
+    maxiter: int = 500,
     method: Literal["del2", "iteration"] = "del2",
 ) -> np.float64: ...
 @overload  # 0-d complex
 def fixed_point(
     func: _Fun[np.complex128, onp.ToComplex] | _Fun[complex, onp.ToComplex],
-    x0: onp.ToComplex,
+    x0: onp.ToJustComplex,
     args: tuple[object, ...] = (),
-    xtol: onp.ToFloat = 1e-08,
-    maxiter: onp.ToJustInt = 500,
+    xtol: float = 1e-8,
+    maxiter: int = 500,
     method: Literal["del2", "iteration"] = "del2",
-) -> np.float64 | np.complex128: ...
+) -> np.complex128: ...
 @overload  # 1-d real
 def fixed_point(
     func: _Fun[_Float1D, onp.ToFloat1D],
     x0: onp.ToFloat1D,
     args: tuple[object, ...] = (),
-    xtol: onp.ToFloat = 1e-08,
-    maxiter: onp.ToJustInt = 500,
+    xtol: float = 1e-8,
+    maxiter: int = 500,
     method: Literal["del2", "iteration"] = "del2",
 ) -> _Float1D: ...
 @overload  # 1-d complex
 def fixed_point(
     func: _Fun[onp.Array1D[np.complex128], onp.ToComplex1D],
-    x0: onp.ToComplex1D,
+    x0: onp.ToJustComplex1D,
     args: tuple[object, ...] = (),
-    xtol: onp.ToFloat = 1e-08,
-    maxiter: onp.ToJustInt = 500,
+    xtol: float = 1e-8,
+    maxiter: int = 500,
     method: Literal["del2", "iteration"] = "del2",
-) -> onp.Array1D[np.float64 | np.complex128]: ...
+) -> onp.Array1D[np.complex128]: ...
 @overload  # 2-d real
 def fixed_point(
     func: _Fun[_Float2D, onp.ToFloat2D],
     x0: onp.ToFloat2D,
     args: tuple[object, ...] = (),
-    xtol: onp.ToFloat = 1e-08,
-    maxiter: onp.ToJustInt = 500,
+    xtol: float = 1e-8,
+    maxiter: int = 500,
     method: Literal["del2", "iteration"] = "del2",
 ) -> _Float2D: ...
 @overload  # 2-d complex
 def fixed_point(
     func: _Fun[onp.Array2D[np.complex128], onp.ToComplex2D],
-    x0: onp.ToComplex2D,
+    x0: onp.ToJustComplex2D,
     args: tuple[object, ...] = (),
-    xtol: onp.ToFloat = 1e-08,
-    maxiter: onp.ToJustInt = 500,
+    xtol: float = 1e-8,
+    maxiter: int = 500,
     method: Literal["del2", "iteration"] = "del2",
-) -> onp.Array2D[np.float64 | np.complex128]: ...
+) -> onp.Array2D[np.complex128]: ...
 @overload  # 3-d real
 def fixed_point(
     func: _Fun[onp.Array3D[np.float64], onp.ToFloat3D],
     x0: onp.ToFloat3D,
     args: tuple[object, ...] = (),
-    xtol: onp.ToFloat = 1e-08,
-    maxiter: onp.ToJustInt = 500,
+    xtol: float = 1e-8,
+    maxiter: int = 500,
     method: Literal["del2", "iteration"] = "del2",
 ) -> onp.Array3D[np.float64]: ...
 @overload  # 3-d complex
 def fixed_point(
     func: _Fun[onp.Array3D[np.complex128], onp.ToComplex3D],
-    x0: onp.ToComplex3D,
+    x0: onp.ToJustComplex3D,
     args: tuple[object, ...] = (),
-    xtol: onp.ToFloat = 1e-08,
-    maxiter: onp.ToJustInt = 500,
+    xtol: float = 1e-8,
+    maxiter: int = 500,
     method: Literal["del2", "iteration"] = "del2",
-) -> onp.Array3D[np.float64 | np.complex128]: ...
+) -> onp.Array3D[np.complex128]: ...
 @overload  # N-d real
 def fixed_point(
     func: _Fun[onp.ArrayND[np.float64], onp.ToFloatND],
     x0: onp.ToFloatND,
     args: tuple[object, ...] = (),
-    xtol: onp.ToFloat = 1e-08,
-    maxiter: onp.ToJustInt = 500,
+    xtol: float = 1e-8,
+    maxiter: int = 500,
     method: Literal["del2", "iteration"] = "del2",
 ) -> onp.ArrayND[np.float64]: ...
 @overload  # N-d complex
 def fixed_point(
     func: _Fun[onp.ArrayND[np.complex128], onp.ToComplexND],
-    x0: onp.ToComplexND,
+    x0: onp.ToJustComplexND,
     args: tuple[object, ...] = (),
-    xtol: onp.ToFloat = 1e-08,
-    maxiter: onp.ToJustInt = 500,
+    xtol: float = 1e-8,
+    maxiter: int = 500,
     method: Literal["del2", "iteration"] = "del2",
-) -> onp.ArrayND[np.float64 | np.complex128]: ...
+) -> onp.ArrayND[np.complex128]: ...
