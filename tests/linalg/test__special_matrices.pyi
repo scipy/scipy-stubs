87b67df4f216a1ca4d7575691942f837b6ae2c0a
jorenham <jhammudoglu@gmail.com>
2025-12-11 12:50:43 +0100

    ✅ `linalg`: add tests for `hankel`

--- tests/linalg/test__special_matrices.pyi
+++ tests/linalg/test__special_matrices.pyi
@@ -3,7 +3,7 @@ from typing import Any, assert_type
 import numpy as np
 import optype.numpy as onp
 
-from scipy.linalg import toeplitz  # pyright: ignore[reportUnknownVariableType]
+from scipy.linalg import hankel, toeplitz  # pyright: ignore[reportUnknownVariableType]
 
 arr_f8_1d: onp.Array1D[np.float64]
 arr_f8_2d: onp.Array2D[np.float64]
@@ -26,4 +26,21 @@ assert_type(toeplitz(arr_f8_nd, arr_f8_nd), onp.Array2D[np.float64])
 # https://github.com/microsoft/pyright/issues/11127
 assert_type(toeplitz([[0], [1]]), onp.Array[tuple[int, int, int, tuple[Any, ...]], np.int_])  # type: ignore[assert-type, type-var]  # pyright: ignore[reportInvalidTypeForm]
 assert_type(toeplitz([[[0], [1]], [[2], [3]]]), onp.Array[tuple[int, int, int, tuple[Any, ...]], np.int_])  # type: ignore[assert-type, type-var]  # pyright: ignore[reportInvalidTypeForm]
+###
+
+###
+# hankel
+# > non-deprecated overloads
+assert_type(hankel([0]), onp.Array2D[np.int_])
+assert_type(hankel([0.0]), onp.Array2D[np.float64])
+assert_type(hankel([0j]), onp.Array2D[np.complex128])
+assert_type(hankel(arr_f8_1d), onp.Array2D[np.float64])
+assert_type(hankel(arr_f8_nd), onp.Array2D[np.float64])
+assert_type(hankel(arr_f8_1d, arr_f8_1d), onp.Array2D[np.float64])
+assert_type(hankel(arr_f8_1d, arr_f8_nd), onp.Array2D[np.float64])
+assert_type(hankel(arr_f8_nd, arr_f8_nd), onp.Array2D[np.float64])
+# > deprecated (raveled input)
+# https://github.com/python/mypy/issues/20354
+assert_type(hankel([[0], [1]]), onp.Array2D[np.int_])  # type: ignore[assert-type]  # pyright: ignore[reportDeprecated]  # pyrefly: ignore[deprecated]
+assert_type(hankel([[[0], [1]], [[2], [3]]]), onp.Array2D[np.int_])  # type: ignore[assert-type]   # pyright: ignore[reportDeprecated]  # pyrefly: ignore[deprecated]
 ###
