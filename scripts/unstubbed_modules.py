2712622065fa3d905d528e96ae3aa34f5cf5f77d
jorenham <jhammudoglu@gmail.com>
2025-12-11 12:15:25 +0100

    👽️ `linalg`: new private `_batched_linalg` module

--- scripts/unstubbed_modules.py
+++ scripts/unstubbed_modules.py
@@ -22,7 +22,6 @@ BUNDLED = (
 )
 # TODO(@jorenham): remove when stubs are added for these new SciPy 1.17 modules
 TODO_1_17 = (
-    "scipy.linalg._batched_linalg",
     "scipy.sparse.linalg._funm_multiply_krylov",
     "scipy.spatial.transform._rigid_transform_cy",
     "scipy.spatial.transform._rigid_transform_xp",
