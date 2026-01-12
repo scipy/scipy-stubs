c61002c1e674ad08e2fd5e2c4165e3dd89e04836
jorenham <jhammudoglu@gmail.com>
2026-01-12 17:11:23 +0100

    ✅ `fft`: add tests for `[i]dstn`

--- tests/fft/test_realtransforms.pyi
+++ tests/fft/test_realtransforms.pyi
@@ -37,11 +37,12 @@ from .test_basic import (
     int_1d,
     int_2d,
 )
-from scipy.fft import dctn, idctn
+from scipy.fft import dctn, dstn, idctn, idstn
 
 ###
+# NOTE: the signatures are practically equivalent
 
-# dctn (same as idctn)
+# dctn
 _assert_1d_f64(dctn(int_1d))
 _assert_1d_f64(dctn(float_1d))
 _assert_1d_f64(dctn(float_1d))
@@ -63,7 +64,8 @@ _assert_2d_f80(dctn(f80_2d))
 _assert_2d_c64(dctn(c64_2d))
 _assert_2d_c128(dctn(c128_2d))
 _assert_2d_c160(dctn(c160_2d))
-# idctn (same as dctn)
+
+# idctn
 _assert_1d_f64(idctn(int_1d))
 _assert_1d_f64(idctn(float_1d))
 _assert_1d_f64(idctn(float_1d))
@@ -85,3 +87,49 @@ _assert_2d_f80(idctn(f80_2d))
 _assert_2d_c64(idctn(c64_2d))
 _assert_2d_c128(idctn(c128_2d))
 _assert_2d_c160(idctn(c160_2d))
+
+# dstn
+_assert_1d_f64(dstn(int_1d))
+_assert_1d_f64(dstn(float_1d))
+_assert_1d_f64(dstn(float_1d))
+_assert_1d_c128(dstn(complex_1d))
+_assert_1d_f64(dstn(i16_1d))
+_assert_1d_f32(dstn(f32_1d))
+_assert_1d_f64(dstn(f64_1d))
+_assert_1d_f80(dstn(f80_1d))
+_assert_1d_c64(dstn(c64_1d))
+_assert_1d_c128(dstn(c128_1d))
+_assert_1d_c160(dstn(c160_1d))
+_assert_2d_f64(dstn(int_2d))
+_assert_2d_f64(dstn(float_2d))
+_assert_2d_f64(dstn(complex_2d))
+_assert_2d_f64(dstn(i16_2d))
+_assert_2d_f32(dstn(f32_2d))
+_assert_2d_f64(dstn(f64_2d))
+_assert_2d_f80(dstn(f80_2d))
+_assert_2d_c64(dstn(c64_2d))
+_assert_2d_c128(dstn(c128_2d))
+_assert_2d_c160(dstn(c160_2d))
+
+# idstn
+_assert_1d_f64(idstn(int_1d))
+_assert_1d_f64(idstn(float_1d))
+_assert_1d_f64(idstn(float_1d))
+_assert_1d_c128(idstn(complex_1d))
+_assert_1d_f64(idstn(i16_1d))
+_assert_1d_f32(idstn(f32_1d))
+_assert_1d_f64(idstn(f64_1d))
+_assert_1d_f80(idstn(f80_1d))
+_assert_1d_c64(idstn(c64_1d))
+_assert_1d_c128(idstn(c128_1d))
+_assert_1d_c160(idstn(c160_1d))
+_assert_2d_f64(idstn(int_2d))
+_assert_2d_f64(idstn(float_2d))
+_assert_2d_f64(idstn(complex_2d))
+_assert_2d_f64(idstn(i16_2d))
+_assert_2d_f32(idstn(f32_2d))
+_assert_2d_f64(idstn(f64_2d))
+_assert_2d_f80(idstn(f80_2d))
+_assert_2d_c64(idstn(c64_2d))
+_assert_2d_c128(idstn(c128_2d))
+_assert_2d_c160(idstn(c160_2d))
