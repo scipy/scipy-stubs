66ff9f38ed891b711940671c2b486d9eee563f1c
jorenham <jhammudoglu@gmail.com>
2026-01-12 16:40:46 +0100

    ✅ `fft`: add tests for `[r]fftfreq`

--- tests/fft/test_helper.pyi
+++ tests/fft/test_helper.pyi
@@ -1,6 +1,9 @@
 from typing import assert_type
 
-from scipy.fft import next_fast_len, prev_fast_len
+import numpy as np
+import optype.numpy as onp
+
+from scipy.fft import fftfreq, next_fast_len, prev_fast_len, rfftfreq
 
 ###
 
@@ -13,3 +16,13 @@ assert_type(next_fast_len(target=42, real=True), int)
 assert_type(prev_fast_len(42), int)
 assert_type(prev_fast_len(42, True), int)
 assert_type(prev_fast_len(target=42, real=True), int)
+
+# fftfreq (same as rfftfreq)
+assert_type(fftfreq(8), onp.Array1D[np.float64])
+assert_type(fftfreq(8, 2.0), onp.Array1D[np.float64])
+assert_type(fftfreq(8, d=2.0), onp.Array1D[np.float64])
+
+# rfftfreq (same as fftfreq)
+assert_type(rfftfreq(8), onp.Array1D[np.float64])
+assert_type(rfftfreq(8, 2.0), onp.Array1D[np.float64])
+assert_type(rfftfreq(8, d=2.0), onp.Array1D[np.float64])
