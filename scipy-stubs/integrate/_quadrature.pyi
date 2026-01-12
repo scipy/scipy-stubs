0e81400d7f0ed4c66293fd67f8b8bc0819a9d1a8
jorenham <jhammudoglu@gmail.com>
2026-01-13 00:03:11 +0100

    ✨ `integrate.trapezoid`: improved overloading

--- scipy-stubs/integrate/_quadrature.pyi
+++ scipy-stubs/integrate/_quadrature.pyi
@@ -1,5 +1,5 @@
 from collections.abc import Callable, Sequence
-from typing import Concatenate, Literal, NamedTuple, TypeAlias, overload
+from typing import Any, Concatenate, Literal, NamedTuple, Never, TypeAlias, overload
 from typing_extensions import TypeVar
 
 import numpy as np
@@ -14,6 +14,11 @@ __all__ = ["cumulative_simpson", "cumulative_trapezoid", "fixed_quad", "newton_c
 _NDT_f = TypeVar("_NDT_f", bound=_QuadFuncOut)
 _QuadFuncOut: TypeAlias = onp.ArrayND[npc.floating] | Sequence[float]
 
+_InexactT = TypeVar("_InexactT", bound=npc.inexact)
+
+# workaround for mypy & pyright's failure to conform to the overload typing specification
+_JustAnyShape: TypeAlias = tuple[Never, Never, Never]
+
 ###
 
 class QMCQuadResult(NamedTuple):
@@ -21,16 +26,56 @@ class QMCQuadResult(NamedTuple):
     standard_error: float
 
 # sample-based integration
-@overload
-def trapezoid(
-    y: onp.ToFloatND, x: onp.ToFloatND | None = None, dx: onp.ToFloat = 1.0, axis: op.CanIndex = -1
-) -> npc.floating | onp.ArrayND[npc.floating]: ...
-@overload
-def trapezoid(
-    y: onp.ToComplexND, x: onp.ToFloatND | None = None, dx: onp.ToFloat = 1.0, axis: op.CanIndex = -1
-) -> npc.inexact | onp.ArrayND[npc.inexact]: ...
 
 #
+@overload  # ?d +complex  (mypy & pyright workaround)
+def trapezoid(
+    y: onp.Array[_JustAnyShape, npc.number | np.bool_], x: onp.ToFloatND | None = None, dx: float = 1.0, axis: int = -1
+) -> Any: ...
+@overload  # 1d T:inexact
+def trapezoid(y: onp.Array1D[_InexactT], x: onp.ToFloat1D | None = None, dx: float = 1.0, axis: int = -1) -> _InexactT: ...
+@overload  # 1d +int
+def trapezoid(
+    y: onp.ToArrayStrict1D[float, npc.integer | np.bool_], x: onp.ToFloat1D | None = None, dx: float = 1.0, axis: int = -1
+) -> np.float64: ...
+@overload  # 1d ~complex
+def trapezoid(
+    y: onp.ToJustComplex128Strict1D, x: onp.ToFloat1D | None = None, dx: float = 1.0, axis: int = -1
+) -> np.complex128: ...
+@overload  # 2d T:inexact
+def trapezoid(
+    y: onp.Array2D[_InexactT], x: onp.ToFloatND | None = None, dx: float = 1.0, axis: int = -1
+) -> onp.Array1D[_InexactT]: ...
+@overload  # 2d +int
+def trapezoid(
+    y: onp.ToArrayStrict2D[float, npc.integer | np.bool_], x: onp.ToFloatND | None = None, dx: float = 1.0, axis: int = -1
+) -> onp.Array1D[np.float64]: ...
+@overload  # 2d ~complex
+def trapezoid(
+    y: onp.ToJustComplex128Strict2D, x: onp.ToFloatND | None = None, dx: float = 1.0, axis: int = -1
+) -> onp.Array1D[np.complex128]: ...
+@overload  # Nd T:inexact
+def trapezoid(
+    y: onp.ArrayND[_InexactT], x: onp.ToFloatND | None = None, dx: float = 1.0, axis: int = -1
+) -> onp.ArrayND[_InexactT] | Any: ...
+@overload  # Nd +int
+def trapezoid(
+    y: onp.ToArrayND[float, npc.integer | np.bool_], x: onp.ToFloatND | None = None, dx: float = 1.0, axis: int = -1
+) -> onp.ArrayND[np.float64] | Any: ...
+@overload  # Nd ~complex
+def trapezoid(
+    y: onp.ToJustComplex128_ND, x: onp.ToFloatND | None = None, dx: float = 1.0, axis: int = -1
+) -> onp.ArrayND[np.complex128] | Any: ...
+@overload  # +float (fallback)
+def trapezoid(
+    y: onp.ToFloatND, x: onp.ToFloatND | None = None, dx: float = 1.0, axis: int = -1
+) -> onp.ArrayND[np.float64 | Any] | Any: ...
+@overload  # +complex (fallback)
+def trapezoid(
+    y: onp.ToComplexND, x: onp.ToFloatND | None = None, dx: float = 1.0, axis: int = -1
+) -> onp.ArrayND[np.complex128 | Any] | Any: ...
+
+# TODO(@jorenham): improve like trapezoid
 @overload
 def simpson(
     y: onp.ToFloatND, x: onp.ToFloatND | None = None, *, dx: onp.ToFloat = 1.0, axis: op.CanIndex = -1
@@ -40,7 +85,7 @@ def simpson(
     y: onp.ToComplexND, x: onp.ToFloatND | None = None, *, dx: onp.ToFloat = 1.0, axis: op.CanIndex = -1
 ) -> npc.inexact | onp.ArrayND[npc.inexact]: ...
 
-#
+# TODO(@jorenham): improve like trapezoid
 @overload
 def romb(
     y: onp.ToFloatND, dx: onp.ToFloat = 1.0, axis: op.CanIndex = -1, show: bool = False
@@ -51,6 +96,8 @@ def romb(
 ) -> npc.inexact | onp.ArrayND[npc.inexact]: ...
 
 # sample-based cumulative integration
+
+# TODO(@jorenham): improve
 @overload
 def cumulative_trapezoid(
     y: onp.ToFloatND,
@@ -68,7 +115,7 @@ def cumulative_trapezoid(
     initial: Literal[0] | None = None,
 ) -> onp.ArrayND[npc.inexact]: ...
 
-#
+# TODO(@jorenham): improve
 @overload
 def cumulative_simpson(
     y: onp.ToFloatND,
