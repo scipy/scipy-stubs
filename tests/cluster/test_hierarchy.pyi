296000a8af7bdcc91fe7dee43457f7d72af2886b
jorenham <jhammudoglu@gmail.com>
2026-01-15 14:39:42 +0100

    ✅ `cluster.hierarchy`: test `fclusterdata`

--- tests/cluster/test_hierarchy.pyi
+++ tests/cluster/test_hierarchy.pyi
@@ -4,7 +4,7 @@ from typing import Any, assert_type
 import numpy as np
 import optype.numpy as onp
 
-from scipy.cluster.hierarchy import DisjointSet, fcluster
+from scipy.cluster.hierarchy import DisjointSet, fcluster, fclusterdata
 
 ###
 
@@ -21,6 +21,13 @@ f64_2d: onp.Array1D[np.int64]
 assert_type(fcluster(f64_2d, 1.5), onp.Array1D[np.int32])
 assert_type(fcluster(f64_2d, t=1.5), onp.Array1D[np.int32])
 assert_type(fcluster(f64_2d, 1.5, "inconsistent", R=f64_2d), onp.Array1D[np.int32])
+
+###
+# fclusterdata
+
+assert_type(fclusterdata(f64_2d, 1.5), onp.Array1D[np.int32])
+assert_type(fclusterdata(f64_2d, t=1.5), onp.Array1D[np.int32])
+assert_type(fclusterdata(f64_2d, 1.5, "inconsistent", R=f64_2d), onp.Array1D[np.int32])
 
 ###
 # DisjointSet
