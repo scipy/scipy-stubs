39b1964f261d64b8ef76ec33473de57c25c92864
jorenham <jhammudoglu@gmail.com>
2026-01-16 22:41:14 +0100

    ✅ `signal`: test `dbode` and `dfreqresp`

--- tests/signal/test_ltisys.pyi
+++ tests/signal/test_ltisys.pyi
@@ -3,7 +3,7 @@ from typing import Any, TypeAlias, assert_type
 import numpy as np
 import optype.numpy as onp
 
-from scipy.signal import bode, freqresp, impulse, lsim, lti, step
+from scipy.signal import bode, dbode, dfreqresp, dlti, freqresp, impulse, lsim, lti, step
 
 ###
 
@@ -31,22 +31,38 @@ _lti_f32: lti[np.float32]
 _lti_f64: lti[np.float64]
 _lti_c64: lti[np.complex64]
 _lti_c128: lti[np.complex128]
+_dlti_f32: dlti[np.float32]
+_dlti_f64: dlti[np.float64]
+_dlti_c64: dlti[np.complex64]
+_dlti_c128: dlti[np.complex128]
 
 _tf_cont_f32: tuple[onp.ArrayND[np.float32], _VecF32]
 _tf_cont_f64: tuple[_ArrF64, _VecF64]
 _tf_cont_c64: tuple[onp.ArrayND[np.complex64], _VecC64]
 _tf_cont_c128: tuple[_ArrC128, _VecC128]
+_tf_disc_f32: tuple[onp.ArrayND[np.float32], _VecF32, float]
+_tf_disc_f64: tuple[_ArrF64, _VecF64, float]
 
 _zpk_cont_f32: tuple[_VecF32, _VecF32, float]
 _zpk_cont_f64: tuple[_VecF64, _VecF64, float]
 _zpk_cont_c64: tuple[_VecC64, _VecC64, float]
 _zpk_cont_c128: tuple[_VecC128, _VecC128, float]
+_zpk_disc_f32: tuple[_VecF32, _VecF32, float, float]
+_zpk_disc_f64: tuple[_VecF64, _VecF64, float, float]
 
 _ss_cont_f32: tuple[onp.Array2D[np.float32], onp.Array2D[np.float32], onp.Array2D[np.float32], onp.Array2D[np.float32]]
 _ss_cont_f64: tuple[onp.Array2D[np.float64], onp.Array2D[np.float64], onp.Array2D[np.float64], onp.Array2D[np.float64]]
 _ss_cont_c64: tuple[onp.Array2D[np.complex64], onp.Array2D[np.complex64], onp.Array2D[np.complex64], onp.Array2D[np.complex64]]
 _ss_cont_c128: tuple[
     onp.Array2D[np.complex128], onp.Array2D[np.complex128], onp.Array2D[np.complex128], onp.Array2D[np.complex128]
+]
+_ss_disc_f32: tuple[onp.Array2D[np.float32], onp.Array2D[np.float32], onp.Array2D[np.float32], onp.Array2D[np.float32], float]
+_ss_disc_f64: tuple[onp.Array2D[np.float64], onp.Array2D[np.float64], onp.Array2D[np.float64], onp.Array2D[np.float64], float]
+_ss_disc_c64: tuple[
+    onp.Array2D[np.complex64], onp.Array2D[np.complex64], onp.Array2D[np.complex64], onp.Array2D[np.complex64], float
+]
+_ss_disc_c128: tuple[
+    onp.Array2D[np.complex128], onp.Array2D[np.complex128], onp.Array2D[np.complex128], onp.Array2D[np.complex128], float
 ]
 
 ###
@@ -168,3 +184,47 @@ assert_type(freqresp(_lti_c128), tuple[_VecF64, _VecC128])
 assert_type(freqresp(_tf_cont_c128), tuple[_VecF64, _VecC128])
 assert_type(freqresp(_zpk_cont_c128), tuple[_VecF64, _VecC128])
 assert_type(freqresp(_ss_cont_c128), tuple[_VecF64, _VecC128])
+
+###
+# dlsim
+# TODO
+
+###
+# dimpulse
+# TODO
+
+###
+# dbode
+
+# f32
+assert_type(dbode(_dlti_f32), tuple[_VecF64, _VecF64, _VecF64])
+assert_type(dbode(_tf_disc_f32), tuple[_VecF64, _VecF64, _VecF64])
+assert_type(dbode(_zpk_disc_f32), tuple[_VecF64, _VecF64, _VecF64])
+assert_type(dbode(_ss_disc_f32), tuple[_VecF64, _VecF64, _VecF64])
+# f64
+assert_type(dbode(_dlti_f64), tuple[_VecF64, _VecF64, _VecF64])
+assert_type(dbode(_tf_disc_f64), tuple[_VecF64, _VecF64, _VecF64])
+assert_type(dbode(_zpk_disc_f64), tuple[_VecF64, _VecF64, _VecF64])
+assert_type(dbode(_ss_disc_f64), tuple[_VecF64, _VecF64, _VecF64])
+# c64
+assert_type(dbode(_dlti_c64), tuple[_VecF64, _VecF64, _VecF64])
+# c128
+assert_type(dbode(_dlti_c128), tuple[_VecF64, _VecF64, _VecF64])
+
+###
+# dfreqresp
+
+# f32
+assert_type(dfreqresp(_dlti_f32), tuple[_VecF64, _VecC128])
+assert_type(dfreqresp(_tf_disc_f32), tuple[_VecF64, _VecC128])
+assert_type(dfreqresp(_zpk_disc_f32), tuple[_VecF64, _VecC128])
+assert_type(dfreqresp(_ss_disc_f32), tuple[_VecF64, _VecC128])
+# f64
+assert_type(dfreqresp(_dlti_f64), tuple[_VecF64, _VecC128])
+assert_type(dfreqresp(_tf_disc_f64), tuple[_VecF64, _VecC128])
+assert_type(dfreqresp(_zpk_disc_f64), tuple[_VecF64, _VecC128])
+assert_type(dfreqresp(_ss_disc_f64), tuple[_VecF64, _VecC128])
+# c64
+assert_type(dfreqresp(_dlti_c64), tuple[_VecF64, _VecC128])
+# c128
+assert_type(dfreqresp(_dlti_c128), tuple[_VecF64, _VecC128])
