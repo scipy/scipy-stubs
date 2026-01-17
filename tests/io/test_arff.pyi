3f9d7bde317f1231d101e4dbe0b74dbb61eb498b
jorenham <jhammudoglu@gmail.com>
2026-01-17 23:22:49 +0100

    ✅ `io.arff`: test `loadarff`

---
+++ tests/io/test_arff.pyi
@@ -0,0 +1,17 @@
+import io
+from typing import assert_type
+
+import numpy as np
+import optype.numpy as onp
+
+from scipy.io.arff import MetaData, loadarff
+
+###
+
+str_literal: str
+str_io: io.StringIO
+
+###
+
+assert_type(loadarff(str_literal), tuple[onp.ArrayND[np.void], MetaData])
+assert_type(loadarff(str_io), tuple[onp.ArrayND[np.void], MetaData])
