e09f94b82212ffdd5f249be67ac362322c219e91
jorenham <jhammudoglu@gmail.com>
2026-01-17 20:13:40 +0100

    ✅ `io`: test `loadmat`

---
+++ tests/io/test_matlab.pyi
@@ -0,0 +1,20 @@
+from typing import Any, assert_type
+
+from scipy.io import loadmat
+
+###
+
+# loadmat
+assert_type(loadmat("file.mat"), dict[str, Any])
+assert_type(loadmat("file.mat", mdict={}), dict[str, Any])
+assert_type(loadmat("file.mat", appendmat=False), dict[str, Any])
+assert_type(loadmat("file.mat", spmatrix=False), dict[str, Any])
+assert_type(loadmat("file.mat", byte_order="native"), dict[str, Any])
+assert_type(loadmat("file.mat", mat_dtype=True), dict[str, Any])
+assert_type(loadmat("file.mat", squeeze_me=False), dict[str, Any])
+assert_type(loadmat("file.mat", chars_as_strings=False), dict[str, Any])
+assert_type(loadmat("file.mat", matlab_compatible=True), dict[str, Any])
+assert_type(loadmat("file.mat", struct_as_record=True), dict[str, Any])
+assert_type(loadmat("file.mat", verify_compressed_data_integrity=True), dict[str, Any])
+assert_type(loadmat("file.mat", simplify_cells=True), dict[str, Any])
+assert_type(loadmat("file.mat", variable_names=("a", "b")), dict[str, Any])
