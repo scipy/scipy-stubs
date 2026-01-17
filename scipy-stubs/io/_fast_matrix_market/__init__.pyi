4568d2f74bed450ec81040f42ff179e16b67ce49
jorenham <jhammudoglu@gmail.com>
2026-01-17 21:00:10 +0100

    ♻️ `io`: dedupe `mm{read,write,info}`

--- scipy-stubs/io/_fast_matrix_market/__init__.pyi
+++ scipy-stubs/io/_fast_matrix_market/__init__.pyi
@@ -1,19 +1,18 @@
 import io
-from typing import Final, Literal, TypeAlias, overload, type_check_only
+from typing import Any, Final, Literal, TypeAlias, overload, type_check_only
 from typing_extensions import TypedDict, Unpack, override
 
 import optype.numpy as onp
-import optype.numpy.compat as npc
 
-from scipy.io._typing import FileLike, FileName
-from scipy.sparse import coo_array, coo_matrix
-from scipy.sparse._base import _spbase
+from scipy.io._typing import FileLike
+from scipy.sparse import coo_array, coo_matrix, sparray, spmatrix
 
 __all__ = ["mminfo", "mmread", "mmwrite"]
 
 _Format: TypeAlias = Literal["coordinate", "array"]
 _Field: TypeAlias = Literal["real", "complex", "pattern", "integer"]
 _Symmetry: TypeAlias = Literal["general", "symmetric", "skew-symmetric", "hermitian"]
+_Info: TypeAlias = tuple[int, int, int, _Format, _Field, _Symmetry]
 
 @type_check_only
 class _TextToBytesWrapperKwargs(TypedDict, total=False):
@@ -45,20 +44,21 @@ class _TextToBytesWrapper(io.BufferedReader):
     @override
     def seek(self, /, offset: int, whence: int = 0) -> None: ...  # type: ignore[override]  # pyright: ignore[reportIncompatibleMethodOverride]  # ty: ignore[invalid-method-override]
 
+#
 @overload
-def mmread(source: FileLike[bytes], *, spmatrix: onp.ToTrue = True) -> onp.ArrayND[npc.number] | coo_array: ...
+def mmread(source: FileLike[bytes], *, spmatrix: onp.ToTrue = True) -> onp.Array2D | coo_matrix: ...
 @overload
-def mmread(source: FileLike[bytes], *, spmatrix: onp.ToFalse) -> onp.ArrayND[npc.number] | coo_matrix: ...
+def mmread(source: FileLike[bytes], *, spmatrix: onp.ToFalse) -> onp.Array2D | coo_array[Any, tuple[int, int]]: ...
 
 #
 def mmwrite(
-    target: FileName,
-    a: onp.CanArray | list[object] | tuple[object, ...] | _spbase,
-    comment: str | None = None,
+    target: FileLike[bytes],
+    a: spmatrix | sparray | onp.ToArrayND,
+    comment: str = "",
     field: _Field | None = None,
     precision: int | None = None,
-    symmetry: _Symmetry | Literal["AUTO"] = "AUTO",
+    symmetry: _Symmetry | None = None,
 ) -> None: ...
 
 #
-def mminfo(source: FileName) -> tuple[int, int, int, _Format, _Field, _Symmetry]: ...
+def mminfo(source: FileLike[bytes]) -> _Info: ...
