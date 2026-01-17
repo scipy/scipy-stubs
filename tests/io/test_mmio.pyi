47712ce57b1709623fc00fc312164475d743962b
jorenham <jhammudoglu@gmail.com>
2026-01-17 21:04:21 +0100

    ✅ `io`: test `mminfo`

--- tests/io/test_mmio.pyi
+++ tests/io/test_mmio.pyi
@@ -1,9 +1,15 @@
 from typing import Any, Literal, assert_type
 
+import numpy as np
 import optype.numpy as onp
 
-from scipy.io import mminfo, mmread
+from scipy.io import mminfo, mmread, mmwrite
 from scipy.sparse import coo_array, coo_matrix
+
+###
+
+_arr_f64_2d: onp.Array2D[np.float64]
+_coo_f64_2d: coo_array[np.float64] | coo_matrix[np.float64]
 
 ###
 
@@ -24,3 +30,7 @@ assert_type(
 assert_type(mmread("file.mtx"), onp.Array2D | coo_matrix)
 assert_type(mmread("file.mtx", spmatrix=True), onp.Array2D | coo_matrix)
 assert_type(mmread("file.mtx", spmatrix=False), onp.Array2D | coo_array[Any, tuple[int, int]])
+
+# mmwrite
+assert_type(mmwrite("file_out.mtx", _arr_f64_2d), None)
+assert_type(mmwrite("file_out.mtx", _coo_f64_2d), None)
