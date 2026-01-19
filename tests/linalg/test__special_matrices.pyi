ec6e590e2cd63101fff4cc751e6dddd6bac84043
jorenham <jhammudoglu@gmail.com>
2026-01-19 23:53:59 +0100

    ✅ `linalg`: test `fiedler`

--- tests/linalg/test__special_matrices.pyi
+++ tests/linalg/test__special_matrices.pyi
@@ -3,7 +3,7 @@ from typing import assert_type
 import numpy as np
 import optype.numpy as onp
 
-from scipy.linalg import circulant, companion, convolution_matrix, hankel, toeplitz
+from scipy.linalg import circulant, companion, convolution_matrix, fiedler, hankel, toeplitz
 
 ###
 
@@ -152,6 +152,30 @@ assert_type(companion(_f32_1d), onp.Array2D[np.float32])
 assert_type(companion(_f32_2d), onp.Array3D[np.float32])
 assert_type(companion(_f32_3d), onp.ArrayND[np.float32])
 assert_type(companion(_f32_nd), onp.ArrayND[np.float32])
+
+# fiedler
+
+assert_type(fiedler(_py_i_1d), onp.Array2D[np.int_])
+assert_type(fiedler(_py_i_2d), onp.Array3D[np.int_])
+assert_type(fiedler(_py_i_3d), onp.ArrayND[np.int_])
+
+assert_type(fiedler(_py_f_1d), onp.Array2D[np.float64])
+assert_type(fiedler(_py_f_2d), onp.Array3D[np.float64])
+assert_type(fiedler(_py_f_3d), onp.ArrayND[np.float64])
+
+assert_type(fiedler(_py_c_1d), onp.Array2D[np.complex128])
+assert_type(fiedler(_py_c_2d), onp.Array3D[np.complex128])
+assert_type(fiedler(_py_c_3d), onp.ArrayND[np.complex128])
+
+assert_type(fiedler(_i8_1d), onp.Array2D[np.int8])
+assert_type(fiedler(_i8_2d), onp.Array3D[np.int8])
+assert_type(fiedler(_i8_3d), onp.ArrayND[np.int8])
+assert_type(fiedler(_i8_nd), onp.ArrayND[np.int8])
+
+assert_type(fiedler(_f32_1d), onp.Array2D[np.float32])
+assert_type(fiedler(_f32_2d), onp.Array3D[np.float32])
+assert_type(fiedler(_f32_3d), onp.ArrayND[np.float32])
+assert_type(fiedler(_f32_nd), onp.ArrayND[np.float32])
 
 # hankel
 
