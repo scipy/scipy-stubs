0fdc57ac25c0500afddaf8237065a61c36fe32c7
jorenham <jhammudoglu@gmail.com>
2026-01-19 23:07:09 +0100

    ♻️ `linalg`: avoid using broken mypy/pyright shape-typing features in the special matrix functions

--- tests/linalg/test__special_matrices.pyi
+++ tests/linalg/test__special_matrices.pyi
@@ -1,45 +1,42 @@
-from typing import Any, assert_type
+from typing import assert_type
 
 import numpy as np
 import optype.numpy as onp
 
-from scipy.linalg import hankel, toeplitz  # pyright: ignore[reportUnknownVariableType]
+from scipy.linalg import hankel, toeplitz
 
-arr_f8_1d: onp.Array1D[np.float64]
-arr_f8_2d: onp.Array2D[np.float64]
-arr_f8_nd: onp.Array[tuple[int, ...], np.float64]
+_py_i_1d: list[int]
+_py_i_2d: list[list[int]]
+_py_i_3d: list[list[list[int]]]
+_py_f_1d: list[float]
+_py_c_1d: list[complex]
+
+_f64_1d: onp.Array1D[np.float64]
+_f64_2d: onp.Array2D[np.float64]
+_f64_nd: onp.ArrayND[np.float64]
 
 ###
+
 # toeplitz
-# > 1d overloads
-assert_type(toeplitz([0]), onp.Array2D[np.int_])
-assert_type(toeplitz([0.0]), onp.Array2D[np.float64])
-assert_type(toeplitz([0j]), onp.Array2D[np.complex128])
-assert_type(toeplitz(arr_f8_1d), onp.Array2D[np.float64])
-assert_type(toeplitz(arr_f8_nd), onp.Array2D[np.float64])
-assert_type(toeplitz(arr_f8_1d, arr_f8_1d), onp.Array2D[np.float64])
-assert_type(toeplitz(arr_f8_1d, arr_f8_nd), onp.Array2D[np.float64])
-assert_type(toeplitz(arr_f8_nd, arr_f8_nd), onp.Array2D[np.float64])
-# > >1d overloads
-# https://github.com/python/mypy/issues/19109
-# https://github.com/python/mypy/issues/20354
-# https://github.com/microsoft/pyright/issues/11127
-assert_type(toeplitz([[0], [1]]), onp.Array[tuple[int, int, int, tuple[Any, ...]], np.int_])  # type: ignore[assert-type, type-var]  # pyright: ignore[reportInvalidTypeForm]
-assert_type(toeplitz([[[0], [1]], [[2], [3]]]), onp.Array[tuple[int, int, int, tuple[Any, ...]], np.int_])  # type: ignore[assert-type, type-var]  # pyright: ignore[reportInvalidTypeForm]
-###
+assert_type(toeplitz(_py_i_1d), onp.Array2D[np.int_])
+assert_type(toeplitz(_py_f_1d), onp.Array2D[np.float64])
+assert_type(toeplitz(_py_c_1d), onp.Array2D[np.complex128])
+assert_type(toeplitz(_f64_1d), onp.Array2D[np.float64])
+assert_type(toeplitz(_f64_nd), onp.Array2D[np.float64])
+assert_type(toeplitz(_f64_1d, _f64_1d), onp.Array2D[np.float64])
+assert_type(toeplitz(_f64_1d, _f64_nd), onp.Array2D[np.float64])
+assert_type(toeplitz(_f64_nd, _f64_nd), onp.Array2D[np.float64])
+assert_type(toeplitz(_py_i_2d), onp.ArrayND[np.int_])
+assert_type(toeplitz(_py_i_3d), onp.ArrayND[np.int_])
 
-###
 # hankel
-# > non-deprecated overloads
 assert_type(hankel([0]), onp.Array2D[np.int_])
 assert_type(hankel([0.0]), onp.Array2D[np.float64])
 assert_type(hankel([0j]), onp.Array2D[np.complex128])
-assert_type(hankel(arr_f8_1d), onp.Array2D[np.float64])
-assert_type(hankel(arr_f8_nd), onp.Array2D[np.float64])
-assert_type(hankel(arr_f8_1d, arr_f8_1d), onp.Array2D[np.float64])
-assert_type(hankel(arr_f8_1d, arr_f8_nd), onp.Array2D[np.float64])
-assert_type(hankel(arr_f8_nd, arr_f8_nd), onp.Array2D[np.float64])
-# > deprecated (raveled input)
-assert_type(hankel([[0], [1]]), onp.Array2D[np.int_])  # pyright: ignore[reportDeprecated]  # pyrefly: ignore[deprecated]
-assert_type(hankel([[[0], [1]], [[2], [3]]]), onp.Array2D[np.int_])  # pyright: ignore[reportDeprecated]  # pyrefly: ignore[deprecated]
-###
+assert_type(hankel(_f64_1d), onp.Array2D[np.float64])
+assert_type(hankel(_f64_nd), onp.Array2D[np.float64])
+assert_type(hankel(_f64_1d, _f64_1d), onp.Array2D[np.float64])
+assert_type(hankel(_f64_1d, _f64_nd), onp.Array2D[np.float64])
+assert_type(hankel(_f64_nd, _f64_nd), onp.Array2D[np.float64])
+assert_type(hankel(_py_i_2d), onp.Array2D[np.int_])  # pyright: ignore[reportDeprecated]  # pyrefly: ignore[deprecated]
+assert_type(hankel(_py_i_3d), onp.Array2D[np.int_])  # pyright: ignore[reportDeprecated]  # pyrefly: ignore[deprecated]
