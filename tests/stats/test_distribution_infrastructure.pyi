47df143353d4829eb4aee848e9f00787b5ced31a
jorenham <jhammudoglu@gmail.com>
2026-01-19 14:17:11 +0100

    ✅ `stats`: test `truncate`

--- tests/stats/test_distribution_infrastructure.pyi
+++ tests/stats/test_distribution_infrastructure.pyi
@@ -1,11 +1,21 @@
 # type-tests for "new" distribution infrastructure functions from `stats/_distribution_infrastructure.pyi`
 
-from typing import type_check_only
+from typing import assert_type, type_check_only
 
 import numpy as np
+import optype.numpy as onp
 
-from scipy.stats import distributions, make_distribution
-from scipy.stats._distribution_infrastructure import ContinuousDistribution
+from scipy.stats import Uniform, distributions, make_distribution
+from scipy.stats._distribution_infrastructure import ContinuousDistribution, TruncatedDistribution, truncate
+
+###
+
+_f64_1d: onp.Array1D[np.float64]
+_f64_2d: onp.Array2D[np.float64]
+
+_uniform_0d_f64: Uniform[tuple[()], np.float64]
+_uniform_1d_f64: Uniform[tuple[int], np.float64]
+_uniform_2d_f64: Uniform[tuple[int, int], np.float64]
 
 ###
 
@@ -38,3 +48,18 @@ def _assert_continuous_distribution_type(dist: type[ContinuousDistribution], /) 
 _assert_continuous_distribution_type(make_distribution(distributions.loguniform))
 _assert_continuous_distribution_type(make_distribution(_DuckRV))
 _assert_continuous_distribution_type(make_distribution(_MultiDuckRV))
+
+# truncate
+assert_type(truncate(_uniform_0d_f64), TruncatedDistribution[Uniform[tuple[()], np.float64], tuple[()]])
+assert_type(truncate(_uniform_0d_f64, 0, 1), TruncatedDistribution[Uniform[tuple[()], np.float64], tuple[()]])
+assert_type(truncate(_uniform_1d_f64), TruncatedDistribution[Uniform[tuple[int], np.float64], tuple[int]])
+assert_type(truncate(_uniform_1d_f64, 0, 1), TruncatedDistribution[Uniform[tuple[int], np.float64], tuple[int]])
+assert_type(truncate(_uniform_1d_f64, _f64_1d, _f64_1d), TruncatedDistribution[Uniform[tuple[int], np.float64], tuple[int]])
+assert_type(truncate(_uniform_2d_f64), TruncatedDistribution[Uniform[tuple[int, int], np.float64], tuple[int, int]])
+assert_type(truncate(_uniform_2d_f64, 0, 1), TruncatedDistribution[Uniform[tuple[int, int], np.float64], tuple[int, int]])
+assert_type(
+    truncate(_uniform_2d_f64, _f64_1d, _f64_1d), TruncatedDistribution[Uniform[tuple[int, int], np.float64], tuple[int, int]]
+)
+assert_type(
+    truncate(_uniform_2d_f64, _f64_2d, _f64_2d), TruncatedDistribution[Uniform[tuple[int, int], np.float64], tuple[int, int]]
+)
