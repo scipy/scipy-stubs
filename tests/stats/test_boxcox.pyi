3408241d9fe8e12527af933bc6ffe6952d585596
jorenham <jhammudoglu@gmail.com>
2026-01-19 11:27:58 +0100

    ✅ `stats`: test `boxcox_normmax`

--- tests/stats/test_boxcox.pyi
+++ tests/stats/test_boxcox.pyi
@@ -5,7 +5,7 @@ from typing import assert_type
 import numpy as np
 import optype.numpy as onp
 
-from scipy.stats import boxcox
+from scipy.stats import boxcox, boxcox_normmax
 
 ###
 
@@ -25,3 +25,13 @@ assert_type(boxcox(_i8_1d, alpha=0.1), tuple[onp.Array1D[np.float64], np.float64
 assert_type(boxcox(_f16_1d, alpha=0.1), tuple[onp.Array1D[np.float64], np.float64, tuple[float, float]])
 assert_type(boxcox(_f32_1d, alpha=0.1), tuple[onp.Array1D[np.float64], np.float64, tuple[float, float]])
 assert_type(boxcox(_f64_1d, alpha=0.1), tuple[onp.Array1D[np.float64], np.float64, tuple[float, float]])
+
+# boxcox_normmax
+assert_type(boxcox_normmax(_i8_1d), np.float64)
+assert_type(boxcox_normmax(_f16_1d), np.float64)
+assert_type(boxcox_normmax(_f32_1d), np.float64)
+assert_type(boxcox_normmax(_f64_1d), np.float64)
+assert_type(boxcox_normmax(_i8_1d, method="all"), onp.Array1D[np.float64])
+assert_type(boxcox_normmax(_f16_1d, method="all"), onp.Array1D[np.float64])
+assert_type(boxcox_normmax(_f32_1d, method="all"), onp.Array1D[np.float64])
+assert_type(boxcox_normmax(_f64_1d, method="all"), onp.Array1D[np.float64])
