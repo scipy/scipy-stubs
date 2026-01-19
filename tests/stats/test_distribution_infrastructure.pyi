4aab0db3fb6040bfa5b6cf78a0928fba83d6ded2
jorenham <jhammudoglu@gmail.com>
2026-01-19 14:21:14 +0100

    ✅ `stats`: test `order_statistic`

--- tests/stats/test_distribution_infrastructure.pyi
+++ tests/stats/test_distribution_infrastructure.pyi
@@ -1,21 +1,35 @@
 # type-tests for "new" distribution infrastructure functions from `stats/_distribution_infrastructure.pyi`
 
-from typing import assert_type, type_check_only
+from typing import TypeAlias, assert_type, type_check_only
 
 import numpy as np
 import optype.numpy as onp
 
-from scipy.stats import Uniform, distributions, make_distribution
-from scipy.stats._distribution_infrastructure import ContinuousDistribution, TruncatedDistribution, truncate
+from scipy.stats import Uniform, distributions, make_distribution, order_statistic
+from scipy.stats._distribution_infrastructure import (
+    ContinuousDistribution,
+    OrderStatisticDistribution,
+    TruncatedDistribution,
+    truncate,
+)
 
 ###
+
+_0d: TypeAlias = tuple[()]  # noqa: PYI042
+_1d: TypeAlias = tuple[int]  # noqa: PYI042
+_2d: TypeAlias = tuple[int, int]  # noqa: PYI042
+
+###
+
+_i64_1d: onp.Array1D[np.int64]
+_i64_2d: onp.Array2D[np.int64]
 
 _f64_1d: onp.Array1D[np.float64]
 _f64_2d: onp.Array2D[np.float64]
 
-_uniform_0d_f64: Uniform[tuple[()], np.float64]
-_uniform_1d_f64: Uniform[tuple[int], np.float64]
-_uniform_2d_f64: Uniform[tuple[int, int], np.float64]
+_uniform_0d_f64: Uniform[_0d, np.float64]
+_uniform_1d_f64: Uniform[_1d, np.float64]
+_uniform_2d_f64: Uniform[_2d, np.float64]
 
 ###
 
@@ -50,16 +64,20 @@ _assert_continuous_distribution_type(make_distribution(_DuckRV))
 _assert_continuous_distribution_type(make_distribution(_MultiDuckRV))
 
 # truncate
-assert_type(truncate(_uniform_0d_f64), TruncatedDistribution[Uniform[tuple[()], np.float64], tuple[()]])
-assert_type(truncate(_uniform_0d_f64, 0, 1), TruncatedDistribution[Uniform[tuple[()], np.float64], tuple[()]])
-assert_type(truncate(_uniform_1d_f64), TruncatedDistribution[Uniform[tuple[int], np.float64], tuple[int]])
-assert_type(truncate(_uniform_1d_f64, 0, 1), TruncatedDistribution[Uniform[tuple[int], np.float64], tuple[int]])
-assert_type(truncate(_uniform_1d_f64, _f64_1d, _f64_1d), TruncatedDistribution[Uniform[tuple[int], np.float64], tuple[int]])
-assert_type(truncate(_uniform_2d_f64), TruncatedDistribution[Uniform[tuple[int, int], np.float64], tuple[int, int]])
-assert_type(truncate(_uniform_2d_f64, 0, 1), TruncatedDistribution[Uniform[tuple[int, int], np.float64], tuple[int, int]])
-assert_type(
-    truncate(_uniform_2d_f64, _f64_1d, _f64_1d), TruncatedDistribution[Uniform[tuple[int, int], np.float64], tuple[int, int]]
-)
-assert_type(
-    truncate(_uniform_2d_f64, _f64_2d, _f64_2d), TruncatedDistribution[Uniform[tuple[int, int], np.float64], tuple[int, int]]
-)
+assert_type(truncate(_uniform_0d_f64), TruncatedDistribution[Uniform[_0d, np.float64], _0d])
+assert_type(truncate(_uniform_0d_f64, 0, 1), TruncatedDistribution[Uniform[_0d, np.float64], _0d])
+assert_type(truncate(_uniform_1d_f64), TruncatedDistribution[Uniform[_1d, np.float64], _1d])
+assert_type(truncate(_uniform_1d_f64, 0, 1), TruncatedDistribution[Uniform[_1d, np.float64], _1d])
+assert_type(truncate(_uniform_1d_f64, _f64_1d, _f64_1d), TruncatedDistribution[Uniform[_1d, np.float64], _1d])
+assert_type(truncate(_uniform_2d_f64), TruncatedDistribution[Uniform[_2d, np.float64], _2d])
+assert_type(truncate(_uniform_2d_f64, 0, 1), TruncatedDistribution[Uniform[_2d, np.float64], _2d])
+assert_type(truncate(_uniform_2d_f64, _f64_1d, _f64_1d), TruncatedDistribution[Uniform[_2d, np.float64], _2d])
+assert_type(truncate(_uniform_2d_f64, _f64_2d, _f64_2d), TruncatedDistribution[Uniform[_2d, np.float64], _2d])
+
+# order_statistic
+assert_type(order_statistic(_uniform_0d_f64, r=0, n=1), OrderStatisticDistribution[Uniform[_0d, np.float64], _0d])
+assert_type(order_statistic(_uniform_1d_f64, r=0, n=1), OrderStatisticDistribution[Uniform[_1d, np.float64], _1d])
+assert_type(order_statistic(_uniform_1d_f64, r=_i64_1d, n=_i64_1d), OrderStatisticDistribution[Uniform[_1d, np.float64], _1d])
+assert_type(order_statistic(_uniform_2d_f64, r=0, n=1), OrderStatisticDistribution[Uniform[_2d, np.float64], _2d])
+assert_type(order_statistic(_uniform_2d_f64, r=_i64_1d, n=_i64_1d), OrderStatisticDistribution[Uniform[_2d, np.float64], _2d])
+assert_type(order_statistic(_uniform_2d_f64, r=_i64_2d, n=_i64_2d), OrderStatisticDistribution[Uniform[_2d, np.float64], _2d])
