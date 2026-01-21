a17a7120f72e159381ac4b0afebb2e682cc13706
jorenham <jhammudoglu@gmail.com>
2026-01-21 15:48:13 +0100

    ♻️ `signal._spectral_py` test clean up

--- tests/signal/test_spectral.pyi
+++ tests/signal/test_spectral.pyi
@@ -1,46 +1,54 @@
+# type-tests for `signal/_spectral_py.pyi`
+
 from typing import Literal, TypeAlias, assert_type
 
 import numpy as np
 import optype.numpy as onp
+import optype.numpy.compat as npc
 
 from scipy.signal import istft, spectrogram
 
 ###
 
-_Float1D: TypeAlias = onp.Array1D[np.float64]
-_FloatND: TypeAlias = onp.ArrayND[np.float64]
-_ComplexND: TypeAlias = onp.ArrayND[np.complex128]
+_F64_1D: TypeAlias = onp.Array1D[np.float64]
+_F64_ND: TypeAlias = onp.ArrayND[np.float64]
+_C128_ND: TypeAlias = onp.ArrayND[np.complex128]
 
 ###
 
-_array_f8_1d: onp.Array[tuple[Literal[256]], np.float64]
-_array_c16_1d: onp.Array[tuple[Literal[256]], np.complex128]
-_spectrogram_mode_real: Literal["psd", "magnitude", "angle", "phase"]
+_i64_1d: onp.Array1D[np.int64]
+_f32_1d: onp.Array1D[np.float32]
+_f64_1d: onp.Array1D[np.float64]
+_f80_1d: onp.Array1D[npc.floating80]
+_c64_1d: onp.Array1D[np.complex64]
+_c128_1d: onp.Array1D[np.complex128]
+_c160_1d: onp.Array1D[npc.complexfloating160]
+
+_mode_real: Literal["psd", "magnitude", "angle", "phase"]
 
 ###
+
 # spectrogram
 
-assert_type(spectrogram(_array_f8_1d), tuple[_Float1D, _Float1D, _FloatND])
-assert_type(spectrogram(_array_f8_1d, mode=_spectrogram_mode_real), tuple[_Float1D, _Float1D, _FloatND])
-assert_type(spectrogram(_array_f8_1d, mode="complex"), tuple[_Float1D, _Float1D, _ComplexND])
+assert_type(spectrogram(_f64_1d), tuple[_F64_1D, _F64_1D, _F64_ND])
+assert_type(spectrogram(_f64_1d, mode=_mode_real), tuple[_F64_1D, _F64_1D, _F64_ND])
+assert_type(spectrogram(_f64_1d, mode="complex"), tuple[_F64_1D, _F64_1D, _C128_ND])
 assert_type(
-    spectrogram(_array_f8_1d, 1.0, ("tukey", 2.5), None, None, None, "constant", True, "density", -1, mode="complex"),
-    tuple[_Float1D, _Float1D, _ComplexND],
+    spectrogram(_f64_1d, 1.0, ("tukey", 2.5), None, None, None, "constant", True, "density", -1, mode="complex"),
+    tuple[_F64_1D, _F64_1D, _C128_ND],
 )
 
-###
 # isft
 
-assert_type(istft(_array_c16_1d), tuple[_Float1D, _FloatND])
-assert_type(istft(_array_c16_1d, input_onesided=True), tuple[_Float1D, _FloatND])
-assert_type(istft(_array_c16_1d, 1.0, "hann", 256, 128, 256, input_onesided=False), tuple[_Float1D, _ComplexND])
+assert_type(istft(_c128_1d), tuple[_F64_1D, _F64_ND])
+assert_type(istft(_c128_1d, input_onesided=True), tuple[_F64_1D, _F64_ND])
+assert_type(istft(_c128_1d, 1.0, "hann", 256, 128, 256, input_onesided=False), tuple[_F64_1D, _C128_ND])
 assert_type(
-    istft(_array_c16_1d, input_onesided=False, fs=1.0, window="hann", nperseg=256, noverlap=128, nfft=256),
-    tuple[_Float1D, _ComplexND],
+    istft(_c128_1d, input_onesided=False, fs=1.0, window="hann", nperseg=256, noverlap=128, nfft=256), tuple[_F64_1D, _C128_ND]
 )
 assert_type(
     istft(
-        _array_c16_1d,
+        _c128_1d,
         fs=2.0,
         window=("tukey", 0.25),
         nperseg=256,
@@ -52,11 +60,11 @@ assert_type(
         freq_axis=0,
         scaling="spectrum",
     ),
-    tuple[_Float1D, _FloatND],
+    tuple[_F64_1D, _F64_ND],
 )
 assert_type(
     istft(
-        _array_c16_1d,
+        _c128_1d,
         fs=2.0,
         window=("tukey", 0.25),
         nperseg=256,
@@ -68,5 +76,5 @@ assert_type(
         freq_axis=1,
         scaling="spectrum",
     ),
-    tuple[_Float1D, _ComplexND],
+    tuple[_F64_1D, _C128_ND],
 )
