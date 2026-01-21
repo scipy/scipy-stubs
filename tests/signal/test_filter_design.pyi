1a4be81266ff480ef7cd1f69386c8f6a5d8f7c63
jorenham <jhammudoglu@gmail.com>
2026-01-21 19:56:37 +0100

    ✅ `signal`: test `buttap`

--- tests/signal/test_filter_design.pyi
+++ tests/signal/test_filter_design.pyi
@@ -1,12 +1,12 @@
 # type-tests for `signal/_max_len_seq.pyi`
 
-from typing import assert_type
+from typing import Any, Literal, assert_type
 
 import numpy as np
 import optype.numpy as onp
 import optype.numpy.compat as npc
 
-from scipy.signal import butter, buttord, cheb1ord, cheb2ord, ellipord
+from scipy.signal import buttap, butter, buttord, cheb1ord, cheb2ord, ellipord
 
 ###
 
@@ -48,3 +48,8 @@ assert_type(cheb2ord(_f64_1d, 0.3, 3, 40), tuple[int, onp.Array1D[np.float64]])
 assert_type(ellipord(0.2, 0.3, 3, 40), tuple[int, np.float64])
 assert_type(ellipord(0.2, _f64_1d, 3, 40), tuple[int, np.float64])
 assert_type(ellipord(_f64_1d, 0.3, 3, 40), tuple[int, onp.Array1D[np.float64]])
+
+# buttap
+
+assert_type(buttap(4), tuple[onp.Array1D[np.float64], onp.Array1D[np.complex128], Literal[1]])
+assert_type(buttap(4, xp=np), tuple[Any, Any, Literal[1]])
