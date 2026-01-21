5ca70f6945cf40b98080de92593767ffc9ceedc4
jorenham <jhammudoglu@gmail.com>
2026-01-21 12:15:33 +0100

    ✅ `signal`: test `lfiltic`

--- tests/signal/test_signaltools.pyi
+++ tests/signal/test_signaltools.pyi
@@ -5,7 +5,7 @@ from typing import assert_type
 import numpy as np
 import optype.numpy as onp
 
-from scipy.signal import lfilter, lfilter_zi
+from scipy.signal import lfilter, lfilter_zi, lfiltic
 
 ###
 
@@ -33,6 +33,18 @@ assert_type(lfilter_zi(_f32_1d, _c64_1d), onp.Array1D[np.complex64])
 assert_type(lfilter_zi(_c64_1d, _c64_1d), onp.Array1D[np.complex64])
 assert_type(lfilter_zi(_c64_1d, _c128_1d), onp.Array1D[np.complex128])
 assert_type(lfilter_zi(_c128_1d, _c128_1d), onp.Array1D[np.complex128])
+
+# lfiltic
+assert_type(lfiltic(_py_i_1d, _py_i_1d, _py_i_1d), onp.Array1D[np.float64])
+assert_type(lfiltic(_py_f_1d, _py_f_1d, _py_f_1d), onp.Array1D[np.float64])
+assert_type(lfiltic(_i16_1d, _i16_1d, _i16_1d), onp.Array1D[np.float64])
+assert_type(lfiltic(_f32_1d, _f32_1d, _f32_1d), onp.Array1D[np.float32])
+assert_type(lfiltic(_f32_1d, _f32_1d, _f64_1d), onp.Array1D[np.float64])
+assert_type(lfiltic(_f64_1d, _f64_1d, _f64_1d), onp.Array1D[np.float64])
+assert_type(lfiltic(_f32_1d, _f32_1d, _c64_1d), onp.Array1D[np.complex64])
+assert_type(lfiltic(_c64_1d, _c64_1d, _c64_1d), onp.Array1D[np.complex64])
+assert_type(lfiltic(_c64_1d, _c64_1d, _c128_1d), onp.Array1D[np.complex128])
+assert_type(lfiltic(_c128_1d, _c128_1d, _c128_1d), onp.Array1D[np.complex128])
 
 # lfilter
 assert_type(lfilter(_py_i_1d, _py_i_1d, _py_i_1d), onp.ArrayND[np.float64])
