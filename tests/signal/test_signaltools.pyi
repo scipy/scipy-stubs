0de5cba4c1bbafb7db10d590e78e38202eac5422
jorenham <jhammudoglu@gmail.com>
2026-01-21 12:14:47 +0100

    ✅ `signal`: test `lfilter_zi`

--- tests/signal/test_signaltools.pyi
+++ tests/signal/test_signaltools.pyi
@@ -5,7 +5,7 @@ from typing import assert_type
 import numpy as np
 import optype.numpy as onp
 
-from scipy.signal import lfilter
+from scipy.signal import lfilter, lfilter_zi
 
 ###
 
@@ -21,12 +21,23 @@ _c128_1d: onp.Array1D[np.complex128]
 
 ###
 
-# lfilter
+# lfilter_zi
+assert_type(lfilter_zi(_py_i_1d, _py_i_1d), onp.Array1D[np.float64])
+assert_type(lfilter_zi(_py_f_1d, _py_f_1d), onp.Array1D[np.float64])
+assert_type(lfilter_zi(_i16_1d, _i16_1d), onp.Array1D[np.float64])
+assert_type(lfilter_zi(_i16_1d, _f32_1d), onp.Array1D[np.float32])
+assert_type(lfilter_zi(_f32_1d, _f32_1d), onp.Array1D[np.float32])
+assert_type(lfilter_zi(_f32_1d, _f64_1d), onp.Array1D[np.float64])
+assert_type(lfilter_zi(_f64_1d, _f64_1d), onp.Array1D[np.float64])
+assert_type(lfilter_zi(_f32_1d, _c64_1d), onp.Array1D[np.complex64])
+assert_type(lfilter_zi(_c64_1d, _c64_1d), onp.Array1D[np.complex64])
+assert_type(lfilter_zi(_c64_1d, _c128_1d), onp.Array1D[np.complex128])
+assert_type(lfilter_zi(_c128_1d, _c128_1d), onp.Array1D[np.complex128])
 
+# lfilter
 assert_type(lfilter(_py_i_1d, _py_i_1d, _py_i_1d), onp.ArrayND[np.float64])
 assert_type(lfilter(_py_f_1d, _py_f_1d, _py_f_1d), onp.ArrayND[np.float64])
 assert_type(lfilter(_i16_1d, _i16_1d, _i16_1d), onp.ArrayND[np.float64])
-assert_type(lfilter(_i16_1d, _i16_1d, _f32_1d), onp.ArrayND[np.float64])
 assert_type(lfilter(_f32_1d, _f32_1d, _f32_1d), onp.ArrayND[np.float32])
 assert_type(lfilter(_f32_1d, _f32_1d, _f64_1d), onp.ArrayND[np.float64])
 assert_type(lfilter(_f64_1d, _f64_1d, _f64_1d), onp.ArrayND[np.float64])
