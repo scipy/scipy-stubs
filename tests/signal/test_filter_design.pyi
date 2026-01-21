e96a586ffdf360d8aa5d98439cb57adea6811c10
jorenham <jhammudoglu@gmail.com>
2026-01-21 20:03:35 +0100

    ✅ `signal`: test `cheby{1,2}`

--- tests/signal/test_filter_design.pyi
+++ tests/signal/test_filter_design.pyi
@@ -6,7 +6,20 @@ import numpy as np
 import optype.numpy as onp
 import optype.numpy.compat as npc
 
-from scipy.signal import besselap, buttap, butter, buttord, cheb1ap, cheb1ord, cheb2ap, cheb2ord, ellipap, ellipord
+from scipy.signal import (
+    besselap,
+    buttap,
+    butter,
+    buttord,
+    cheb1ap,
+    cheb1ord,
+    cheb2ap,
+    cheb2ord,
+    cheby1,
+    cheby2,
+    ellipap,
+    ellipord,
+)
 
 ###
 
@@ -16,60 +29,66 @@ _f80_1d: onp.Array1D[npc.floating80]
 ###
 
 # butter
-
 assert_type(butter(8, 0.1), tuple[onp.Array1D[np.float64], onp.Array1D[np.float64]])
 assert_type(butter(8, 0.1, output="ba"), tuple[onp.Array1D[np.float64], onp.Array1D[np.float64]])
 assert_type(butter(8, 0.1, output="zpk"), tuple[onp.Array1D[np.float64], onp.Array1D[np.complex128], float])
 assert_type(butter(8, 0.1, output="sos"), onp.Array2D[np.float64])
 
-# buttord
+# cheby1
+assert_type(cheby1(8, 3, 0.1), tuple[onp.Array1D[np.float64], onp.Array1D[np.float64]])
+assert_type(cheby1(8, 3, 0.1, output="ba"), tuple[onp.Array1D[np.float64], onp.Array1D[np.float64]])
+assert_type(cheby1(8, 3, 0.1, output="zpk"), tuple[onp.Array1D[np.complex128], onp.Array1D[np.complex128], np.float64])
+assert_type(cheby1(8, 3, 0.1, output="sos"), onp.Array2D[np.float64])
 
+# cheby2
+assert_type(cheby2(8, 3, 0.1), tuple[onp.Array1D[np.float64], onp.Array1D[np.float64]])
+assert_type(cheby2(8, 3, 0.1, output="ba"), tuple[onp.Array1D[np.float64], onp.Array1D[np.float64]])
+assert_type(cheby2(8, 3, 0.1, output="zpk"), tuple[onp.Array1D[np.complex128], onp.Array1D[np.complex128], np.float64])
+assert_type(cheby2(8, 3, 0.1, output="sos"), onp.Array2D[np.float64])
+
+###
+
+# buttord
 assert_type(buttord(0.2, 0.3, 3, 40), tuple[int, np.float64])
 assert_type(buttord(0.2, _f64_1d, 3, 40), tuple[int, np.float64])
 assert_type(buttord(0.2, _f80_1d, 3, 40), tuple[int, np.longdouble])
 assert_type(buttord(_f64_1d, 0.3, 3, 40), tuple[int, onp.Array1D[np.float64]])
 
 # cheb1ord
-
 assert_type(cheb1ord(0.2, 0.3, 3, 40), tuple[int, np.float64])
 assert_type(cheb1ord(0.2, _f64_1d, 3, 40), tuple[int, np.float64])
 assert_type(cheb1ord(0.2, _f80_1d, 3, 40), tuple[int, np.longdouble])
 assert_type(cheb1ord(_f64_1d, 0.3, 3, 40), tuple[int, onp.Array1D[np.float64]])
 
 # cheb2ord
-
 assert_type(cheb2ord(0.2, 0.3, 3, 40), tuple[int, np.float64])
 assert_type(cheb2ord(0.2, _f64_1d, 3, 40), tuple[int, np.float64])
 assert_type(cheb2ord(0.2, _f80_1d, 3, 40), tuple[int, np.longdouble])
 assert_type(cheb2ord(_f64_1d, 0.3, 3, 40), tuple[int, onp.Array1D[np.float64]])
 
 # ellipord
-
 assert_type(ellipord(0.2, 0.3, 3, 40), tuple[int, np.float64])
 assert_type(ellipord(0.2, _f64_1d, 3, 40), tuple[int, np.float64])
 assert_type(ellipord(_f64_1d, 0.3, 3, 40), tuple[int, onp.Array1D[np.float64]])
 
-# buttap
+###
 
+# buttap
 assert_type(buttap(4), tuple[onp.Array1D[np.float64], onp.Array1D[np.complex128], Literal[1]])
 assert_type(buttap(4, xp=np), tuple[Any, Any, Literal[1]])
 
 # cheb1ap
-
 assert_type(cheb1ap(4, 0.1), tuple[onp.Array1D[np.float64], onp.Array1D[np.complex128], float])
 assert_type(cheb1ap(4, 0.1, xp=np), tuple[Any, Any, float])
 
 # cheb2ap
-
 assert_type(cheb2ap(4, 0.1), tuple[onp.Array1D[np.complex128], onp.Array1D[np.complex128], float])
 assert_type(cheb2ap(4, 0.1, xp=np), tuple[Any, Any, float])
 
 # ellipap
-
 assert_type(ellipap(4, 0.1, 0.2), tuple[onp.Array1D[np.complex128], onp.Array1D[np.complex128], float])
 assert_type(ellipap(4, 0.1, 0.2, xp=np), tuple[Any, Any, float])
 
 # besselap
-
 assert_type(besselap(4), tuple[onp.Array1D[np.float64], onp.Array1D[np.complex128], float])
 assert_type(besselap(4, xp=np), tuple[Any, Any, float])
