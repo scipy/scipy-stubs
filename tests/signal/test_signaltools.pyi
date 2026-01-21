e8f352f9d4e365fb5b0ecf6eb2405a7a58ad7498
jorenham <jhammudoglu@gmail.com>
2026-01-21 13:18:35 +0100

    ✅ `signal`: test `convolve` and `correlate`

--- tests/signal/test_signaltools.pyi
+++ tests/signal/test_signaltools.pyi
@@ -5,14 +5,16 @@ from typing import Literal, assert_type
 import numpy as np
 import optype.numpy as onp
 
-from scipy.signal import choose_conv_method, filtfilt, lfilter, lfilter_zi, lfiltic, sosfiltfilt
+from scipy.signal import choose_conv_method, convolve, correlate, filtfilt, lfilter, lfilter_zi, lfiltic, sosfiltfilt
 
 ###
 
+_py_b_1d: list[bool]
 _py_i_1d: list[int]
 _py_f_1d: list[float]
 _py_c_1d: list[complex]
 _i16_1d: onp.Array1D[np.int16]
+_i64_1d: onp.Array1D[np.int64]
 _f32_1d: onp.Array1D[np.float32]
 _f64_1d: onp.Array1D[np.float64]
 _c64_1d: onp.Array1D[np.complex64]
@@ -67,7 +69,30 @@ assert_type(choose_conv_method(_f64_1d, _f64_1d, measure=True)[1]["fft"], float)
 assert_type(choose_conv_method(_c64_1d, _c64_1d, measure=True)[1]["fft"], float)
 assert_type(choose_conv_method(_c128_1d, _c128_1d, measure=True)[1]["fft"], float)
 
+# convolve (same as correlate)
+
+assert_type(convolve(_py_b_1d, _py_b_1d), onp.ArrayND[np.bool_])
+assert_type(convolve(_py_i_1d, _py_i_1d), onp.ArrayND[np.int64])
+assert_type(convolve(_py_f_1d, _py_f_1d), onp.ArrayND[np.float64])
+assert_type(convolve(_i64_1d, _i64_1d), onp.Array1D[np.int64])
+assert_type(convolve(_f32_1d, _f32_1d), onp.Array1D[np.float32])
+assert_type(convolve(_f64_1d, _f64_1d), onp.Array1D[np.float64])
+assert_type(convolve(_c64_1d, _c64_1d), onp.Array1D[np.complex64])
+assert_type(convolve(_c128_1d, _c128_1d), onp.Array1D[np.complex128])
+
+# correlate (same as convolve)
+
+assert_type(correlate(_py_b_1d, _py_b_1d), onp.ArrayND[np.bool_])
+assert_type(correlate(_py_i_1d, _py_i_1d), onp.ArrayND[np.int64])
+assert_type(correlate(_py_f_1d, _py_f_1d), onp.ArrayND[np.float64])
+assert_type(correlate(_i64_1d, _i64_1d), onp.Array1D[np.int64])
+assert_type(correlate(_f32_1d, _f32_1d), onp.Array1D[np.float32])
+assert_type(correlate(_f64_1d, _f64_1d), onp.Array1D[np.float64])
+assert_type(correlate(_c64_1d, _c64_1d), onp.Array1D[np.complex64])
+assert_type(correlate(_c128_1d, _c128_1d), onp.Array1D[np.complex128])
+
 # lfilter_zi
+
 assert_type(lfilter_zi(_py_i_1d, _py_i_1d), onp.Array1D[np.float64])
 assert_type(lfilter_zi(_py_f_1d, _py_f_1d), onp.Array1D[np.float64])
 assert_type(lfilter_zi(_i16_1d, _i16_1d), onp.Array1D[np.float64])
@@ -81,6 +106,7 @@ assert_type(lfilter_zi(_c64_1d, _c128_1d), onp.Array1D[np.complex128])
 assert_type(lfilter_zi(_c128_1d, _c128_1d), onp.Array1D[np.complex128])
 
 # lfiltic
+
 assert_type(lfiltic(_py_i_1d, _py_i_1d, _py_i_1d), onp.Array1D[np.float64])
 assert_type(lfiltic(_py_f_1d, _py_f_1d, _py_f_1d), onp.Array1D[np.float64])
 assert_type(lfiltic(_i16_1d, _i16_1d, _i16_1d), onp.Array1D[np.float64])
@@ -93,6 +119,7 @@ assert_type(lfiltic(_c64_1d, _c64_1d, _c128_1d), onp.Array1D[np.complex128])
 assert_type(lfiltic(_c128_1d, _c128_1d, _c128_1d), onp.Array1D[np.complex128])
 
 # lfilter
+
 assert_type(lfilter(_py_i_1d, _py_i_1d, _py_i_1d), onp.ArrayND[np.float64])
 assert_type(lfilter(_py_f_1d, _py_f_1d, _py_f_1d), onp.ArrayND[np.float64])
 assert_type(lfilter(_i16_1d, _i16_1d, _i16_1d), onp.ArrayND[np.float64])
@@ -105,6 +132,7 @@ assert_type(lfilter(_c64_1d, _c64_1d, _c128_1d), onp.ArrayND[np.complex128])
 assert_type(lfilter(_c128_1d, _c128_1d, _c128_1d), onp.ArrayND[np.complex128])
 
 # filtfilt
+
 assert_type(filtfilt(_py_i_1d, _py_i_1d, _py_i_1d), onp.ArrayND[np.float64])
 assert_type(filtfilt(_py_f_1d, _py_f_1d, _py_f_1d), onp.ArrayND[np.float64])
 assert_type(filtfilt(_i16_1d, _i16_1d, _i16_1d), onp.ArrayND[np.float64])
@@ -117,6 +145,7 @@ assert_type(filtfilt(_c64_1d, _c64_1d, _c128_1d), onp.ArrayND[np.complex128])
 assert_type(filtfilt(_c128_1d, _c128_1d, _c128_1d), onp.ArrayND[np.complex128])
 
 # sosfiltfilt
+
 assert_type(sosfiltfilt(_py_i_2d, _py_i_1d), onp.ArrayND[np.float64])
 assert_type(sosfiltfilt(_py_f_2d, _py_f_1d), onp.ArrayND[np.float64])
 assert_type(sosfiltfilt(_i16_2d, _i16_1d), onp.ArrayND[np.float64])
