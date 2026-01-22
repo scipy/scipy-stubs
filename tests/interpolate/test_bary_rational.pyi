ca07342e8b653bdc88d7836b2adafe32d0ca5860
jorenham <jhammudoglu@gmail.com>
2026-01-22 12:36:32 +0100

    ✨ `interpolate`: test `FloaterHormannInterpolator`

--- tests/interpolate/test_bary_rational.pyi
+++ tests/interpolate/test_bary_rational.pyi
@@ -6,7 +6,7 @@ import numpy as np
 import optype.numpy as onp
 import optype.numpy.compat as npc
 
-from scipy.interpolate import AAA
+from scipy.interpolate import AAA, FloaterHormannInterpolator
 
 ###
 
@@ -44,3 +44,26 @@ assert_type(_aaa.support_values, onp.Array1D[np.float32])
 assert_type(_aaa.residues(), onp.Array1D[np.float32])
 assert_type(_aaa.poles(), onp.Array1D[np.complex64])
 assert_type(_aaa.roots(), onp.Array1D[np.complex64])
+
+# FloaterHormannInterpolator
+
+_fhi: FloaterHormannInterpolator[np.float32, tuple[int, int]]
+
+assert_type(FloaterHormannInterpolator(_i64_1d, _i64_1d), FloaterHormannInterpolator[np.float64, tuple[int]])
+assert_type(FloaterHormannInterpolator(_f16_1d, _f16_1d), FloaterHormannInterpolator[np.float64, tuple[int]])
+assert_type(FloaterHormannInterpolator(_f32_1d, _f32_1d), FloaterHormannInterpolator[np.float32, tuple[int]])
+assert_type(FloaterHormannInterpolator(_f64_1d, _f64_1d), FloaterHormannInterpolator[np.float64, tuple[int]])
+# pyrefly: ignore[assert-type]
+assert_type(FloaterHormannInterpolator(_f80_1d, _f80_1d), FloaterHormannInterpolator[npc.floating80, tuple[int]])
+assert_type(FloaterHormannInterpolator(_c64_1d, _c64_1d), FloaterHormannInterpolator[np.complex64, tuple[int]])
+assert_type(FloaterHormannInterpolator(_c128_1d, _c128_1d), FloaterHormannInterpolator[np.complex128, tuple[int]])
+# pyrefly: ignore[assert-type]
+assert_type(FloaterHormannInterpolator(_c160_1d, _c160_1d), FloaterHormannInterpolator[npc.complexfloating160, tuple[int]])
+
+assert_type(_fhi(1), onp.Array2D[np.float32])
+assert_type(_fhi(1.0), onp.Array2D[np.float64])
+assert_type(_fhi(1j), onp.Array2D[np.complex128])
+
+assert_type(_fhi.residues(), onp.Array2D[np.float32])
+assert_type(_fhi.poles(), onp.Array2D[np.complex64])
+assert_type(_fhi.roots(), onp.Array2D[np.complex64])
