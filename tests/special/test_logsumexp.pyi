2f52821af841aebdb9621197e44ba3f3097143ee
jorenham <jhammudoglu@gmail.com>
2026-01-24 23:28:38 +0100

    ✅ `special`: test `log_softmax`

--- tests/special/test_logsumexp.pyi
+++ tests/special/test_logsumexp.pyi
@@ -4,7 +4,7 @@ import numpy as np
 import optype.numpy as onp
 import optype.numpy.compat as npc
 
-from scipy.special import logsumexp, softmax
+from scipy.special import log_softmax, logsumexp, softmax
 
 py_f_0d: float
 py_c_0d: complex
@@ -73,8 +73,7 @@ assert_type(logsumexp(f16_1d, axis=0, return_sign=True), tuple[onp.ArrayND[np.fl
 assert_type(logsumexp(c64_0d, axis=0, return_sign=True), tuple[onp.ArrayND[npc.floating] | Any, onp.ArrayND[np.complex64] | Any])
 assert_type(logsumexp(c64_1d, axis=0, return_sign=True), tuple[onp.ArrayND[npc.floating] | Any, onp.ArrayND[np.complex64] | Any])
 
-###
-# softmax (equiv log_softmax)
+# softmax
 
 assert_type(softmax(py_f_0d), np.float64)
 assert_type(softmax(py_c_0d), np.complex128)
@@ -85,3 +84,15 @@ assert_type(softmax(py_f_1d), onp.ArrayND[np.float64])
 assert_type(softmax(py_c_1d), onp.ArrayND[np.complex128])
 assert_type(softmax(f16_1d), np.ndarray[tuple[int], np.dtype[np.float16]])
 assert_type(softmax(c64_1d), np.ndarray[tuple[int], np.dtype[np.complex64]])
+
+# log_softmax
+
+assert_type(log_softmax(py_f_0d), np.float64)
+assert_type(log_softmax(py_c_0d), np.complex128)
+assert_type(log_softmax(f16_0d), np.float16)
+assert_type(log_softmax(c64_0d), np.complex64)
+
+assert_type(log_softmax(py_f_1d), onp.ArrayND[np.float64])
+assert_type(log_softmax(py_c_1d), onp.ArrayND[np.complex128])
+assert_type(log_softmax(f16_1d), np.ndarray[tuple[int], np.dtype[np.float16]])
+assert_type(log_softmax(c64_1d), np.ndarray[tuple[int], np.dtype[np.complex64]])
