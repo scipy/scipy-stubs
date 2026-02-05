9b1e0f3d90869ad9bd83d4a1308f169cfa53faa1
Pavadol Yamsiri <pavyamsiri@gmail.com>
2026-02-05 22:58:17 +1100

    :bug: stats: Import `TypeVar` from `typing_extensions`
    - Move `binned_statistic_result` to be keyword only
    as well.
    - Fix up naming inconsistencies

--- scipy-stubs/stats/_binned_statistic.pyi
+++ scipy-stubs/stats/_binned_statistic.pyi
@@ -1,5 +1,6 @@
 from collections.abc import Callable, Sequence
-from typing import Any, Generic, Literal, NamedTuple, TypeAlias, TypeVar, overload
+from typing import Generic, Literal, NamedTuple, TypeAlias, overload
+from typing_extensions import TypeVar
 
 import numpy as np
 import optype.numpy as onp
@@ -8,7 +9,7 @@ import optype.numpy.compat as npc
 __all__ = ["binned_statistic", "binned_statistic_2d", "binned_statistic_dd"]
 
 _Statistic: TypeAlias = Literal["mean", "std", "median", "count", "sum", "min", "max"]
-_BinNumber_co = TypeVar("_BinNumber_co", bound=tuple[Any, ...], covariant=True, default=tuple[int] | tuple[int, int])
+_ShapeT_co = TypeVar("_ShapeT_co", bound=tuple[int] | tuple[int, int], covariant=True, default=tuple[int] | tuple[int, int])
 
 class BinnedStatisticResult(NamedTuple):
     statistic: onp.Array1D[npc.inexact]
@@ -23,11 +24,11 @@ def binned_statistic(
     range: tuple[float, float] | Sequence[tuple[float, float]] | None = None,
 ) -> BinnedStatisticResult: ...
 
-class BinnedStatistic2dResult(NamedTuple, Generic[_BinNumber_co]):
+class BinnedStatistic2dResult(NamedTuple, Generic[_ShapeT_co]):
     statistic: onp.Array2D[npc.inexact]
     x_edge: onp.Array1D[np.float64]
     y_edge: onp.Array1D[np.float64]
-    binnumber: onp.Array[_BinNumber_co, np.intp]
+    binnumber: onp.Array[_ShapeT_co, np.intp]
 
 #
 @overload
@@ -52,10 +53,10 @@ def binned_statistic_2d(
     expand_binnumbers: Literal[True],
 ) -> BinnedStatistic2dResult[tuple[int, int]]: ...
 
-class BinnedStatisticddResult(NamedTuple, Generic[_BinNumber_co]):
+class BinnedStatisticddResult(NamedTuple, Generic[_ShapeT_co]):
     statistic: onp.ArrayND[npc.inexact]
     bin_edges: list[onp.Array1D[np.float64]]
-    binnumber: onp.Array[_BinNumber_co, np.intp]
+    binnumber: onp.Array[_ShapeT_co, np.intp]
 
 #
 @overload
@@ -75,7 +76,7 @@ def binned_statistic_dd(
     statistic: _Statistic | Callable[[onp.ArrayND[np.float64]], onp.ToFloat] = "mean",
     bins: onp.ToInt | onp.ToFloat1D = 10,
     range: tuple[int, int] | None = None,
-    binned_statistic_result: BinnedStatisticddResult | None = None,
     *,
     expand_binnumbers: Literal[True],
+    binned_statistic_result: BinnedStatisticddResult | None = None,
 ) -> BinnedStatisticddResult[tuple[int, int]]: ...
