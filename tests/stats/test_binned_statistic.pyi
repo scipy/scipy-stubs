9caa369715f6981a10b4711bad93e162a142c3f2
Pavadol Yamsiri <pavyamsiri@gmail.com>
2026-02-05 21:44:23 +1100

    :white_check_mark: stats: `binned_statistic2d `overload tests
    Add tests for `binned_statistic2d` overload on
    the argument `expand_binnumbers`

---
+++ tests/stats/test_binned_statistic.pyi
@@ -0,0 +1,18 @@
+# type-tests for `binned_statistic2d` from `stats/_binned_statistic.pyi`
+
+from typing import assert_type
+
+import numpy as np
+import optype.numpy as onp
+
+from scipy.stats import binned_statistic_2d
+
+###
+
+_f64_1d: onp.Array1D[np.float64]
+
+###
+
+assert_type(binned_statistic_2d(_f64_1d, _f64_1d, _f64_1d).binnumber, onp.Array1D[np.intp])
+assert_type(binned_statistic_2d(_f64_1d, _f64_1d, _f64_1d, expand_binnumbers=False).binnumber, onp.Array1D[np.intp])
+assert_type(binned_statistic_2d(_f64_1d, _f64_1d, _f64_1d, expand_binnumbers=True).binnumber, onp.Array2D[np.intp])
