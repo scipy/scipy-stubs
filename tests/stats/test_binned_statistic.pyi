bf9222b3afdddce5b3c86d9f65be6bfab756197a
Pavadol Yamsiri <pavyamsiri@gmail.com>
2026-02-05 22:05:11 +1100

    :white_check_mark: stats: `binned_statistic_dd `overload tests
    Add tests for `binned_statistic_dd` overload on
    the argument `expand_binnumbers`

--- tests/stats/test_binned_statistic.pyi
+++ tests/stats/test_binned_statistic.pyi
@@ -1,11 +1,11 @@
-# type-tests for `binned_statistic2d` from `stats/_binned_statistic.pyi`
+# type-tests for `binned_statistic_2d` and `binned_statistic_dd` from `stats/_binned_statistic.pyi`
 
 from typing import assert_type
 
 import numpy as np
 import optype.numpy as onp
 
-from scipy.stats import binned_statistic_2d
+from scipy.stats import binned_statistic_2d, binned_statistic_dd
 
 ###
 
@@ -16,3 +16,7 @@ _f64_1d: onp.Array1D[np.float64]
 assert_type(binned_statistic_2d(_f64_1d, _f64_1d, _f64_1d).binnumber, onp.Array1D[np.intp])
 assert_type(binned_statistic_2d(_f64_1d, _f64_1d, _f64_1d, expand_binnumbers=False).binnumber, onp.Array1D[np.intp])
 assert_type(binned_statistic_2d(_f64_1d, _f64_1d, _f64_1d, expand_binnumbers=True).binnumber, onp.Array2D[np.intp])
+
+assert_type(binned_statistic_dd(_f64_1d, _f64_1d).binnumber, onp.Array1D[np.intp])
+assert_type(binned_statistic_dd(_f64_1d, _f64_1d, expand_binnumbers=False).binnumber, onp.Array1D[np.intp])
+assert_type(binned_statistic_dd(_f64_1d, _f64_1d, expand_binnumbers=True).binnumber, onp.Array2D[np.intp])
