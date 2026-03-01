97f66537eb2ae0ae261ea6ebb4d8c0e97d371be9
jorenham <jhammudoglu@gmail.com>
2026-03-01 20:44:03 +0100

    ✅ `sparse.linalg`: increase type-test coverage

---
+++ tests/sparse/linalg/test__isolve.pyi
@@ -0,0 +1,32 @@
+from typing import assert_type
+
+import numpy as np
+import optype.numpy as onp
+
+from scipy.sparse import csr_array
+from scipy.sparse.linalg import bicg, bicgstab, cg, gmres, qmr
+
+a_f64: csr_array[np.float64]
+a_c128: csr_array[np.complex128]
+b_f: onp.Array1D[np.float64]
+b_c: onp.Array1D[np.complex128]
+
+# bicg
+assert_type(bicg(a_f64, b_f), tuple[onp.Array1D[np.float64], int])
+assert_type(bicg(a_c128, b_c), tuple[onp.Array1D[np.complex128], int])
+
+# bicgstab
+assert_type(bicgstab(a_f64, b_f), tuple[onp.Array1D[np.float64], int])
+assert_type(bicgstab(a_c128, b_c), tuple[onp.Array1D[np.complex128], int])
+
+# cg
+assert_type(cg(a_f64, b_f), tuple[onp.Array1D[np.float64], int])
+assert_type(cg(a_c128, b_c), tuple[onp.Array1D[np.complex128], int])
+
+# gmres
+assert_type(gmres(a_f64, b_f), tuple[onp.Array1D[np.float64], int])
+assert_type(gmres(a_c128, b_c), tuple[onp.Array1D[np.complex128], int])
+
+# qmr
+assert_type(qmr(a_f64, b_f), tuple[onp.Array1D[np.float64], int])
+assert_type(qmr(a_c128, b_c), tuple[onp.Array1D[np.complex128], int])
