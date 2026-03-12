5a864fa0af9c23981a8e9fe338bf48f204867bcb
jorenham <jhammudoglu@gmail.com>
2026-03-12 23:45:49 +0100

    🐴 `spatial`: mypy workaround for duplicate test module name

--- tests/spatial/test_procrustes.pyi
+++ tests/spatial/test_procrustes.pyi
@@ -7,12 +7,7 @@ import optype.numpy as onp
 
 from scipy.spatial import procrustes
 
-###
-
 _f64_2d: onp.Array2D[np.float64]
-
-###
-# procrustes
 
 assert_type(procrustes(_f64_2d, _f64_2d)[0], onp.Array2D[np.float64])
 assert_type(procrustes(_f64_2d, _f64_2d)[1], onp.Array2D[np.float64])
