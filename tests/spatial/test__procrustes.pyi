2c45d58a026652f43edd800f9f2a60f7c5639131
jorenham <jhammudoglu@gmail.com>
2026-03-12 23:41:44 +0100

    ✅ `spatial`: test `procrustes`

---
+++ tests/spatial/test__procrustes.pyi
@@ -0,0 +1,19 @@
+# type-tests for `spatial/_procrustes.pyi`
+
+from typing import assert_type
+
+import numpy as np
+import optype.numpy as onp
+
+from scipy.spatial import procrustes
+
+###
+
+_f64_2d: onp.Array2D[np.float64]
+
+###
+# procrustes
+
+assert_type(procrustes(_f64_2d, _f64_2d)[0], onp.Array2D[np.float64])
+assert_type(procrustes(_f64_2d, _f64_2d)[1], onp.Array2D[np.float64])
+assert_type(procrustes(_f64_2d, _f64_2d)[2], np.float64)
