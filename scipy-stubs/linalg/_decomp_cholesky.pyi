def7b5628c903f7b4e8745cfc47d53142bd7c906
jorenham <jhammudoglu@gmail.com>
2026-03-13 14:16:22 +0100

    ✅ `linalg`: improved `cho_solve` dtypes

--- scipy-stubs/linalg/_decomp_cholesky.pyi
+++ scipy-stubs/linalg/_decomp_cholesky.pyi
@@ -19,8 +19,6 @@ _as_f64: TypeAlias = npc.floating64 | npc.floating80 | npc.integer64 | npc.integ
 _as_c64: TypeAlias = np.complex64  # noqa: PYI042
 _as_c128: TypeAlias = npc.complexfloating160 | npc.complexfloating128  # noqa: PYI042
 
-_Float2D: TypeAlias = onp.Array2D[npc.floating]
-_FloatND: TypeAlias = onp.ArrayND[npc.floating]
 _Complex2D: TypeAlias = onp.Array2D[npc.inexact]
 _ComplexND: TypeAlias = onp.ArrayND[npc.inexact]
 
@@ -28,7 +26,9 @@ _Sequence2D: TypeAlias = Sequence[Sequence[_T]]
 
 ###
 
-# keep in sync with `cho_factor` and `cholesky_banded`
+# NOTE: The ignored `overload-overlap` mypy errors are false positives
+
+# keep in sync with `cholesky_banded` and `cho_factor`
 @overload  # Nd +f64
 def cholesky(  # type: ignore[overload-overlap]
     a: nptc.CanArray[_Shape2T, np.dtype[_as_f64]], lower: bool = False, overwrite_a: bool = False, check_finite: bool = True
@@ -66,6 +66,47 @@ def cholesky(
     a: onp.SequenceND[Sequence[MutableSequence[complex]]],
     lower: bool = False,
     overwrite_a: bool = False,
+    check_finite: bool = True,
+) -> onp.ArrayND[np.complex128]: ...
+
+# keep in sync with `cholesky`
+@overload  # Nd +f64
+def cholesky_banded(  # type: ignore[overload-overlap]
+    ab: nptc.CanArray[_Shape2T, np.dtype[_as_f64]], lower: bool = False, overwrite_ab: bool = False, check_finite: bool = True
+) -> onp.ArrayND[np.float64, _Shape2T]: ...
+@overload  # Nd +f32
+def cholesky_banded(
+    ab: nptc.CanArray[_Shape2T, np.dtype[_as_f32]], lower: bool = False, overwrite_ab: bool = False, check_finite: bool = True
+) -> onp.ArrayND[np.float32, _Shape2T]: ...
+@overload  # Nd +c128
+def cholesky_banded(
+    ab: nptc.CanArray[_Shape2T, np.dtype[_as_c128]], lower: bool = False, overwrite_ab: bool = False, check_finite: bool = True
+) -> onp.ArrayND[np.complex128, _Shape2T]: ...
+@overload  # Nd ~c64
+def cholesky_banded(
+    ab: nptc.CanArray[_Shape2T, np.dtype[_as_c64]], lower: bool = False, overwrite_ab: bool = False, check_finite: bool = True
+) -> onp.ArrayND[np.complex64, _Shape2T]: ...
+@overload  # Nd ~number
+def cholesky_banded(
+    ab: nptc.CanArray[_Shape2T, np.dtype[npc.number]], lower: bool = False, overwrite_ab: bool = False, check_finite: bool = True
+) -> onp.ArrayND[Any, _Shape2T]: ...
+@overload  # 2d +f64
+def cholesky_banded(
+    ab: _Sequence2D[float], lower: bool = False, overwrite_ab: bool = False, check_finite: bool = True
+) -> onp.Array2D[np.float64]: ...
+@overload  # 2d ~c128
+def cholesky_banded(
+    ab: Sequence[MutableSequence[complex]], lower: bool = False, overwrite_ab: bool = False, check_finite: bool = True
+) -> onp.Array2D[np.complex128]: ...
+@overload  # ?d +f64
+def cholesky_banded(
+    ab: onp.SequenceND[_Sequence2D[float]], lower: bool = False, overwrite_ab: bool = False, check_finite: bool = True
+) -> onp.ArrayND[np.float64]: ...
+@overload  # ?d ~c128
+def cholesky_banded(
+    ab: onp.SequenceND[Sequence[MutableSequence[complex]]],
+    lower: bool = False,
+    overwrite_ab: bool = False,
     check_finite: bool = True,
 ) -> onp.ArrayND[np.complex128]: ...
 
@@ -111,66 +152,50 @@ def cho_factor(
 ) -> tuple[onp.ArrayND[np.complex128], bool]: ...
 
 #
-@overload
+@overload  # ?d +f32, ?d +f32
+def cho_solve(  # type: ignore[overload-overlap]
+    c_and_lower: tuple[onp.ToFloat32_ND, bool], b: onp.ToFloat32_ND, overwrite_b: bool = False, check_finite: bool = True
+) -> onp.ArrayND[np.float32]: ...
+@overload  # ?d +f64\+f32, ?d +f64
 def cho_solve(
-    c_and_lower: tuple[onp.ToFloatStrict2D, bool], b: onp.ToFloatStrict1D, overwrite_b: bool = False, check_finite: bool = True
-) -> _Float2D: ...
-@overload
-def cho_solve(
-    c_and_lower: tuple[onp.ToFloatND, bool], b: onp.ToFloatND, overwrite_b: bool = False, check_finite: bool = True
-) -> _FloatND: ...
-@overload
-def cho_solve(
-    c_and_lower: tuple[onp.ToComplexStrict2D, bool],
-    b: onp.ToComplexStrict1D,
+    c_and_lower: tuple[onp.ToArrayND[float, _as_f64], bool],
+    b: onp.ToFloatND,
     overwrite_b: bool = False,
     check_finite: bool = True,
-) -> _Complex2D: ...
-@overload
-def cho_solve(
-    c_and_lower: tuple[onp.ToComplexND, bool], b: onp.ToComplexND, overwrite_b: bool = False, check_finite: bool = True
-) -> _ComplexND: ...
-
-# keep in sync with `cholesky`
-@overload  # Nd +f64
-def cholesky_banded(  # type: ignore[overload-overlap]
-    ab: nptc.CanArray[_Shape2T, np.dtype[_as_f64]], lower: bool = False, overwrite_ab: bool = False, check_finite: bool = True
-) -> onp.ArrayND[np.float64, _Shape2T]: ...
-@overload  # Nd +f32
-def cholesky_banded(
-    ab: nptc.CanArray[_Shape2T, np.dtype[_as_f32]], lower: bool = False, overwrite_ab: bool = False, check_finite: bool = True
-) -> onp.ArrayND[np.float32, _Shape2T]: ...
-@overload  # Nd +c128
-def cholesky_banded(
-    ab: nptc.CanArray[_Shape2T, np.dtype[_as_c128]], lower: bool = False, overwrite_ab: bool = False, check_finite: bool = True
-) -> onp.ArrayND[np.complex128, _Shape2T]: ...
-@overload  # Nd ~c64
-def cholesky_banded(
-    ab: nptc.CanArray[_Shape2T, np.dtype[_as_c64]], lower: bool = False, overwrite_ab: bool = False, check_finite: bool = True
-) -> onp.ArrayND[np.complex64, _Shape2T]: ...
-@overload  # Nd ~number
-def cholesky_banded(
-    ab: nptc.CanArray[_Shape2T, np.dtype[npc.number]], lower: bool = False, overwrite_ab: bool = False, check_finite: bool = True
-) -> onp.ArrayND[Any, _Shape2T]: ...
-@overload  # 2d +f64
-def cholesky_banded(
-    ab: _Sequence2D[float], lower: bool = False, overwrite_ab: bool = False, check_finite: bool = True
-) -> onp.Array2D[np.float64]: ...
-@overload  # 2d ~c128
-def cholesky_banded(
-    ab: Sequence[MutableSequence[complex]], lower: bool = False, overwrite_ab: bool = False, check_finite: bool = True
-) -> onp.Array2D[np.complex128]: ...
-@overload  # ?d +f64
-def cholesky_banded(
-    ab: onp.SequenceND[_Sequence2D[float]], lower: bool = False, overwrite_ab: bool = False, check_finite: bool = True
 ) -> onp.ArrayND[np.float64]: ...
-@overload  # ?d ~c128
-def cholesky_banded(
-    ab: onp.SequenceND[Sequence[MutableSequence[complex]]],
-    lower: bool = False,
-    overwrite_ab: bool = False,
+@overload  # ?d +f64, ?d +f64\+f32
+def cho_solve(
+    c_and_lower: tuple[onp.ToFloatND, bool],
+    b: onp.ToArrayND[float, _as_f64],
+    overwrite_b: bool = False,
+    check_finite: bool = True,
+) -> onp.ArrayND[np.float64]: ...
+@overload  # ?d ~c128|c160, ?d +c128
+def cho_solve(
+    c_and_lower: tuple[onp.ToJustComplex128_ND | onp.ToJustCLongDoubleND, bool],
+    b: onp.ToComplexND,
+    overwrite_b: bool = False,
     check_finite: bool = True,
 ) -> onp.ArrayND[np.complex128]: ...
+@overload  # ?d +c128, ?d ~c128|c160
+def cho_solve(
+    c_and_lower: tuple[onp.ToComplexND, bool],
+    b: onp.ToJustComplex128_ND | onp.ToJustCLongDoubleND,
+    overwrite_b: bool = False,
+    check_finite: bool = True,
+) -> onp.ArrayND[np.complex128]: ...
+@overload  # ?d ~c64, ?d +c64
+def cho_solve(
+    c_and_lower: tuple[onp.ToJustComplex64_ND, bool], b: onp.ToComplex64_ND, overwrite_b: bool = False, check_finite: bool = True
+) -> onp.ArrayND[np.complex64]: ...
+@overload  # ?d +c64, ?d ~c64
+def cho_solve(
+    c_and_lower: tuple[onp.ToComplex64_ND, bool], b: onp.ToJustComplex64_ND, overwrite_b: bool = False, check_finite: bool = True
+) -> onp.ArrayND[np.complex64]: ...
+@overload  # ?d +cfloating, ?d ~cfloating (fallback)
+def cho_solve(
+    c_and_lower: tuple[onp.ToComplexND, bool], b: onp.ToComplexND, overwrite_b: bool = False, check_finite: bool = True
+) -> onp.ArrayND[Any]: ...
 
 #
 @overload
--- tests/linalg/test__decomp_cholesky.pyi
+++ tests/linalg/test__decomp_cholesky.pyi
@@ -8,8 +8,6 @@ import optype.numpy.compat as npc
 
 from scipy.linalg import cho_factor, cho_solve, cho_solve_banded, cholesky, cholesky_banded
 
-_Float2D: TypeAlias = onp.Array2D[npc.floating]
-_FloatND: TypeAlias = onp.ArrayND[npc.floating]
 _Complex2D: TypeAlias = onp.Array2D[npc.inexact]
 _ComplexND: TypeAlias = onp.ArrayND[npc.inexact]
 
@@ -19,9 +17,9 @@ _bool_2d: onp.Array2D[np.bool_]
 _i8_2d: onp.Array2D[np.int8]
 _i16_2d: onp.Array2D[np.int16]
 _i32_2d: onp.Array2D[np.int32]
-_i64_2d: onp.Array2D[np.int64]
 _f16_2d: onp.Array2D[np.float16]
 _f32_2d: onp.Array2D[np.float32]
+_i64_2d: onp.Array2D[np.int64]
 _f80_2d: onp.Array2D[np.float128]
 
 _f64_1d: onp.Array1D[np.float64]
@@ -52,6 +50,7 @@ assert_type(cholesky(_i8_2d), onp.Array2D[np.float32])
 assert_type(cholesky(_i16_2d), onp.Array2D[np.float32])
 assert_type(cholesky(_i32_2d), onp.Array2D[np.float64])
 assert_type(cholesky(_i64_2d), onp.Array2D[np.float64])
+assert_type(cholesky(_f16_2d), onp.Array2D[np.float32])
 assert_type(cholesky(_f32_2d), onp.Array2D[np.float32])
 assert_type(cholesky(_f64_2d), onp.Array2D[np.float64])
 assert_type(cholesky(_f80_2d), onp.Array2D[np.float64])
@@ -77,6 +76,7 @@ assert_type(cho_factor(_i8_2d), tuple[onp.Array2D[np.float32], bool])
 assert_type(cho_factor(_i16_2d), tuple[onp.Array2D[np.float32], bool])
 assert_type(cho_factor(_i32_2d), tuple[onp.Array2D[np.float64], bool])
 assert_type(cho_factor(_i64_2d), tuple[onp.Array2D[np.float64], bool])
+assert_type(cho_factor(_f16_2d), tuple[onp.Array2D[np.float32], bool])
 assert_type(cho_factor(_f32_2d), tuple[onp.Array2D[np.float32], bool])
 assert_type(cho_factor(_f64_2d), tuple[onp.Array2D[np.float64], bool])
 assert_type(cho_factor(_f80_2d), tuple[onp.Array2D[np.float64], bool])
@@ -102,6 +102,7 @@ assert_type(cholesky_banded(_i8_2d), onp.Array2D[np.float32])
 assert_type(cholesky_banded(_i16_2d), onp.Array2D[np.float32])
 assert_type(cholesky_banded(_i32_2d), onp.Array2D[np.float64])
 assert_type(cholesky_banded(_i64_2d), onp.Array2D[np.float64])
+assert_type(cholesky_banded(_f16_2d), onp.Array2D[np.float32])
 assert_type(cholesky_banded(_f32_2d), onp.Array2D[np.float32])
 assert_type(cholesky_banded(_f64_2d), onp.Array2D[np.float64])
 assert_type(cholesky_banded(_f80_2d), onp.Array2D[np.float64])
@@ -122,10 +123,49 @@ assert_type(cholesky_banded(_py_c_3d), onp.ArrayND[np.complex128])
 ###
 # cho_solve
 
-assert_type(cho_solve((_f64_2d, False), _f64_1d), _Float2D)
-assert_type(cho_solve((_f64_3d, False), _f64_3d), _FloatND)
-assert_type(cho_solve((_c128_2d, False), _c128_1d), _Complex2D)
-assert_type(cho_solve((_c128_3d, False), _c128_3d), _ComplexND)
+assert_type(cho_solve((_bool_2d, False), _bool_2d), onp.ArrayND[np.float32])
+assert_type(cho_solve((_i8_2d, False), _i8_2d), onp.ArrayND[np.float32])
+assert_type(cho_solve((_i16_2d, False), _i16_2d), onp.ArrayND[np.float32])
+assert_type(cho_solve((_i32_2d, False), _i32_2d), onp.ArrayND[np.float64])
+assert_type(cho_solve((_i64_2d, False), _i64_2d), onp.ArrayND[np.float64])
+assert_type(cho_solve((_f16_2d, False), _f16_2d), onp.ArrayND[np.float32])
+assert_type(cho_solve((_f32_2d, False), _f32_2d), onp.ArrayND[np.float32])
+assert_type(cho_solve((_f64_2d, False), _f64_2d), onp.ArrayND[np.float64])
+assert_type(cho_solve((_f80_2d, False), _f80_2d), onp.ArrayND[np.float64])
+assert_type(cho_solve((_c64_2d, False), _c64_2d), onp.ArrayND[np.complex64])
+assert_type(cho_solve((_c128_2d, False), _c128_2d), onp.ArrayND[np.complex128])
+assert_type(cho_solve((_c160_2d, False), _c160_2d), onp.ArrayND[np.complex128])
+assert_type(cho_solve((_number_2d, False), _number_2d), onp.ArrayND[Any])
+
+assert_type(cho_solve((_i16_2d, False), _f32_2d), onp.ArrayND[np.float32])
+assert_type(cho_solve((_i16_2d, False), _f64_2d), onp.ArrayND[np.float64])
+assert_type(cho_solve((_i16_2d, False), _c64_2d), onp.ArrayND[np.complex64])
+assert_type(cho_solve((_i16_2d, False), _c128_2d), onp.ArrayND[np.complex128])
+
+assert_type(cho_solve((_f32_2d, False), _i16_2d), onp.ArrayND[np.float32])
+assert_type(cho_solve((_f32_2d, False), _f64_2d), onp.ArrayND[np.float64])
+assert_type(cho_solve((_f32_2d, False), _c64_2d), onp.ArrayND[np.complex64])
+assert_type(cho_solve((_f32_2d, False), _c128_2d), onp.ArrayND[np.complex128])
+
+assert_type(cho_solve((_f64_2d, False), _i16_2d), onp.ArrayND[np.float64])
+assert_type(cho_solve((_f64_2d, False), _f32_2d), onp.ArrayND[np.float64])
+assert_type(cho_solve((_f64_2d, False), _c64_2d), onp.ArrayND[Any])
+assert_type(cho_solve((_f64_2d, False), _c128_2d), onp.ArrayND[np.complex128])
+
+assert_type(cho_solve((_c64_2d, False), _i16_2d), onp.ArrayND[np.complex64])
+assert_type(cho_solve((_c64_2d, False), _f32_2d), onp.ArrayND[np.complex64])
+assert_type(cho_solve((_c64_2d, False), _f64_2d), onp.ArrayND[Any])
+assert_type(cho_solve((_c64_2d, False), _c128_2d), onp.ArrayND[np.complex128])
+
+assert_type(cho_solve((_c128_2d, False), _i16_2d), onp.ArrayND[np.complex128])
+assert_type(cho_solve((_c128_2d, False), _f32_2d), onp.ArrayND[np.complex128])
+assert_type(cho_solve((_c128_2d, False), _f64_2d), onp.ArrayND[np.complex128])
+assert_type(cho_solve((_c128_2d, False), _c64_2d), onp.ArrayND[np.complex128])
+
+assert_type(cho_solve((_py_f_2d, False), _py_f_2d), onp.ArrayND[np.float64])
+assert_type(cho_solve((_py_f_2d, False), _py_c_2d), onp.ArrayND[np.complex128])
+assert_type(cho_solve((_py_c_2d, False), _py_f_2d), onp.ArrayND[np.complex128])
+assert_type(cho_solve((_py_c_2d, False), _py_c_2d), onp.ArrayND[np.complex128])
 
 ###
 # cho_solve_banded
