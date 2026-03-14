6f37ed56b27293aee05716984ca2ee9aead0b2c4
jorenham <jhammudoglu@gmail.com>
2026-03-14 16:30:05 +0100

    ✅ `interpolate`: test `interpn`

--- tests/interpolate/test_rgi.pyi
+++ tests/interpolate/test_rgi.pyi
@@ -1,6 +1,18 @@
-import numpy as np
+from typing import assert_type
 
-from scipy.interpolate import RegularGridInterpolator
+import numpy as np
+import optype.numpy as onp
+
+from scipy.interpolate import RegularGridInterpolator, interpn
 
 # regression test for https://github.com/scipy/scipy-stubs/issues/497
 RegularGridInterpolator(np.array([], dtype=np.float64), np.array([], dtype=np.float64))
+
+###
+# interpn
+
+_f64_1d: onp.Array1D[np.float64]
+_c128_1d: onp.Array1D[np.complex128]
+
+assert_type(interpn((_f64_1d,), _f64_1d, _f64_1d), onp.ArrayND[np.float64])
+assert_type(interpn((_f64_1d,), _c128_1d, _f64_1d), onp.ArrayND[np.complex128])
