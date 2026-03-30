8ab1457daacd14f62d1d04d5c02f906e36392148
jorenham <jhammudoglu@gmail.com>
2026-03-30 22:52:19 +0200

    ✨ `stats`: improved `f_oneway` shape-typing

--- scipy-stubs/stats/_stats_py.pyi
+++ scipy-stubs/stats/_stats_py.pyi
@@ -3372,13 +3372,138 @@ def trim_mean(
 ) -> onp.ArrayND[np.float64]: ...
 
 # TODO(jorenham): improve
+@overload  # ?d, ?d|1d
 def f_oneway(
-    *samples: onp.ToFloatND,
-    axis: int | None = 0,
-    keepdims: bool = False,
-    nan_policy: NanPolicy = "propagate",
+    sample1: _ToFloatStrictND,
+    sample2: _ToFloatStrictND | onp.ToFloatStrict1D,
+    /,
+    *samples: _ToFloatStrictND | onp.ToFloatStrict1D,
     equal_var: bool = True,
-) -> F_onewayResult: ...
+    axis: int = 0,
+    nan_policy: NanPolicy = "propagate",
+    keepdims: L[False] = False,
+) -> F_onewayResult[np.float64 | Any]: ...
+@overload  # ?d|1d, ?d
+def f_oneway(
+    sample1: _ToFloatStrictND | onp.ToFloatStrict1D,
+    sample2: _ToFloatStrictND,
+    /,
+    *samples: _ToFloatStrictND | onp.ToFloatStrict1D,
+    equal_var: bool = True,
+    axis: int = 0,
+    nan_policy: NanPolicy = "propagate",
+    keepdims: L[False] = False,
+) -> F_onewayResult[np.float64 | Any]: ...
+@overload  # ?d, 2d|3d
+def f_oneway(
+    sample1: _ToFloatStrictND,
+    sample2: onp.ToFloatStrict2D | onp.ToFloatStrict3D,
+    /,
+    *samples: _ToFloatStrictND | onp.ToFloatStrict1D,
+    equal_var: bool = True,
+    axis: int = 0,
+    nan_policy: NanPolicy = "propagate",
+    keepdims: L[False] = False,
+) -> F_onewayResult[onp.ArrayND[np.float64]]: ...
+@overload  # 2d|3d, ?d
+def f_oneway(
+    sample1: onp.ToFloatStrict2D | onp.ToFloatStrict3D,
+    sample2: _ToFloatStrictND,
+    /,
+    *samples: _ToFloatStrictND | onp.ToFloatStrict1D,
+    equal_var: bool = True,
+    axis: int = 0,
+    nan_policy: NanPolicy = "propagate",
+    keepdims: L[False] = False,
+) -> F_onewayResult[onp.ArrayND[np.float64]]: ...
+@overload  # 1d, 1d
+def f_oneway(
+    sample1: onp.ToFloatStrict1D,
+    sample2: onp.ToFloatStrict1D,
+    /,
+    *samples: onp.ToFloatStrict1D,
+    equal_var: bool = True,
+    axis: int = 0,
+    nan_policy: NanPolicy = "propagate",
+    keepdims: L[False] = False,
+) -> F_onewayResult[np.float64]: ...
+@overload  # 2d, <=2d
+def f_oneway(
+    sample1: onp.ToFloatStrict2D,
+    sample2: onp.ToFloatStrict2D | onp.ToFloatStrict1D,
+    /,
+    *samples: onp.ToFloatStrict2D | onp.ToFloatStrict1D,
+    equal_var: bool = True,
+    axis: int = 0,
+    nan_policy: NanPolicy = "propagate",
+    keepdims: L[False] = False,
+) -> F_onewayResult[onp.Array1D[np.float64]]: ...
+@overload  # <=2d, 2d
+def f_oneway(
+    sample1: onp.ToFloatStrict2D | onp.ToFloatStrict1D,
+    sample2: onp.ToFloatStrict2D,
+    /,
+    *samples: onp.ToFloatStrict2D | onp.ToFloatStrict1D,
+    equal_var: bool = True,
+    axis: int = 0,
+    nan_policy: NanPolicy = "propagate",
+    keepdims: L[False] = False,
+) -> F_onewayResult[onp.Array1D[np.float64]]: ...
+@overload  # 3d, <=3d
+def f_oneway(
+    sample1: onp.ToFloatStrict3D,
+    sample2: onp.ToFloatStrict3D | onp.ToFloatStrict2D | onp.ToFloatStrict1D,
+    /,
+    *samples: onp.ToFloatStrict3D | onp.ToFloatStrict2D | onp.ToFloatStrict1D,
+    equal_var: bool = True,
+    axis: int = 0,
+    nan_policy: NanPolicy = "propagate",
+    keepdims: L[False] = False,
+) -> F_onewayResult[onp.Array2D[np.float64]]: ...
+@overload  # <=3d, 3d
+def f_oneway(
+    sample1: onp.ToFloatStrict3D | onp.ToFloatStrict2D | onp.ToFloatStrict1D,
+    sample2: onp.ToFloatStrict3D,
+    /,
+    *samples: onp.ToFloatStrict3D | onp.ToFloatStrict2D | onp.ToFloatStrict1D,
+    equal_var: bool = True,
+    axis: int = 0,
+    nan_policy: NanPolicy = "propagate",
+    keepdims: L[False] = False,
+) -> F_onewayResult[onp.Array2D[np.float64]]: ...
+@overload  # Nd, Nd
+def f_oneway(
+    sample1: onp.ToFloatND,
+    sample2: onp.ToFloatND,
+    /,
+    *samples: onp.ToFloatND,
+    equal_var: bool = True,
+    axis: int = 0,
+    nan_policy: NanPolicy = "propagate",
+    keepdims: L[False] = False,
+) -> F_onewayResult[onp.ArrayND[np.float64] | Any]: ...
+@overload  # axis=None
+def f_oneway(
+    sample1: onp.ToFloatND,
+    sample2: onp.ToFloatND,
+    /,
+    *samples: onp.ToFloatND,
+    equal_var: bool = True,
+    axis: None,
+    nan_policy: NanPolicy = "propagate",
+    keepdims: L[False] = False,
+) -> F_onewayResult[np.float64]: ...
+@overload  # keepdims=True
+def f_oneway(
+    sample1: onp.ToFloatND,
+    sample2: onp.ToFloatND,
+    /,
+    *samples: onp.ToFloatND,
+    equal_var: bool = True,
+    axis: int | None = 0,
+    nan_policy: NanPolicy = "propagate",
+    keepdims: L[True],
+) -> F_onewayResult[onp.ArrayND[np.float64]]: ...
 
 # TODO(jorenham): improve
 def alexandergovern(
