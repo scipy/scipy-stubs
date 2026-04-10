7c180ad8759f6a9aedd442c7470eb19f7354ca5e
Aniket <148300120+Aniketsy@users.noreply.github.com>
2026-04-11 04:11:25 +0530

    Update scipy-stubs/stats/_common.pyi

--- scipy-stubs/stats/_common.pyi
+++ scipy-stubs/stats/_common.pyi
@@ -4,7 +4,7 @@ from typing_extensions import TypeVar
 import numpy as np
 import optype.numpy.compat as npc
 
-_FltT = TypeVar("_FltT", bound=npc.floating, default=np.float64)
+_FltT = TypeVar("_FltT", bound=npc.floating, default=np.float64, covariant=True)
 
 class ConfidenceInterval(NamedTuple, Generic[_FltT]):
     low: _FltT
