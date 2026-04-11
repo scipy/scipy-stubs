a6737e2490e8a526271bd60a328bd9ea61f84944
fbourgey <bourgeyflorian@gmail.com>
2026-04-11 17:57:16 -0400

    ✅ optimize: add test for newton

--- tests/optimize/test_zeros_py.pyi
+++ tests/optimize/test_zeros_py.pyi
@@ -9,11 +9,13 @@ _Float: TypeAlias = float | np.float64
 _RR: TypeAlias = RootResults[float | np.float64]
 
 def f(x: float) -> float: ...
-def g(x: onp.Array1D[np.float64]) -> float: ...
+def g(x: onp.Array1D[np.float64]) -> onp.Array1D[np.float64]: ...
+def g2(x: onp.Array2D[np.float64]) -> onp.Array2D[np.float64]: ...
 def h(x: float) -> tuple[float, float]: ...
 def k(x: float) -> tuple[float, float, float]: ...
 
 arr_1d: onp.Array1D[np.float64]
+arr_2d: onp.Array2D[np.float64]
 
 # bisect
 assert_type(bisect(f, 0.0, 1.0), float)
@@ -40,6 +42,8 @@ assert_type(newton(f, 0.5), _Float)
 assert_type(newton(f, 0.5, full_output=True), tuple[_Float, RootResults[_Float]])
 assert_type(newton(g, arr_1d), onp.Array1D[np.float64])
 assert_type(newton(g, arr_1d, full_output=True), tuple[onp.Array1D[np.float64], onp.Array1D[np.bool_], onp.Array1D[np.bool_]])
+assert_type(newton(g2, arr_2d), onp.Array2D[np.float64])
+assert_type(newton(g2, arr_2d, full_output=True), tuple[onp.Array2D[np.float64], onp.Array2D[np.bool_], onp.Array2D[np.bool_]])
 
 # root_scalar
 assert_type(root_scalar(f, bracket=(0.0, 1.0)), RootResults[float])
