3808b455144c2d7f4f0d82fac8e678f1c34f541e
Aniket Singh Yadav <singhyadavaniket43@gmail.com>
2026-04-19 20:57:33 +0000

    add type test for min, max, mean

--- tests/stats/test_mstats.pyi
+++ tests/stats/test_mstats.pyi
@@ -1,6 +1,6 @@
 # type-tests for `stats/mstats.pyi`
 
-from typing import assert_type
+from typing import Any, assert_type
 
 import numpy as np
 import optype.numpy as onp
@@ -17,7 +17,7 @@ _f32_1d: onp.Array1D[np.float32]
 assert_type(tmax(_py_i_1d), np.int_ | onp.MArray[np.int_])
 assert_type(tmax(_f32_1d), np.float32 | onp.MArray[np.float32])
 
-assert_type(tmean(_f32_1d), np.float32 | onp.MArray[np.float32])
+assert_type(tmean(_f32_1d), np.floating[Any] | onp.MArray[np.floating[Any]])
 
 assert_type(tmin(_py_i_1d), np.int_ | onp.MArray[np.int_])
 assert_type(tmin(_f32_1d), np.float32 | onp.MArray[np.float32])
