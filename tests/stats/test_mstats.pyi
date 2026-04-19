760dabc43ba71a49813b070951aa3cb928d62f48
Aniket Singh Yadav <singhyadavaniket43@gmail.com>
2026-04-19 21:16:31 +0000

    arrange in order tmean -> tmin -> tmax

--- tests/stats/test_mstats.pyi
+++ tests/stats/test_mstats.pyi
@@ -14,10 +14,10 @@ _f32_1d: onp.Array1D[np.float32]
 
 ###
 
-assert_type(tmax(_py_i_1d), np.int_ | onp.MArray[np.int_])
-assert_type(tmax(_f32_1d), np.float32 | onp.MArray[np.float32])
-
 assert_type(tmean(_f32_1d), np.floating[Any] | onp.MArray[np.floating[Any]])
 
 assert_type(tmin(_py_i_1d), np.int_ | onp.MArray[np.int_])
 assert_type(tmin(_f32_1d), np.float32 | onp.MArray[np.float32])
+
+assert_type(tmax(_py_i_1d), np.int_ | onp.MArray[np.int_])
+assert_type(tmax(_f32_1d), np.float32 | onp.MArray[np.float32])
