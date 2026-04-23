930fdee57dc82dc167a2c6a414ba87330b29bee8
Aniket Singh Yadav <singhyadavaniket43@gmail.com>
2026-04-23 04:50:22 +0000

    move tests to _shortest_path

--- tests/sparse/test_csgraph_traversal.pyi
+++ tests/sparse/test_csgraph_traversal.pyi
@@ -6,17 +6,7 @@ import optype.numpy as onp
 
 import scipy.sparse as sparse
 from ._types import ScalarType, csr_arr
-from scipy.sparse.csgraph import (
-    bellman_ford,
-    breadth_first_order,
-    breadth_first_tree,
-    depth_first_order,
-    depth_first_tree,
-    dijkstra,
-    floyd_warshall,
-    minimum_spanning_tree,
-    shortest_path,
-)
+from scipy.sparse.csgraph import breadth_first_order, breadth_first_tree, depth_first_order, depth_first_tree
 
 assert_type(breadth_first_order(csr_arr, 0, return_predecessors=True), tuple[onp.Array1D[np.int32], onp.Array1D[np.int32]])
 
@@ -31,22 +21,3 @@ assert_type(depth_first_order(csr_arr, 0, True, False), onp.Array1D[np.int32])
 assert_type(depth_first_order(csr_arr, 0), tuple[onp.Array1D[np.int32], onp.Array1D[np.int32]])
 
 assert_type(depth_first_tree(csr_arr, 0), sparse.csr_array[ScalarType, tuple[int, int]])
-
-assert_type(bellman_ford(csr_arr), onp.Array2D[np.float64])
-assert_type(bellman_ford(csr_arr, return_predecessors=True), tuple[onp.Array2D[np.float64], onp.Array2D[np.int32]])
-
-assert_type(minimum_spanning_tree(csr_arr), sparse.csr_array[ScalarType, tuple[int, int]])
-
-assert_type(shortest_path(csr_arr), onp.Array2D[np.float64])
-assert_type(shortest_path(csr_arr, return_predecessors=True), tuple[onp.Array2D[np.float64], onp.Array2D[np.int32]])
-
-assert_type(floyd_warshall(csr_arr), onp.Array2D[np.float64])
-assert_type(floyd_warshall(csr_arr, return_predecessors=True), tuple[onp.Array2D[np.float64], onp.Array2D[np.int32]])
-
-assert_type(dijkstra(csr_arr), onp.Array2D[np.float64])
-assert_type(dijkstra(csr_arr, return_predecessors=True), tuple[onp.Array2D[np.float64], onp.Array2D[np.int32]])
-assert_type(dijkstra(csr_arr, min_only=True), onp.Array1D[np.float64])
-assert_type(
-    dijkstra(csr_arr, True, None, True, False, np.inf, min_only=True),
-    tuple[onp.Array1D[np.float64], onp.Array1D[np.int32], onp.Array1D[np.int32]],
-)
