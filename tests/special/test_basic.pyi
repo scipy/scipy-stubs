ee8fef3638b8830ea1ebb63d5417169f5fefcea6
fbourgey <bourgeyflorian@gmail.com>
2026-04-29 08:03:28 -0400

    MAINT: reorder tests with order from special/_basic.pyi

--- tests/special/test_basic.pyi
+++ tests/special/test_basic.pyi
@@ -47,6 +47,95 @@ assert_type(sinc(1), np.float64)
 assert_type(sinc(1j), np.complex128)
 assert_type(sinc(i_arr), onp.ArrayND[np.float64])
 
+# yn_zeros
+assert_type(yn_zeros(1, 5), onp.Array1D[np.float64])
+
+# ynp_zeros
+assert_type(ynp_zeros(1, 5), onp.Array1D[np.float64])
+
+# y0_zeros
+assert_type(y0_zeros(5, False), tuple[onp.Array1D[np.complex128], onp.Array1D[np.complex128]])
+assert_type(y0_zeros(5, True), tuple[onp.Array1D[np.complex128], onp.Array1D[np.complex128]])
+
+# y1_zeros
+assert_type(y1_zeros(5, False), tuple[onp.Array1D[np.complex128], onp.Array1D[np.complex128]])
+assert_type(y1_zeros(5, True), tuple[onp.Array1D[np.complex128], onp.Array1D[np.complex128]])
+
+# y1p_zeros
+assert_type(y1p_zeros(5, False), tuple[onp.Array1D[np.complex128], onp.Array1D[np.complex128]])
+assert_type(y1p_zeros(5, True), tuple[onp.Array1D[np.complex128], onp.Array1D[np.complex128]])
+
+# erf_zeros
+assert_type(erf_zeros(5), onp.Array1D[np.complex128])
+
+# fresnelc_zeros
+assert_type(fresnelc_zeros(5), onp.Array1D[np.complex128])
+
+# fresnels_zeros
+assert_type(fresnels_zeros(5), onp.Array1D[np.complex128])
+
+# fresnel_zeros
+assert_type(fresnel_zeros(5), onp.Array1D[np.complex128])
+
+# polygamma
+assert_type(polygamma(1, 1.0), np.float64)
+assert_type(polygamma(1, f_arr), onp.ArrayND[np.float64])
+assert_type(polygamma(i_arr, 1.0), onp.ArrayND[np.float64])
+
+# bernoulli
+assert_type(bernoulli(5), onp.Array1D[np.float64])
+assert_type(bernoulli(5.0), onp.Array1D[np.float64])
+
+# euler
+assert_type(euler(5), onp.Array1D[np.float64])
+assert_type(euler(5.0), onp.Array1D[np.float64])
+
+# ai_zeros
+assert_type(
+    ai_zeros(5), tuple[onp.Array1D[np.float64], onp.Array1D[np.float64], onp.Array1D[np.float64], onp.Array1D[np.float64]]
+)
+
+# bi_zeros
+assert_type(
+    bi_zeros(5), tuple[onp.Array1D[np.float64], onp.Array1D[np.float64], onp.Array1D[np.float64], onp.Array1D[np.float64]]
+)
+
+# ber_zeros
+assert_type(ber_zeros(5), onp.Array1D[np.float64])
+
+# bei_zeros
+assert_type(bei_zeros(5), onp.Array1D[np.float64])
+
+# kei_zeros
+assert_type(kei_zeros(5), onp.Array1D[np.float64])
+
+# berp_zeros
+assert_type(berp_zeros(5), onp.Array1D[np.float64])
+
+# beip_zeros
+assert_type(beip_zeros(5), onp.Array1D[np.float64])
+
+# kerp_zeros
+assert_type(kerp_zeros(5), onp.Array1D[np.float64])
+
+# keip_zeros
+assert_type(keip_zeros(5), onp.Array1D[np.float64])
+
+# kelvin_zeros
+assert_type(
+    kelvin_zeros(5),
+    tuple[
+        onp.Array1D[np.float64],
+        onp.Array1D[np.float64],
+        onp.Array1D[np.float64],
+        onp.Array1D[np.float64],
+        onp.Array1D[np.float64],
+        onp.Array1D[np.float64],
+        onp.Array1D[np.float64],
+        onp.Array1D[np.float64],
+    ],
+)
+
 # comb
 assert_type(comb(5, 2, exact=True), int)
 assert_type(comb(5.0, 2.0), np.float32 | np.float64)
@@ -92,11 +181,6 @@ assert_type(stirling2(5, 2), np.float64)
 assert_type(stirling2(5, i_arr), onp.ArrayND[np.float64])
 assert_type(stirling2(i_arr, 2), onp.ArrayND[np.float64])
 
-# polygamma
-assert_type(polygamma(1, 1.0), np.float64)
-assert_type(polygamma(1, f_arr), onp.ArrayND[np.float64])
-assert_type(polygamma(i_arr, 1.0), onp.ArrayND[np.float64])
-
 # zeta
 assert_type(zeta(2.0), np.float64)
 assert_type(zeta(2.0, f_arr), onp.ArrayND[np.float64])
@@ -104,87 +188,3 @@ assert_type(zeta(f_arr), onp.ArrayND[np.float64])
 assert_type(zeta(2j), np.complex128)
 assert_type(zeta(2j, f_arr), onp.ArrayND[np.complex128])
 assert_type(zeta(c_arr), onp.ArrayND[np.complex128])
-
-# ai_zeros
-assert_type(
-    ai_zeros(5), tuple[onp.Array1D[np.float64], onp.Array1D[np.float64], onp.Array1D[np.float64], onp.Array1D[np.float64]]
-)
-
-# bei_zeros
-assert_type(bei_zeros(5), onp.Array1D[np.float64])
-
-# beip_zeros
-assert_type(beip_zeros(5), onp.Array1D[np.float64])
-
-# ber_zeros
-assert_type(ber_zeros(5), onp.Array1D[np.float64])
-
-# bernoulli
-assert_type(bernoulli(5), onp.Array1D[np.float64])
-assert_type(bernoulli(5.0), onp.Array1D[np.float64])
-
-# berp_zeros
-assert_type(berp_zeros(5), onp.Array1D[np.float64])
-
-# bi_zeros
-assert_type(
-    bi_zeros(5), tuple[onp.Array1D[np.float64], onp.Array1D[np.float64], onp.Array1D[np.float64], onp.Array1D[np.float64]]
-)
-
-# erf_zeros
-assert_type(erf_zeros(5), onp.Array1D[np.complex128])
-
-# euler
-assert_type(euler(5), onp.Array1D[np.float64])
-assert_type(euler(5.0), onp.Array1D[np.float64])
-
-# fresnel_zeros
-assert_type(fresnel_zeros(5), onp.Array1D[np.complex128])
-
-# fresnelc_zeros
-assert_type(fresnelc_zeros(5), onp.Array1D[np.complex128])
-
-# fresnels_zeros
-assert_type(fresnels_zeros(5), onp.Array1D[np.complex128])
-
-# kei_zeros
-assert_type(kei_zeros(5), onp.Array1D[np.float64])
-
-# keip_zeros
-assert_type(keip_zeros(5), onp.Array1D[np.float64])
-
-# kelvin_zeros
-assert_type(
-    kelvin_zeros(5),
-    tuple[
-        onp.Array1D[np.float64],
-        onp.Array1D[np.float64],
-        onp.Array1D[np.float64],
-        onp.Array1D[np.float64],
-        onp.Array1D[np.float64],
-        onp.Array1D[np.float64],
-        onp.Array1D[np.float64],
-        onp.Array1D[np.float64],
-    ],
-)
-
-# kerp_zeros
-assert_type(kerp_zeros(5), onp.Array1D[np.float64])
-
-# y0_zeros
-assert_type(y0_zeros(5, False), tuple[onp.Array1D[np.complex128], onp.Array1D[np.complex128]])
-assert_type(y0_zeros(5, True), tuple[onp.Array1D[np.complex128], onp.Array1D[np.complex128]])
-
-# y1_zeros
-assert_type(y1_zeros(5, False), tuple[onp.Array1D[np.complex128], onp.Array1D[np.complex128]])
-assert_type(y1_zeros(5, True), tuple[onp.Array1D[np.complex128], onp.Array1D[np.complex128]])
-
-# y1p_zeros
-assert_type(y1p_zeros(5, False), tuple[onp.Array1D[np.complex128], onp.Array1D[np.complex128]])
-assert_type(y1p_zeros(5, True), tuple[onp.Array1D[np.complex128], onp.Array1D[np.complex128]])
-
-# yn_zeros
-assert_type(yn_zeros(1, 5), onp.Array1D[np.float64])
-
-# ynp_zeros
-assert_type(ynp_zeros(1, 5), onp.Array1D[np.float64])
