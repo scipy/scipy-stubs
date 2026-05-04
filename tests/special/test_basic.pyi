e14917791a12ceb706f0972ee1bb395cc8f7f9ce
fbourgey <bourgeyflorian@gmail.com>
2026-05-04 10:30:53 -0400

    add test ellip_harm functions

--- tests/special/test_basic.pyi
+++ tests/special/test_basic.pyi
@@ -15,6 +15,9 @@ from scipy.special import (
     bi_zeros,
     comb,
     diric,
+    ellip_harm,
+    ellip_harm_2,
+    ellip_normal,
     erf_zeros,
     euler,
     factorial,
@@ -358,6 +361,14 @@ assert_type(multigammaln(1.0, 2), np.float64)
 assert_type(multigammaln(1.0, np.uint8(2)), np.float64)
 assert_type(multigammaln(f_arr, 2), onp.ArrayND[np.float64])
 assert_type(multigammaln(f_arr, np.uint8(2)), onp.ArrayND[np.float64])
+
+# ellip_harm
+assert_type(ellip_harm(2.0, 3.0, 3, 4.0, 6), np.float64)
+assert_type(ellip_harm(np.float32(2.0), np.float32(3.0), np.uint8(3), np.float32(4.0), np.float32(6.0)), np.float64)
+assert_type(ellip_harm(2.0, 3.0, 3, 4.0, 6, signm=1), np.float64)
+assert_type(ellip_harm(2.0, 3.0, 3, 4.0, 6, signm=1, signn=-1), np.float64)
+assert_type(ellip_harm_2(2.0, 3.0, 3, 4, 6.0), onp.Array0D[np.float64])
+assert_type(ellip_normal(2.0, 3.0, 3, 4), onp.Array0D[np.float64])
 
 # legendre_p
 assert_type(legendre_p(1, 1.0), onp.Array1D[np.float64])
