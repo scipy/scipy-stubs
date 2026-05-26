ef5c3594f4c549f8901eead11da2c8a603d0aa4a
jorenham <jhammudoglu@gmail.com>
2026-05-26 22:46:31 +0200

    👽️ `io.harwell_boeing`: sync with upstream

--- scipy-stubs/io/harwell_boeing.pyi
+++ scipy-stubs/io/harwell_boeing.pyi
@@ -2,9 +2,13 @@
 
 from typing_extensions import deprecated
 
+import optype as op
+
 __all__ = ["hb_read", "hb_write"]
 
+type _NoValueType = op.JustObject
+
 @deprecated("will be removed in SciPy v2.0.0")
-def hb_read(path_or_open_file: object, *, spmatrix: bool = True) -> object: ...
+def hb_read(path_or_open_file: object, *, spmatrix: bool | _NoValueType = ...) -> object: ...
 @deprecated("will be removed in SciPy v2.0.0")
 def hb_write(path_or_open_file: object, m: object, hb_info: object = None) -> object: ...
