fc5a5b00156b2dd6346470c1023c0a7c597b1057
jorenham <jhammudoglu@gmail.com>
2026-05-27 15:28:37 +0200

    ✅ `optimize`: update tests

--- tests/optimize/test__nnls.pyi
+++ tests/optimize/test__nnls.pyi
@@ -13,4 +13,3 @@ _b: list[float]
 
 assert_type(nnls(_a, _b), tuple[onp.ArrayND[np.float64], np.float64])
 assert_type(nnls(_a, _b, maxiter=9), tuple[onp.ArrayND[np.float64], np.float64])
-assert_type(nnls(_a, _b, atol=1e-8), tuple[onp.ArrayND[np.float64], np.float64])  # pyright:ignore[reportDeprecated] # pyrefly:ignore[deprecated]
