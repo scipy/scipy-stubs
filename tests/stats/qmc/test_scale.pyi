9cf95528fd22ad698e061f0f92bc89e90fe755f3
Aniket Singh Yadav <singhyadavaniket43@gmail.com>
2026-05-27 22:42:02 +0000

    add test for update_discrepancy

--- tests/stats/qmc/test_scale.pyi
+++ tests/stats/qmc/test_scale.pyi
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, assert_type
 
 import numpy as np
 import optype.numpy as onp
@@ -13,3 +13,5 @@ qmc.scale(_f8_nd, 0, 1)
 
 _f8_2d: onp.Array2D[np.float64]
 qmc.scale(_f8_2d, 0, 1)
+
+assert_type(qmc.update_discrepancy(_f8_2d[0], _f8_2d, 0.5), float)
