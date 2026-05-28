b1dcf901be1a157fc5f5e4c8a0a8178705fe924a
jorenham <jhammudoglu@gmail.com>
2026-05-28 21:11:43 +0200

    🚨 fix/ignore `UP046` ruff errors

--- scipy-stubs/linalg/_testutils.pyi
+++ scipy-stubs/linalg/_testutils.pyi
@@ -1,30 +1,29 @@
 from _typeshed import Unused
 from collections.abc import Callable, Iterable
-from typing import Any, Final, Generic, TypeVar
+from typing import Any, Final
 
 import numpy as np
 import optype.numpy as onp
 
-_ScalarT = TypeVar("_ScalarT", bound=np.generic)
-_ShapeT = TypeVar("_ShapeT", bound=tuple[int, ...])
+class _FakeMatrix[ScalarT: np.generic, ShapeT: tuple[int, ...]]:  # undocumented
+    _data: onp.ArrayND[ScalarT, ShapeT]
 
-class _FakeMatrix(Generic[_ScalarT, _ShapeT]):  # undocumented
-    _data: onp.ArrayND[_ScalarT, _ShapeT]
-
-    def __init__(self, /, data: onp.ArrayND[_ScalarT, _ShapeT]) -> None: ...
+    def __init__(self, /, data: onp.ArrayND[ScalarT, ShapeT]) -> None: ...
     __array_interface__: Final[Callable[[], dict[str, Any]]] = ...
 
     @property
-    def shape(self) -> _ShapeT: ...
+    def shape(self) -> ShapeT: ...
 
-class _FakeMatrix2(Generic[_ScalarT, _ShapeT]):  # undocumented
-    _data: onp.ArrayND[_ScalarT, _ShapeT]
-    def __init__(self, /, data: onp.ArrayND[_ScalarT, _ShapeT]) -> None: ...
-    def __array__(self, /, dtype: np.dtype[Any] | None = None, copy: bool | None = None) -> onp.ArrayND[_ScalarT, _ShapeT]: ...
+class _FakeMatrix2[ScalarT: np.generic, ShapeT: tuple[int, ...]]:  # undocumented
+    _data: onp.ArrayND[ScalarT, ShapeT]
+    def __init__(self, /, data: onp.ArrayND[ScalarT, ShapeT]) -> None: ...
+    def __array__(self, /, dtype: np.dtype[Any] | None = None, copy: bool | None = None) -> onp.ArrayND[ScalarT, ShapeT]: ...
     @property
-    def shape(self) -> _ShapeT: ...
+    def shape(self) -> ShapeT: ...
 
-def _get_array(shape: _ShapeT, dtype: type[_ScalarT]) -> onp.ArrayND[_ScalarT, _ShapeT]: ...  # undocumented
+def _get_array[ScalarT: np.generic, ShapeT: tuple[int, ...]](
+    shape: ShapeT, dtype: type[ScalarT]
+) -> onp.ArrayND[ScalarT, ShapeT]: ...  # undocumented
 def _id[T](x: T) -> T: ...  # undocumented
 
 #
