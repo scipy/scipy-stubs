d0debe577abadd4a74bc2be85c3922155d576203
jorenham <jhammudoglu@gmail.com>
2026-06-19 12:10:42 +0200

    🐛 `optimize`: fix global optimizer `func` type when `jac=True`

--- scipy-stubs/optimize/_basinhopping.pyi
+++ scipy-stubs/optimize/_basinhopping.pyi
@@ -1,12 +1,12 @@
 from collections.abc import Callable
-from typing import Concatenate, Literal, Protocol, TypeVar, type_check_only
+from typing import Concatenate, Literal, Protocol, TypeVar, overload, type_check_only
 
 import numpy as np
 import optype.numpy as onp
 
 from ._minimize import OptimizeResult as _MinimizeResult
 from ._optimize import OptimizeResult as _OptimizeResult
-from ._typing import MinimizerKwargs
+from ._typing import MinimizerKwargs, MinimizerKwargsJac
 
 __all__ = ["basinhopping"]
 
@@ -43,21 +43,42 @@ class OptimizeResult(_OptimizeResult):
 
 ###
 
+@overload
 def basinhopping(
     func: Callable[Concatenate[_Float1D, ...], onp.ToFloat],
     x0: onp.ToFloat1D,
-    niter: onp.ToJustInt = 100,
+    niter: int = 100,
     T: onp.ToFloat = 1.0,
     stepsize: onp.ToFloat = 0.5,
     minimizer_kwargs: MinimizerKwargs | None = None,
     take_step: Callable[[_Float1D], onp.ToFloat] | None = None,
     accept_test: _AcceptTestFun[onp.ToFloat] | None = None,
     callback: _CallbackFun[float] | _CallbackFun[np.float64] | None = None,
-    interval: onp.ToJustInt = 50,
+    interval: int = 50,
     disp: bool = False,
-    niter_success: onp.ToJustInt | None = None,
+    niter_success: int | None = None,
     rng: onp.random.ToRNG | None = None,
     *,
+    seed: onp.random.ToRNG | None = None,
+    target_accept_rate: onp.ToFloat = 0.5,
+    stepwise_factor: onp.ToFloat = 0.9,
+) -> OptimizeResult: ...
+@overload  # minimizer_kwargs={"jac": True, ...}
+def basinhopping(
+    func: Callable[Concatenate[_Float1D, ...], tuple[onp.ToFloat, onp.ToFloat1D]],
+    x0: onp.ToFloat1D,
+    niter: int = 100,
+    T: onp.ToFloat = 1.0,
+    stepsize: onp.ToFloat = 0.5,
+    *,
+    minimizer_kwargs: MinimizerKwargsJac,
+    take_step: Callable[[_Float1D], onp.ToFloat] | None = None,
+    accept_test: _AcceptTestFun[onp.ToFloat] | None = None,
+    callback: _CallbackFun[float] | _CallbackFun[np.float64] | None = None,
+    interval: int = 50,
+    disp: bool = False,
+    niter_success: int | None = None,
+    rng: onp.random.ToRNG | None = None,
     seed: onp.random.ToRNG | None = None,
     target_accept_rate: onp.ToFloat = 0.5,
     stepwise_factor: onp.ToFloat = 0.9,
--- tests/optimize/test_basinhopping.pyi
+++ tests/optimize/test_basinhopping.pyi
@@ -8,6 +8,7 @@ from scipy.optimize import basinhopping
 ###
 
 def _fn_f64_1d(x: onp.Array1D[np.float64]) -> float: ...
+def _fn_f64_1d_jac(x: onp.Array1D[np.float64]) -> tuple[float, onp.Array1D[np.float64]]: ...
 
 _f64_1d: onp.Array1D[np.float64]
 
@@ -24,3 +25,7 @@ assert_type(basinhopping(_fn_f64_1d, _f64_1d).lowest_optimization_result.status,
 # https://github.com/scipy/scipy-stubs/issues/1730
 assert_type(basinhopping(_fn_f64_1d, _f64_1d, minimizer_kwargs={"args": (1, 2)}).x, onp.Array1D[np.float64])
 assert_type(basinhopping(_fn_f64_1d, _f64_1d, minimizer_kwargs={"bounds": [(0, 1)]}).x, onp.Array1D[np.float64])
+
+# https://github.com/scipy/scipy-stubs/issues/1733
+assert_type(basinhopping(_fn_f64_1d, _f64_1d, minimizer_kwargs={"jac": False}).x, onp.Array1D[np.float64])
+assert_type(basinhopping(_fn_f64_1d_jac, _f64_1d, minimizer_kwargs={"jac": True}).x, onp.Array1D[np.float64])
