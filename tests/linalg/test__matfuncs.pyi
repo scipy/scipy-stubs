63f54a7939d77c608b978bc5d5657e1d66c7c4c4
jorenham <jhammudoglu@gmail.com>
2026-07-18 21:49:39 +0200

    ✨ `linalg.signm`: improved shape-typing and dtype propagation

--- tests/linalg/test__matfuncs.pyi
+++ tests/linalg/test__matfuncs.pyi
@@ -127,10 +127,26 @@ assert_type(logm(c64_nd), onp.ArrayND[npc.complexfloating])
 ###
 # signm
 
-assert_type(signm(py_f_2d), onp.ArrayND[np.float64])
-assert_type(signm(f32_nd), onp.ArrayND[npc.floating])
+assert_type(signm(py_f_2d), onp.Array2D[np.float64])
+assert_type(signm(py_c_2d), onp.Array2D[np.complex128])
+assert_type(signm(b1_2d), onp.Array2D[np.float32])
+assert_type(signm(i32_2d), onp.Array2D[np.float64])
+assert_type(signm(f32_2d), onp.Array2D[np.float32])
+assert_type(signm(f64_2d), onp.Array2D[np.float64])
+assert_type(signm(c64_2d), onp.Array2D[np.complex64])
+assert_type(signm(c128_2d), onp.Array2D[np.complex128])
+
+assert_type(signm(py_f_3d), onp.Array3D[np.float64])
+assert_type(signm(py_c_3d), onp.Array3D[np.complex128])
+assert_type(signm(f64_3d), onp.Array3D[np.float64])
+assert_type(signm(c128_3d), onp.Array3D[np.complex128])
+
+assert_type(signm(i32_nd), onp.ArrayND[np.float64])
+assert_type(signm(f32_nd), onp.ArrayND[np.float32])
+assert_type(signm(f64_nd), onp.ArrayND[np.float64])
+assert_type(signm(c64_nd), onp.ArrayND[np.complex64])
 assert_type(signm(c128_nd), onp.ArrayND[np.complex128])
-assert_type(signm(c64_nd), onp.ArrayND[npc.complexfloating])
+assert_type(signm(c160_nd), onp.ArrayND[np.complex128])  # pyright:ignore[reportDeprecated] # pyrefly:ignore[deprecated]
 
 ###
 # funm
