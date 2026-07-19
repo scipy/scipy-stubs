a6af98298e6fa8dfa42839ce7f6cb1cb0ac41327
Aniket Singh Yadav <singhyadavaniket43@gmail.com>
2026-07-19 01:22:00 +0000

    add test type tests for argstoarray

--- tests/stats/test_mstats_basic.pyi
+++ tests/stats/test_mstats_basic.pyi
@@ -5,8 +5,18 @@ from typing import assert_type
 import numpy as np
 import optype.numpy as onp
 
-from scipy.stats.mstats import count_tied_groups
+from scipy.stats.mstats import argstoarray, count_tied_groups
 
 ###
 _x: onp.ToFloatND
 assert_type(count_tied_groups(_x), dict[np.intp, np.intp | int])
+
+###
+_a: onp.ToFloatND
+assert_type(argstoarray(_a), onp.MArray[np.float64])
+
+_b: onp.ToFloatND
+assert_type(argstoarray(_a, _b), onp.MArray[np.float64])
+
+_c: onp.ToFloatND
+assert_type(argstoarray(_a, _b, _c), onp.MArray[np.float64])
