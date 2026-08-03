46ad23c6d65a819ce79fb9a3c00890df99544f52
Aniket Singh Yadav <singhyadavaniket43@gmail.com>
2026-08-03 05:28:49 +0000

    revert test stats.kruskal and add for mstats

--- tests/stats/test_mstats_basic.pyi
+++ tests/stats/test_mstats_basic.pyi
@@ -5,18 +5,19 @@ from typing import assert_type
 import numpy as np
 import optype.numpy as onp
 
-from scipy.stats.mstats import argstoarray, count_tied_groups
+from scipy.stats._mstats_basic import KruskalResult
+from scipy.stats.mstats import argstoarray, count_tied_groups, kruskal
 
-###
-_x: onp.ToFloatND
-assert_type(count_tied_groups(_x), dict[np.intp, np.intp | int])
+_nd: onp.ToFloatND
 
-###
-_a: onp.ToFloatND
-assert_type(argstoarray(_a), onp.MArray[np.float64])
+# count_tied_groups
+assert_type(count_tied_groups(_nd), dict[np.intp, np.intp | int])
 
-_b: onp.ToFloatND
-assert_type(argstoarray(_a, _b), onp.MArray[np.float64])
+# argstoarray
+assert_type(argstoarray(_nd), onp.MArray[np.float64])
+assert_type(argstoarray(_nd, _nd), onp.MArray[np.float64])
+assert_type(argstoarray(_nd, _nd, _nd), onp.MArray[np.float64])
 
-_c: onp.ToFloatND
-assert_type(argstoarray(_a, _b, _c), onp.MArray[np.float64])
+# kruskal
+assert_type(kruskal(_nd, _nd), KruskalResult)
+assert_type(kruskal(_nd, _nd, _nd), KruskalResult)
