51ad1348140e99a9be4ddf0ebfaee0f64ad60af7
jorenham <jhammudoglu@gmail.com>
2026-08-07 20:22:39 +0200

    🐛 `sparse.linalg.norm`: propagate `float32`/`complex64`

--- tests/sparse/linalg/test__norm.pyi
+++ tests/sparse/linalg/test__norm.pyi
@@ -1,24 +1,45 @@
-from typing import assert_type
+from typing import Any, assert_type
 
 import numpy as np
 import optype.numpy as onp
+from optype.test import assert_subtype
 
 from scipy.sparse import csr_array
 from scipy.sparse.linalg import norm
 
 ###
 
-type _Real = np.int32 | np.int64 | np.float64
-
-###
-
 a: csr_array
+a_i: csr_array[np.int64]
+a_f4: csr_array[np.float32]
+a_f8: csr_array[np.float64]
+a_c8: csr_array[np.complex64]
+a_c16: csr_array[np.complex128]
 
 ###
 
-assert_type(norm(a), _Real)
-assert_type(norm(a, ord="fro"), _Real)
-assert_type(norm(a, ord=1), _Real)
-assert_type(norm(a, axis=(0, 1)), _Real)
-assert_type(norm(a, ord=None, axis=0), onp.Array1D[_Real])
-assert_type(norm(a, axis=1), onp.Array1D[_Real])
+assert_subtype[np.float64](norm(a))
+assert_subtype[onp.Array1D[np.float64]](norm(a, axis=0))
+
+assert_type(norm(a_i), np.float64)
+assert_type(norm(a_f8), np.float64)
+assert_type(norm(a_c16), np.float64)
+assert_type(norm(a_f8, ord="fro"), np.float64)
+assert_type(norm(a_f8, axis=(0, 1)), np.float64)
+assert_type(norm(a_f8, ord=1), np.float64 | Any)
+assert_type(norm(a_f8, ord=None, axis=0), onp.Array1D[np.float64])
+assert_type(norm(a_f8, axis=1), onp.Array1D[np.float64])
+assert_type(norm(a_f8, ord=1, axis=1), onp.Array1D[np.float64 | Any])
+
+assert_type(norm(a_f4), np.float32)
+assert_type(norm(a_f4, ord="fro"), np.float32)
+assert_type(norm(a_f4, ord=1), np.float64 | Any)
+assert_type(norm(a_f4, ord=None, axis=0), onp.Array1D[np.float64])
+assert_type(norm(a_f4, axis=1), onp.Array1D[np.float64])
+
+assert_type(norm(a_c8), np.float32)
+assert_type(norm(a_c8, ord="fro"), np.float32)
+assert_type(norm(a_c8, ord=1), np.float32 | Any)
+assert_type(norm(a_c8, ord=None, axis=0), onp.Array1D[np.float32])
+assert_type(norm(a_c8, axis=1), onp.Array1D[np.float32])
+assert_type(norm(a_c8, ord=1, axis=1), onp.Array1D[np.float32 | Any])
