cbcce02982bfb318d25d41ca53fec65eac9f1e23
jorenham <jhammudoglu@gmail.com>
2026-08-15 00:22:53 +0200

    🐛 `optimize.minimize`: propagate `fun` scalar type

--- tests/optimize/test_minimize.pyi
+++ tests/optimize/test_minimize.pyi
@@ -1,17 +1,42 @@
+from typing import Literal, assert_type, overload
+
 import numpy as np
-import numpy.typing as npt
+import optype.numpy as onp
 
 from scipy.optimize import minimize
 
 ###
 
-type NDArrayFloat = npt.NDArray[np.float16 | np.float32 | np.float64]
+def _f_f32(x: onp.Array1D[np.float64]) -> np.float32: ...
+def _f_f64(x: onp.Array1D[np.float64]) -> np.float64: ...
+def _f_f64_f64(CCT: onp.Array1D[np.float64], uv_: onp.ArrayND[np.float64]) -> np.float64: ...
+def _f_float(x: onp.Array1D[np.float64]) -> float: ...
+def _f_jac_f32(x: onp.Array1D[np.float64]) -> tuple[np.float32, onp.Array1D[np.float64]]: ...
 
-def objective_function(CCT: NDArrayFloat, uv_: NDArrayFloat) -> np.float64: ...
+#
+@overload
+def _f_jac_over(x: onp.Array1D[np.float64], extra: Literal[False] = False) -> tuple[float, onp.Array1D[np.float64]]: ...
+@overload
+def _f_jac_over(x: onp.Array1D[np.float64], extra: Literal[True]) -> tuple[float, onp.Array1D[np.float64], np.float64]: ...
 
-uv = np.atleast_1d([0.1978, 0.3124])
+_f64_1d: onp.Array1D[np.float64]
+_f64_nd: onp.ArrayND[np.float64]
 
-minimize(objective_function, x0=6400, args=(uv,), method="Nelder-Mead", options={"fatol": 1e-10})
+###
 
-# https://github.com/scipy/scipy-stubs/issues/635
 minimize(lambda x: x**2, 0.0)  # type: ignore[misc]  # pyrefly: ignore[implicit-any-lambda]
+
+minimize(_f_f64_f64, x0=6400, args=(_f64_nd,), method="Nelder-Mead", options={"fatol": 1e-10})
+
+assert_type(minimize(_f_f32, _f64_1d).fun, np.float32)
+assert_type(minimize(_f_f64, _f64_1d).fun, np.float64)
+assert_type(minimize(_f_float, _f64_1d).fun, float)
+assert_type(minimize(_f_f32, _f64_1d, method="BFGS").fun, np.float32)
+assert_type(minimize(_f_jac_f32, _f64_1d, jac=True).fun, np.float32)
+assert_type(minimize(_f_jac_f32, _f64_1d, (), "SLSQP", True).fun, np.float32)
+assert_type(minimize(_f_jac_over, _f64_1d, method="L-BFGS-B", jac=True).fun, float)
+assert_type(minimize(_f_jac_over, _f64_1d, (), "L-BFGS-B", True).fun, float)
+
+assert_type(minimize(_f_f32, _f64_1d, method="Nelder-Mead").fun, np.float64)
+assert_type(minimize(_f_f32, _f64_1d, method="cobyqa").fun, np.float64)
+assert_type(minimize(_f_f32, _f64_1d, (), "COBYLA").fun, np.float64)
