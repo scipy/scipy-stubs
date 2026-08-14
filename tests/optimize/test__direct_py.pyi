7bc26a98a0542ae365977bd0889a367ed10ffa83
jorenham <jhammudoglu@gmail.com>
2026-08-14 15:48:25 +0200

    🐛 `optimize.direct`: accept more valid `bounds` types

--- tests/optimize/test__direct_py.pyi
+++ tests/optimize/test__direct_py.pyi
@@ -10,7 +10,14 @@ from scipy.optimize import direct
 
 def _func(x: onp.Array1D[np.float64]) -> float: ...
 
-_res = direct(_func, bounds=([0.0, 0.0], [1.0, 1.0]))
+_bounds_2d: onp.Array2D[np.float64]
+
+direct(_func, bounds=((0.0, 1.0), (0.0, 1.0)))
+direct(_func, bounds=[[0.0, 1.0], [0.0, 1.0]])
+direct(_func, bounds=[(0, 1), (0, 1)])
+direct(_func, bounds=_bounds_2d)
+
+_res = direct(_func, bounds=[(0.0, 1.0), (0.0, 1.0)])
 assert_type(_res.x, onp.Array1D[np.float64])
 assert_type(_res.fun, float | np.float64)
 assert_type(_res.status, int)
