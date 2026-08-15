0af51b394ceb727679ec219d3c9f92be6fbd461e
jorenham <jhammudoglu@gmail.com>
2026-08-15 16:37:11 +0200

    ♻️ `sparse._lil`: PEP 695 generics

--- scipy-stubs/sparse/_lil.pyi
+++ scipy-stubs/sparse/_lil.pyi
@@ -16,16 +16,15 @@ from ._typing import _ToShape2D
 
 __all__ = ["isspmatrix_lil", "lil_array", "lil_matrix"]
 
-#
+###
 
-type _ToMatrixPy[_T] = list[_T] | list[list[_T]]  # intentionally invariant
-type _ToMatrix[_ScalarT: npc.number | np.bool] = (
-    _spbase[_ScalarT] | onp.CanArrayND[_ScalarT] | Sequence[onp.CanArrayND[_ScalarT]] | _ToMatrixPy[_ScalarT]
-)
-type _ToAnyLIL = _ToShape2D | _ToMatrix[npc.number | np.bool]
+type _Scalar = npc.number | np.bool
 
-_ScalarT = TypeVar("_ScalarT", bound=npc.number | np.bool)
-_ScalarT_co = TypeVar("_ScalarT_co", bound=npc.number | np.bool, default=Any, covariant=True)
+type _ToMatrixPy[T] = list[T] | list[list[T]]  # intentionally invariant
+type _ToMatrix[ST: _Scalar] = _spbase[ST] | onp.CanArrayND[ST] | Sequence[onp.CanArrayND[ST]] | _ToMatrixPy[ST]
+type _ToAnyLIL = _ToShape2D | _ToMatrix[_Scalar]
+
+_ScalarT_co = TypeVar("_ScalarT_co", bound=_Scalar, default=Any, covariant=True)
 
 ###
 
@@ -91,7 +90,7 @@ class lil_array(_lil_base[_ScalarT_co], sparray[_ScalarT_co, tuple[int, int]], G
     def __assoc_stacked__(self, /) -> coo_array[_ScalarT_co, tuple[int, int]]: ...
     @override
     @type_check_only
-    def __assoc_stacked_as__(self, sctype: _ScalarT, /) -> coo_array[_ScalarT, tuple[int, int]]: ...
+    def __assoc_stacked_as__[ST: _Scalar](self, sctype: ST, /) -> coo_array[ST, tuple[int, int]]: ...
     @override
     @type_check_only
     def __assoc_as_any__(self, /) -> lil_array[Any]: ...
@@ -297,7 +296,7 @@ class lil_matrix(_lil_base[_ScalarT_co], spmatrix[_ScalarT_co], Generic[_ScalarT
     def __assoc_stacked__(self, /) -> coo_matrix[_ScalarT_co]: ...
     @override
     @type_check_only
-    def __assoc_stacked_as__(self, sctype: _ScalarT, /) -> coo_matrix[_ScalarT]: ...
+    def __assoc_stacked_as__[ST: _Scalar](self, sctype: ST, /) -> coo_matrix[ST]: ...
     @override
     @type_check_only
     def __assoc_as_any__(self, /) -> lil_matrix[Any]: ...
