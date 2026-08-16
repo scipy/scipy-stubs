038d9c52297294f5df98e9299d3e24bd4cbd2038
jorenham <jhammudoglu@gmail.com>
2026-08-16 17:20:58 +0200

    🐛 `optimize.minimize_scalar`: accept 1d array-likes for `bounds` and `bracket`

--- tests/optimize/test_minimize_scalar.pyi
+++ tests/optimize/test_minimize_scalar.pyi
@@ -1,6 +1,8 @@
 from typing import assert_type
 
+import numpy as np
 import numpy.polynomial as npp
+import optype.numpy as onp
 
 from scipy.optimize import minimize_scalar
 
@@ -8,6 +10,7 @@ def f(x: float, /) -> float: ...
 
 bracket: None
 bounds: tuple[float, float]
+arr_1d: onp.Array1D[np.float64]
 
 res = minimize_scalar(f)
 assert_type(res.success, bool)
@@ -17,6 +20,12 @@ assert_type(res.nfev, int)
 # https://github.com/scipy/scipy-stubs/issues/949
 res = minimize_scalar(f, bounds=bounds)
 res = minimize_scalar(f, bracket, bounds)
+
+res = minimize_scalar(f, bracket=[0, 2])
+res = minimize_scalar(f, bracket=[0, 1, 2])
+res = minimize_scalar(f, bracket=arr_1d)
+res = minimize_scalar(f, bounds=[0, 2], method="bounded")
+res = minimize_scalar(f, bounds=arr_1d, method="bounded")
 
 # https://github.com/scipy/scipy-stubs/issues/465
 p = npp.Polynomial([3, -2, 1, 1, 0.2])
