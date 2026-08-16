6572f3979b309554f53e5fc84edd512536545538
Aniket Singh Yadav <singhyadavaniket43@gmail.com>
2026-08-16 14:47:52 +0000

    fix(linalg): accept array-likes in qr_delete/qr_insert/qr_update

--- scipy-stubs/linalg/_decomp_update.pyi
+++ scipy-stubs/linalg/_decomp_update.pyi
@@ -1,4 +1,6 @@
-from typing import Literal
+# NOTE: mypy incorrectly sees disjoint dtypes as overlapping
+# mypy: disable-error-code=overload-overlap
+from typing import Literal, overload
 
 import numpy as np
 import optype.numpy as onp
@@ -13,30 +15,127 @@ type _Which = Literal["row", "col"]
 
 ###
 
-def qr_delete[SCT: _Inexact](
-    Q: onp.ArrayND[SCT],
-    R: onp.ArrayND[SCT],
+@overload  # float32 -> float32
+def qr_delete(
+    Q: onp.ToArrayND[np.float32, np.float32],
+    R: onp.ToArrayND[np.float32, np.float32],
     k: onp.ToJustInt,
     p: onp.ToJustInt = 1,
     which: _Which = "row",
     overwrite_qr: bool = False,
     check_finite: bool = True,
-) -> _QR[SCT]: ...
-def qr_insert[SCT: _Inexact](
-    Q: onp.ArrayND[SCT],
-    R: onp.ArrayND[SCT],
-    u: onp.ArrayND[SCT],
+) -> _QR[np.float32]: ...
+@overload  # float64 -> float64
+def qr_delete(
+    Q: onp.ToJustFloat64_ND,
+    R: onp.ToJustFloat64_ND,
+    k: onp.ToJustInt,
+    p: onp.ToJustInt = 1,
+    which: _Which = "row",
+    overwrite_qr: bool = False,
+    check_finite: bool = True,
+) -> _QR[np.float64]: ...
+@overload  # complex64 -> complex64
+def qr_delete(
+    Q: onp.ToArrayND[np.complex64, np.complex64],
+    R: onp.ToArrayND[np.complex64, np.complex64],
+    k: onp.ToJustInt,
+    p: onp.ToJustInt = 1,
+    which: _Which = "row",
+    overwrite_qr: bool = False,
+    check_finite: bool = True,
+) -> _QR[np.complex64]: ...
+@overload  # complex128 -> complex128
+def qr_delete(
+    Q: onp.ToJustComplex128_ND,
+    R: onp.ToJustComplex128_ND,
+    k: onp.ToJustInt,
+    p: onp.ToJustInt = 1,
+    which: _Which = "row",
+    overwrite_qr: bool = False,
+    check_finite: bool = True,
+) -> _QR[np.complex128]: ...
+
+#
+@overload  # float32 -> float32
+def qr_insert(
+    Q: onp.ToArrayND[np.float32, np.float32],
+    R: onp.ToArrayND[np.float32, np.float32],
+    u: onp.ToArrayND[np.float32, np.float32],
     k: onp.ToJustInt,
     which: _Which = "row",
     rcond: onp.ToFloat | None = None,
     overwrite_qru: bool = False,
     check_finite: bool = True,
-) -> _QR[SCT]: ...
-def qr_update[SCT: _Inexact](
-    Q: onp.ArrayND[SCT],
-    R: onp.ArrayND[SCT],
-    u: onp.ArrayND[SCT],
-    v: onp.ArrayND[SCT],
+) -> _QR[np.float32]: ...
+@overload  # float64 -> float64
+def qr_insert(
+    Q: onp.ToJustFloat64_ND,
+    R: onp.ToJustFloat64_ND,
+    u: onp.ToJustFloat64_ND,
+    k: onp.ToJustInt,
+    which: _Which = "row",
+    rcond: onp.ToFloat | None = None,
+    overwrite_qru: bool = False,
+    check_finite: bool = True,
+) -> _QR[np.float64]: ...
+@overload  # complex64 -> complex64
+def qr_insert(
+    Q: onp.ToArrayND[np.complex64, np.complex64],
+    R: onp.ToArrayND[np.complex64, np.complex64],
+    u: onp.ToArrayND[np.complex64, np.complex64],
+    k: onp.ToJustInt,
+    which: _Which = "row",
+    rcond: onp.ToFloat | None = None,
+    overwrite_qru: bool = False,
+    check_finite: bool = True,
+) -> _QR[np.complex64]: ...
+@overload  # complex128 -> complex128
+def qr_insert(
+    Q: onp.ToJustComplex128_ND,
+    R: onp.ToJustComplex128_ND,
+    u: onp.ToJustComplex128_ND,
+    k: onp.ToJustInt,
+    which: _Which = "row",
+    rcond: onp.ToFloat | None = None,
+    overwrite_qru: bool = False,
+    check_finite: bool = True,
+) -> _QR[np.complex128]: ...
+
+#
+@overload  # float32 -> float32
+def qr_update(
+    Q: onp.ToArrayND[np.float32, np.float32],
+    R: onp.ToArrayND[np.float32, np.float32],
+    u: onp.ToArrayND[np.float32, np.float32],
+    v: onp.ToArrayND[np.float32, np.float32],
     overwrite_qruv: bool = False,
     check_finite: bool = True,
-) -> _QR[SCT]: ...
+) -> _QR[np.float32]: ...
+@overload  # float64 -> float64
+def qr_update(
+    Q: onp.ToJustFloat64_ND,
+    R: onp.ToJustFloat64_ND,
+    u: onp.ToJustFloat64_ND,
+    v: onp.ToJustFloat64_ND,
+    overwrite_qruv: bool = False,
+    check_finite: bool = True,
+) -> _QR[np.float64]: ...
+@overload  # complex64 -> complex64
+def qr_update(
+    Q: onp.ToArrayND[np.complex64, np.complex64],
+    R: onp.ToArrayND[np.complex64, np.complex64],
+    u: onp.ToArrayND[np.complex64, np.complex64],
+    v: onp.ToArrayND[np.complex64, np.complex64],
+    overwrite_qruv: bool = False,
+    check_finite: bool = True,
+) -> _QR[np.complex64]: ...
+@overload  # complex128 -> complex128
+def qr_update(
+    Q: onp.ToJustComplex128_ND,
+    R: onp.ToJustComplex128_ND,
+    u: onp.ToJustComplex128_ND,
+    v: onp.ToJustComplex128_ND,
+    overwrite_qruv: bool = False,
+    check_finite: bool = True,
+) -> _QR[np.complex128]: ...
--- tests/linalg/test__decomp_update.pyi
+++ tests/linalg/test__decomp_update.pyi
@@ -3,6 +3,7 @@
 from typing import assert_type
 
 import numpy as np
+import optype as op
 import optype.numpy as onp
 
 from scipy.linalg import qr_delete, qr_insert, qr_update
@@ -13,6 +14,9 @@ f32_nd: onp.ArrayND[np.float32]
 f64_nd: onp.ArrayND[np.float64]
 c64_nd: onp.ArrayND[np.complex64]
 c128_nd: onp.ArrayND[np.complex128]
+
+py_float_2d: list[list[float]]
+py_complex_2d: list[list[op.JustComplex]]
 
 type _QR32 = tuple[onp.ArrayND[np.float32], onp.ArrayND[np.float32]]
 type _QR64 = tuple[onp.ArrayND[np.float64], onp.ArrayND[np.float64]]
@@ -26,6 +30,8 @@ assert_type(qr_delete(f32_nd, f32_nd, 0), _QR32)
 assert_type(qr_delete(f64_nd, f64_nd, 0), _QR64)
 assert_type(qr_delete(c64_nd, c64_nd, 0), _QRc64)
 assert_type(qr_delete(c128_nd, c128_nd, 0), _QRc128)
+assert_type(qr_delete(py_float_2d, py_float_2d, 0), _QR64)
+assert_type(qr_delete(py_complex_2d, py_complex_2d, 0), _QRc128)
 
 ###
 # qr_insert
@@ -34,6 +40,8 @@ assert_type(qr_insert(f32_nd, f32_nd, f32_nd, 0), _QR32)
 assert_type(qr_insert(f64_nd, f64_nd, f64_nd, 0), _QR64)
 assert_type(qr_insert(c64_nd, c64_nd, c64_nd, 0), _QRc64)
 assert_type(qr_insert(c128_nd, c128_nd, c128_nd, 0), _QRc128)
+assert_type(qr_insert(py_float_2d, py_float_2d, py_float_2d, 0), _QR64)
+assert_type(qr_insert(py_complex_2d, py_complex_2d, py_complex_2d, 0), _QRc128)
 
 ###
 # qr_update
@@ -42,3 +50,5 @@ assert_type(qr_update(f32_nd, f32_nd, f32_nd, f32_nd), _QR32)
 assert_type(qr_update(f64_nd, f64_nd, f64_nd, f64_nd), _QR64)
 assert_type(qr_update(c64_nd, c64_nd, c64_nd, c64_nd), _QRc64)
 assert_type(qr_update(c128_nd, c128_nd, c128_nd, c128_nd), _QRc128)
+assert_type(qr_update(py_float_2d, py_float_2d, py_float_2d, py_float_2d), _QR64)
+assert_type(qr_update(py_complex_2d, py_complex_2d, py_complex_2d, py_complex_2d), _QRc128)
