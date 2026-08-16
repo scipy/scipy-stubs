a91704529cbbf3edcbb963cb358c3b764c2b6704
Aniket Singh Yadav <singhyadavaniket43@gmail.com>
2026-08-16 16:04:42 +0000

    improve as per suggestion to combine overloads in generic

--- scipy-stubs/linalg/_decomp_update.pyi
+++ scipy-stubs/linalg/_decomp_update.pyi
@@ -1,31 +1,29 @@
-# NOTE: mypy incorrectly sees disjoint dtypes as overlapping
-# mypy: disable-error-code=overload-overlap
 from typing import Literal, overload
 
 import numpy as np
 import optype.numpy as onp
+import optype.numpy.compat as npc
 
 __all__ = ["qr_delete", "qr_insert", "qr_update"]
 
 ###
 
-type _Inexact = np.float32 | np.float64 | np.complex64 | np.complex128
-type _QR[SCT: _Inexact] = tuple[onp.ArrayND[SCT], onp.ArrayND[SCT]]
+type _QR[ScalarT: npc.inexact] = tuple[onp.ArrayND[ScalarT], onp.ArrayND[ScalarT]]
 type _Which = Literal["row", "col"]
 
 ###
 
-@overload  # float32 -> float32
-def qr_delete(
-    Q: onp.ToArrayND[np.float32, np.float32],
-    R: onp.ToArrayND[np.float32, np.float32],
+@overload  # ~f32 | ~c64
+def qr_delete[ScalarT: npc.inexact32](
+    Q: onp.ToArrayND[ScalarT, ScalarT],
+    R: onp.ToArrayND[ScalarT, ScalarT],
     k: onp.ToJustInt,
     p: onp.ToJustInt = 1,
     which: _Which = "row",
     overwrite_qr: bool = False,
     check_finite: bool = True,
-) -> _QR[np.float32]: ...
-@overload  # float64 -> float64
+) -> _QR[ScalarT]: ...
+@overload  # ~f64
 def qr_delete(
     Q: onp.ToJustFloat64_ND,
     R: onp.ToJustFloat64_ND,
@@ -35,17 +33,7 @@ def qr_delete(
     overwrite_qr: bool = False,
     check_finite: bool = True,
 ) -> _QR[np.float64]: ...
-@overload  # complex64 -> complex64
-def qr_delete(
-    Q: onp.ToArrayND[np.complex64, np.complex64],
-    R: onp.ToArrayND[np.complex64, np.complex64],
-    k: onp.ToJustInt,
-    p: onp.ToJustInt = 1,
-    which: _Which = "row",
-    overwrite_qr: bool = False,
-    check_finite: bool = True,
-) -> _QR[np.complex64]: ...
-@overload  # complex128 -> complex128
+@overload  # ~c128
 def qr_delete(
     Q: onp.ToJustComplex128_ND,
     R: onp.ToJustComplex128_ND,
@@ -57,18 +45,18 @@ def qr_delete(
 ) -> _QR[np.complex128]: ...
 
 #
-@overload  # float32 -> float32
-def qr_insert(
-    Q: onp.ToArrayND[np.float32, np.float32],
-    R: onp.ToArrayND[np.float32, np.float32],
-    u: onp.ToArrayND[np.float32, np.float32],
+@overload  # ~f32 | ~c64
+def qr_insert[ScalarT: npc.inexact32](
+    Q: onp.ToArrayND[ScalarT, ScalarT],
+    R: onp.ToArrayND[ScalarT, ScalarT],
+    u: onp.ToArrayND[ScalarT, ScalarT],
     k: onp.ToJustInt,
     which: _Which = "row",
     rcond: onp.ToFloat | None = None,
     overwrite_qru: bool = False,
     check_finite: bool = True,
-) -> _QR[np.float32]: ...
-@overload  # float64 -> float64
+) -> _QR[ScalarT]: ...
+@overload  # ~f64
 def qr_insert(
     Q: onp.ToJustFloat64_ND,
     R: onp.ToJustFloat64_ND,
@@ -79,18 +67,7 @@ def qr_insert(
     overwrite_qru: bool = False,
     check_finite: bool = True,
 ) -> _QR[np.float64]: ...
-@overload  # complex64 -> complex64
-def qr_insert(
-    Q: onp.ToArrayND[np.complex64, np.complex64],
-    R: onp.ToArrayND[np.complex64, np.complex64],
-    u: onp.ToArrayND[np.complex64, np.complex64],
-    k: onp.ToJustInt,
-    which: _Which = "row",
-    rcond: onp.ToFloat | None = None,
-    overwrite_qru: bool = False,
-    check_finite: bool = True,
-) -> _QR[np.complex64]: ...
-@overload  # complex128 -> complex128
+@overload  # ~c128
 def qr_insert(
     Q: onp.ToJustComplex128_ND,
     R: onp.ToJustComplex128_ND,
@@ -103,16 +80,16 @@ def qr_insert(
 ) -> _QR[np.complex128]: ...
 
 #
-@overload  # float32 -> float32
-def qr_update(
-    Q: onp.ToArrayND[np.float32, np.float32],
-    R: onp.ToArrayND[np.float32, np.float32],
-    u: onp.ToArrayND[np.float32, np.float32],
-    v: onp.ToArrayND[np.float32, np.float32],
+@overload  # ~f32 | ~c64
+def qr_update[ScalarT: npc.inexact32](
+    Q: onp.ToArrayND[ScalarT, ScalarT],
+    R: onp.ToArrayND[ScalarT, ScalarT],
+    u: onp.ToArrayND[ScalarT, ScalarT],
+    v: onp.ToArrayND[ScalarT, ScalarT],
     overwrite_qruv: bool = False,
     check_finite: bool = True,
-) -> _QR[np.float32]: ...
-@overload  # float64 -> float64
+) -> _QR[ScalarT]: ...
+@overload  # ~f64
 def qr_update(
     Q: onp.ToJustFloat64_ND,
     R: onp.ToJustFloat64_ND,
@@ -121,16 +98,7 @@ def qr_update(
     overwrite_qruv: bool = False,
     check_finite: bool = True,
 ) -> _QR[np.float64]: ...
-@overload  # complex64 -> complex64
-def qr_update(
-    Q: onp.ToArrayND[np.complex64, np.complex64],
-    R: onp.ToArrayND[np.complex64, np.complex64],
-    u: onp.ToArrayND[np.complex64, np.complex64],
-    v: onp.ToArrayND[np.complex64, np.complex64],
-    overwrite_qruv: bool = False,
-    check_finite: bool = True,
-) -> _QR[np.complex64]: ...
-@overload  # complex128 -> complex128
+@overload  # ~c128
 def qr_update(
     Q: onp.ToJustComplex128_ND,
     R: onp.ToJustComplex128_ND,
