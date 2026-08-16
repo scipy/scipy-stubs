c912d8606b3e041c25c96ab82cd4d8d1acb1dcd3
jorenham <jhammudoglu@gmail.com>
2026-08-16 19:26:47 +0200

    ✨ `optimize`: fix `least_squares`/`lsq_linear` dtype of `active_mask` in certain cases

--- tests/optimize/test_least_squares.pyi
+++ tests/optimize/test_least_squares.pyi
@@ -24,7 +24,8 @@ assert_type(least_squares(_f_f64_0d, x0=_f64_nd).x, onp.Array1D[np.float64])
 ###
 # Regression tests for https://github.com/scipy/scipy-stubs/issues/1852
 
-assert_type(least_squares(_f_f64_0d, 0.0).active_mask, onp.Array1D[np.float64])
-assert_type(least_squares(_f_f64_0d, 0.0, method="trf").active_mask, onp.Array1D[np.float64])
+assert_type(least_squares(_f_f64_0d, 0.0).active_mask, onp.Array1D[np.int_ | np.float64])
+assert_type(least_squares(_f_f64_0d, 0.0, method="trf").active_mask, onp.Array1D[np.int_ | np.float64])
+assert_type(least_squares(_f_f64_0d, 0.0, bounds=(0, 1)).active_mask, onp.Array1D[np.int_ | np.float64])
 assert_type(least_squares(_f_f64_0d, 0.0, method="dogbox").active_mask, onp.Array1D[np.int_])
 assert_type(least_squares(_f_f64_0d, 0.0, method="lm").active_mask, onp.Array1D[np.int_])
