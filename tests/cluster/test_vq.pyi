713df10c6bd3ed77cdff7a00cee40fc67356405a
jorenham <jhammudoglu@gmail.com>
2026-08-18 20:49:44 +0200

    🐛 `cluster.vq`: return 1d output for 1d input

--- tests/cluster/test_vq.pyi
+++ tests/cluster/test_vq.pyi
@@ -1,55 +1,54 @@
-from typing import assert_type
+from typing import Any, assert_type
 
 import numpy as np
 import optype.numpy as onp
-import optype.numpy.compat as npc
+from optype.test import assert_subtype
 
 from scipy.cluster import vq
 
-i64_2d: np.ndarray[tuple[int, int], np.dtype[np.int64]]
-f32_2d: onp.Array2D[np.float32]
-f64_2d: onp.Array2D[np.float64]
-floating_2d: onp.Array2D[np.float32 | np.float64]
-c128_2d: np.ndarray[tuple[int, int], np.dtype[np.complex128]]
+###
+
+_i64_1d: onp.Array1D[np.int64]
+_i64_2d: onp.Array2D[np.int64]
+_f32_2d: onp.Array2D[np.float32]
+_f32_1d: onp.Array1D[np.float32]
+_f64_1d: onp.Array1D[np.float64]
+_f64_2d: onp.Array2D[np.float64]
+_f64_nd: onp.ArrayND[np.float64]
+_c128_2d: onp.Array2D[np.complex128]
+
+_py_f_1d: list[float]
 
 ###
 
 # whiten
-assert_type(vq.whiten(i64_2d), onp.Array2D[np.float64])
-assert_type(vq.whiten(f64_2d), onp.Array2D[np.float64])
-assert_type(vq.whiten(c128_2d), onp.Array2D[np.complex128])
+assert_type(vq.whiten(_i64_2d), onp.Array2D[np.float64])
+assert_type(vq.whiten(_f64_2d), onp.Array2D[np.float64])
+assert_type(vq.whiten(_c128_2d), onp.Array2D[np.complex128])
+assert_type(vq.whiten(_i64_1d), onp.Array1D[np.float64])
+assert_type(vq.whiten(_f64_1d), onp.Array1D[np.float64])
+assert_subtype[onp.ArrayND[np.float64]](vq.whiten(_f64_nd))
 
 # vq
-# pyrefly: ignore [no-matching-overload]
-vq.vq(i64_2d, i64_2d)  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType, reportCallIssue]
-assert_type(vq.vq(f32_2d, f32_2d), tuple[onp.Array1D[np.int32], onp.Array1D[np.float32]])
-assert_type(vq.vq(f64_2d, f64_2d), tuple[onp.Array1D[np.int32], onp.Array1D[np.float64]])
-assert_type(vq.vq(floating_2d, floating_2d), tuple[onp.Array1D[np.int32], onp.Array1D[npc.floating]])
-# pyrefly: ignore [no-matching-overload]
-vq.vq(c128_2d, f64_2d)  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType, reportCallIssue]
+assert_type(vq.vq(_f32_2d, _f32_2d), tuple[onp.Array1D[np.int32], onp.Array1D[np.float32]])
+assert_type(vq.vq(_f64_2d, _f64_2d), tuple[onp.Array1D[np.int32], onp.Array1D[np.float64]])
 
 # py_vq
-assert_type(vq.py_vq(i64_2d, i64_2d), tuple[onp.Array1D[np.intp], onp.Array1D[np.float64]])  # pyright:ignore[reportDeprecated] # pyrefly:ignore[deprecated]
-assert_type(vq.py_vq(f32_2d, f32_2d), tuple[onp.Array1D[np.intp], onp.Array1D[np.float64]])  # pyright:ignore[reportDeprecated] # pyrefly:ignore[deprecated]
-assert_type(vq.py_vq(f64_2d, f64_2d), tuple[onp.Array1D[np.intp], onp.Array1D[np.float64]])  # pyright:ignore[reportDeprecated] # pyrefly:ignore[deprecated]
-assert_type(vq.py_vq(floating_2d, floating_2d), tuple[onp.Array1D[np.intp], onp.Array1D[np.float64]])  # pyright:ignore[reportDeprecated] # pyrefly:ignore[deprecated]
-# pyrefly: ignore [no-matching-overload]
-vq.py_vq(c128_2d, f64_2d)  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType, reportCallIssue]
+assert_type(vq.py_vq(_i64_2d, _i64_2d), tuple[onp.Array1D[np.intp], onp.Array1D[np.float64]])  # pyright:ignore[reportDeprecated] # pyrefly:ignore[deprecated]
+assert_type(vq.py_vq(_f32_2d, _f32_2d), tuple[onp.Array1D[np.intp], onp.Array1D[np.float64]])  # pyright:ignore[reportDeprecated] # pyrefly:ignore[deprecated]
+assert_type(vq.py_vq(_f64_2d, _f64_2d), tuple[onp.Array1D[np.intp], onp.Array1D[np.float64]])  # pyright:ignore[reportDeprecated] # pyrefly:ignore[deprecated]
 
 # kmeans
-# pyrefly: ignore [no-matching-overload]
-vq.kmeans(i64_2d, 2)  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType, reportCallIssue]
-assert_type(vq.kmeans(f32_2d, 2), tuple[onp.Array2D[np.float32], np.float32])
-assert_type(vq.kmeans(f64_2d, 2), tuple[onp.Array2D[np.float64], np.float64])
-assert_type(vq.kmeans(floating_2d, 2), tuple[onp.Array2D[npc.floating], npc.floating])
-# pyrefly: ignore [no-matching-overload]
-vq.kmeans(c128_2d, 2)  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType, reportCallIssue]
+assert_type(vq.kmeans(_f32_2d, 2), tuple[onp.Array2D[np.float32], np.float32])
+assert_type(vq.kmeans(_f64_2d, 2), tuple[onp.Array2D[np.float64], np.float64])
+assert_type(vq.kmeans(_f32_1d, 2), tuple[onp.Array1D[np.float32], np.float32])
+assert_type(vq.kmeans(_f64_1d, 2), tuple[onp.Array1D[np.float64], np.float64])
+assert_subtype[tuple[onp.ArrayND[np.float64 | Any], np.float64 | Any]](vq.kmeans(_f64_nd, 2))
 
 # kmeans2
-# pyrefly: ignore [no-matching-overload]
-vq.kmeans2(i64_2d, 2)  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType, reportCallIssue]
-assert_type(vq.kmeans2(f32_2d, 2), tuple[onp.Array2D[np.float32], onp.Array1D[np.int32]])
-assert_type(vq.kmeans2(f64_2d, 2), tuple[onp.Array2D[np.float64], onp.Array1D[np.int32]])
-assert_type(vq.kmeans2(floating_2d, 2), tuple[onp.Array2D[npc.floating], onp.Array1D[np.int32]])
-# pyrefly: ignore [no-matching-overload]
-vq.kmeans2(c128_2d, 2)  # type: ignore[arg-type]  # pyright: ignore[reportArgumentType, reportCallIssue]
+assert_type(vq.kmeans2(_f32_2d, 2), tuple[onp.Array2D[np.float32], onp.Array1D[np.int32]])
+assert_type(vq.kmeans2(_f64_2d, 2), tuple[onp.Array2D[np.float64], onp.Array1D[np.int32]])
+assert_type(vq.kmeans2(_f32_1d, 2), tuple[onp.Array1D[np.float32], onp.Array1D[np.int32]])
+assert_type(vq.kmeans2(_f64_1d, 2), tuple[onp.Array1D[np.float64], onp.Array1D[np.int32]])
+assert_type(vq.kmeans2(_py_f_1d, 2), tuple[onp.Array1D[np.float64], onp.Array1D[np.int32]])
+assert_subtype[tuple[onp.ArrayND[np.float64 | Any], onp.Array1D[np.int32]]](vq.kmeans2(_f64_nd, 2))
