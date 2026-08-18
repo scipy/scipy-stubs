3a24e6aa44ad74801e7c85efb40eee603af74a37
jorenham <jhammudoglu@gmail.com>
2026-08-18 21:47:12 +0200

    🐛 `linalg.expm_frechet`: fix return type when `compute_expm=False`

--- tests/linalg/test__expm_frechet.pyi
+++ tests/linalg/test__expm_frechet.pyi
@@ -10,29 +10,30 @@ from scipy.linalg import expm_cond, expm_frechet
 
 ###
 
-f64_2d: onp.Array2D[np.float64]
-f64_3d: onp.Array3D[np.float64]
-f64_nd: onp.ArrayND[np.float64]
-c128_2d: onp.Array2D[np.complex128]
-
-type _FreoResult = (
-    tuple[onp.ArrayND[np.float64], onp.ArrayND[np.float64]]
-    | tuple[onp.ArrayND[np.float64] | onp.ArrayND[np.complex128], onp.ArrayND[np.complex128]]
-)
+_f64_2d: onp.Array2D[np.float64]
+_f64_3d: onp.Array3D[np.float64]
+_f64_nd: onp.ArrayND[np.float64]
+_c128_2d: onp.Array2D[np.complex128]
 
 ###
 # expm_frechet
 
-assert_type(expm_frechet(f64_2d, f64_2d), _FreoResult)
-assert_type(expm_frechet(c128_2d, c128_2d), _FreoResult)
-assert_type(expm_frechet(f64_2d, f64_2d, compute_expm=True), _FreoResult)
-assert_type(expm_frechet(f64_2d, f64_2d, "SPS", False), _FreoResult)
-assert_type(expm_frechet(f64_2d, f64_2d, compute_expm=False), _FreoResult)
+assert_type(expm_frechet(_f64_2d, _f64_2d), tuple[onp.ArrayND[np.float64], onp.ArrayND[np.float64]])
+assert_type(expm_frechet(_f64_2d, _f64_2d, compute_expm=True), tuple[onp.ArrayND[np.float64], onp.ArrayND[np.float64]])
+assert_type(expm_frechet(_c128_2d, _c128_2d), tuple[onp.ArrayND[np.complex128], onp.ArrayND[np.complex128]])
+assert_type(expm_frechet(_c128_2d, _f64_2d), tuple[onp.ArrayND[np.complex128], onp.ArrayND[np.complex128]])
+assert_type(expm_frechet(_f64_2d, _c128_2d), tuple[onp.ArrayND[np.float64], onp.ArrayND[np.complex128]])
+
+assert_type(expm_frechet(_f64_2d, _f64_2d, "SPS", False), onp.ArrayND[np.float64])
+assert_type(expm_frechet(_f64_2d, _f64_2d, compute_expm=False), onp.ArrayND[np.float64])
+assert_type(expm_frechet(_c128_2d, _f64_2d, compute_expm=False), onp.ArrayND[np.complex128])
+assert_type(expm_frechet(_f64_2d, _c128_2d, compute_expm=False), onp.ArrayND[np.complex128])
+assert_type(expm_frechet(_c128_2d, _c128_2d, None, False), onp.ArrayND[np.complex128])
 
 ###
 # expm_cond
 
-assert_type(expm_cond(f64_2d), np.float64)
-assert_type(expm_cond(f64_3d), onp.Array1D[np.float64])
-assert_subtype[np.float64 | onp.ArrayND[np.float64]](expm_cond(f64_nd))
-assert_type(expm_cond(c128_2d), np.float64)
+assert_type(expm_cond(_f64_2d), np.float64)
+assert_type(expm_cond(_f64_3d), onp.Array1D[np.float64])
+assert_subtype[np.float64 | onp.ArrayND[np.float64]](expm_cond(_f64_nd))
+assert_type(expm_cond(_c128_2d), np.float64)
